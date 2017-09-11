54f3c7c010324e4d37e093bcf90a2bb67ba03baf
dmitrykurmanov <kurmanov.work@gmail.com>
2017-09-11 14:00:57 +0300

    fixed gitignore added test "options-showJSONEditorTab"

---
+++ testCafe/settings.js
@@ -0,0 +1,1 @@
+export const url = 'http://127.0.0.1:7777/example/';
---
+++ testCafe/tests/options.js
@@ -0,0 +1,60 @@
+import { url } from "../settings";
+import {Selector, ClientFunction} from 'testcafe';
+const assert = require('assert');
+const title = `options`;
+
+fixture `surveyjseditor: ${title}`
+
+    .page `${url}`
+
+    test(`showJSONEditorTab`, async t => {
+        const isTabExists = ClientFunction(() =>
+            document.documentElement.innerText.indexOf("JSON Editor"));
+        const hideTab = ClientFunction(() => {
+            let editorOptions = {
+                showJSONEditorTab: false
+            };
+            editor.render(null, editorOptions);
+        });
+
+        assert.notEqual(await isTabExists(), -1);
+
+        await hideTab();
+
+        assert.equal(await isTabExists(), -1);
+    });
+
+    test(`showTestSurveyTab`, async t => {
+        const isTabExists = ClientFunction(() =>
+            document.documentElement.innerText.indexOf("Test Survey"));
+        const hideTab = ClientFunction(() => {
+            let editorOptions = {
+                showTestSurveyTab: false
+            };
+            editor.render(null, editorOptions);
+        });
+
+        assert.notEqual(await isTabExists(), -1);
+
+        await hideTab();
+
+        assert.equal(await isTabExists(), -1);
+    });
+
+    
+    test(`showEmbededSurveyTab`, async t => {
+        const isTabExists = ClientFunction(() =>
+            document.documentElement.innerText.indexOf("Embed Survey"));
+        const showTab = ClientFunction(() => {
+            let editorOptions = {
+                showEmbededSurveyTab: true
+            };
+            editor.render(null, editorOptions);
+        });
+
+        assert.equal(await isTabExists(), -1);
+
+        await showTab();
+
+        assert.notEqual(await isTabExists(), -1);
+    });
