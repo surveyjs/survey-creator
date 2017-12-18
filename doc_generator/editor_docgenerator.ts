d1495aaedc5bf603df1bf8c009a3b3c212c55171
tsv2013 <tsv2013@users.noreply.github.com>
2017-12-18 11:35:08 +0300

    Fixed documentation generator

--- doc_generator/editor_docgenerator.ts
+++ doc_generator/editor_docgenerator.ts
@@ -1,6 +1,6 @@
 import * as doc from "surveyjs-doc-generator";
 import * as ts from "typescript";
 
-doc.generateDocumentation(process.argv.slice(2), {
+doc.generateDocumentation(process.argv.slice(2), <any>{
     target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS
 });
