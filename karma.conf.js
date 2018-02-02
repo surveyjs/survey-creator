ab6078b3d589523b4660401568a83cd8c9f755e2
dmitrykurmanov <kurmanov.work@gmail.com>
2018-02-02 15:15:18 +0300

    fixed karma conf because of travis timeout prettier

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,41 @@
-﻿var webpack = require('webpack');
-var webpackConfigCreator = require('./webpack.config');
+﻿var webpack = require("webpack");
+var webpackConfigCreator = require("./webpack.config");
 var webpackConfig = webpackConfigCreator({ buildType: "dev" });
 
 module.exports = function(config) {
-    config.set({
-        basePath: '',
-        frameworks: ['qunit'],
-        files: [
-            'tests/entries/*.ts'
-        ],
-        exclude: [
-        ],
-        mime: {
-            'text/x-typescript': ['ts','tsx']
-        },
-        junitReporter: {
-            outputDir: 'tmp/testresults/',
-            outputFile: 'test-results.xml'
-        },
-        preprocessors: {
-            '**/*.ts': ['webpack', 'sourcemap']
-        },
-        webpack: {
-            module: webpackConfig.module,
-            resolve: webpackConfig.resolve,
-            plugins: [
-                new webpack.SourceMapDevToolPlugin({
-                    filename: null, // if no value is provided the sourcemap is inlined
-                    test: /\.(ts|js)($|\?)/i // process .js and .ts files only
-                })
-            ]
-        },
-        reporters: ['progress', 'dots', 'junit'],
-        browsers: ['PhantomJS'],
-        colors: true,
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_WARN,
-        autoWatch: true,
-        singleRun: false,
-        concurrency: Infinity
-    })
+  config.set({
+    basePath: "",
+    frameworks: ["qunit"],
+    files: ["tests/entries/*.ts"],
+    exclude: [],
+    mime: {
+      "text/x-typescript": ["ts", "tsx"]
+    },
+    junitReporter: {
+      outputDir: "tmp/testresults/",
+      outputFile: "test-results.xml"
+    },
+    preprocessors: {
+      "**/*.ts": ["webpack", "sourcemap"]
+    },
+    browserNoActivityTimeout: 100000,
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve,
+      plugins: [
+        new webpack.SourceMapDevToolPlugin({
+          filename: null, // if no value is provided the sourcemap is inlined
+          test: /\.(ts|js)($|\?)/i // process .js and .ts files only
+        })
+      ]
+    },
+    reporters: ["progress", "dots", "junit"],
+    browsers: ["PhantomJS"],
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_WARN,
+    autoWatch: true,
+    singleRun: false,
+    concurrency: Infinity
+  });
 };
