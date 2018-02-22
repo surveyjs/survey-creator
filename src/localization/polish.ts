b10bbcfb64b6ee3499645f738ee8e3ebae351bbb
majakubowski <36697064+majakubowski@users.noreply.github.com>
2018-02-22 09:07:08 +0100

    Importing editorLocalization.ts (#274)
    Importing editorLocalization.ts instead defining object in polish.ts

--- src/localization/polish.ts
+++ src/localization/polish.ts
@@ -1,71 +1,6 @@
-export var editorLocalization = {
-    currentLocale: "",
-    locales: {},
-    getString: function (strName: string, locale: string = null) {
-        var loc = this.getLocale(locale);
-        var path = strName.split(".");
-        var obj = loc;
-        for (var i = 0; i < path.length; i++) {
-            obj = obj[path[i]];
-            if (!obj) {
-                if (loc === defaultStrings) return path[i];
-                return this.getString(strName, "pl");
-            }
-        }
-        return obj;
-    },
-    getPropertyName: function (strName: string, locale: string = null) {
-        var obj = this.getProperty(strName, locale);
-        if (obj["name"]) return obj["name"];
-        return obj;
-    },
-    getPropertyTitle: function (strName: string, locale: string = null) {
-        var obj = this.getProperty(strName, locale);
-        if (obj["title"]) return obj["title"];
-        return "";
-    },
-    getProperty: function (strName: string, locale: string = null) {
-        var obj = this.getString("p." + strName, locale);
-        if (obj !== strName) return obj;
-        var pos = strName.indexOf("_");
-        if (pos < -1) return obj;
-        strName = strName.substr(pos + 1);
-        return this.getString("p." + strName, locale);
-    },
-    getPropertyValue: function (value: any, locale: string = null) {
-        return this.getValueInternal(value, "pv", locale);
-    },
-    getValidatorName: function (name: string, locale: string = null) {
-        return this.getValueInternal(name, "validators", locale);
-    },
-    getTriggerName: function (name: string, locale: string = null) {
-        return this.getValueInternal(name, "triggers", locale);
-    },
-    getLocale(locale: string) {
-        if (!locale) locale = this.currentLocale;
-        var loc = locale ? this.locales[locale] : defaultStrings;
-        if (!loc) loc = defaultStrings;
-        return loc;
-    },
-    getValueInternal(value: any, prefix: string, locale: string = null) {
-        if (value === "" || value === null || value === undefined) return "";
-        value = value.toString();
-        var loc = this.getLocale(locale);
-        var res = loc[prefix] ? loc[prefix][value] : null;
-        if (!res) res = defaultStrings[prefix][value];
-        return res ? res : value;
-    },
-    getLocales: function (): Array<string> {
-        var res = [];
-        res.push("");
-        for (var key in this.locales) {
-            res.push(key);
-        }
-        return res;
-    }
-};
+import { editorLocalization } from "../editorLocalization";
 
-export var defaultStrings = {
+var polishStrings = {
     //survey templates
     survey: {
         edit: "Edytuj",
@@ -358,4 +293,4 @@ export var defaultStrings = {
     }
 };
 
-editorLocalization.locales["pl"] = defaultStrings;
+editorLocalization.locales["pl"] = polishStrings;
