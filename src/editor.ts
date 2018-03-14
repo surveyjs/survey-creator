8bcbb090e02d75255ed1d2204b8aaeef397041ed
dmitrykurmanov <kurmanov.work@gmail.com>
2018-03-14 15:53:09 +0300

    added "isRequired" and "titleLocation" context actions

--- src/editor.ts
+++ src/editor.ts
@@ -1249,6 +1249,34 @@ export class SurveyEditor implements ISurveyObjectEditorOptions {
       let opts = options.obj.allowingOptions;
       if (!opts) opts = {};
 
+      if (typeof options.obj.titleLocation !== "undefined") {
+        options.items.push({
+          name: "showTitle",
+          hasTitle: true,
+          text: this.getLocString("pe.showTitle"),
+          onClick: (question: Survey.Question) => {
+            if (question.titleLocation !== "hidden") {
+              question.titleLocation = "hidden";
+            } else {
+              question.titleLocation = "default";
+            }
+            this.onQuestionEditorChanged(question);
+          }
+        });
+      }
+
+      if (typeof options.obj.isRequired !== "undefined") {
+        options.items.push({
+          name: "isRequired",
+          hasTitle: true,
+          text: this.getLocString("pe.isRequired"),
+          onClick: (question: Survey.Question) => {
+            question.isRequired = !question.isRequired;
+            this.onQuestionEditorChanged(question);
+          }
+        });
+      }
+
       if (opts.allowEdit) {
         options.items.push({
           name: "editElement",
