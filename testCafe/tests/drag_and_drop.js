d84eea4f8f4ff41cfd7a707c8f1f606ab735bf65
dmitrykurmanov <kurmanov.work@gmail.com>
2018-03-22 17:02:59 +0300

    fix build

--- testCafe/tests/drag_and_drop.js
+++ testCafe/tests/drag_and_drop.js
@@ -26,19 +26,20 @@ test(`check drop to the bottom`, async t => {
   const checkCheckbox = ClientFunction(
     () =>
       document
-        .querySelectorAll(".svd_container .svd_question ")[1]
+        .querySelectorAll(".svd_container .svd_question ")[0]
         .querySelectorAll(".sv_qcbx ").length === 1
   );
+
+  await t.dragToElement(
+    `[title~=Checkbox]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 1);
+  assert.ok(await checkCheckbox());
 
   await t.dragToElement(
     `[title~=Radiogroup]`,
     `.svd_container .svd_questions_editor`
   );
-  assert.equal(await getQuestionsAddedCount(), 1);
-  await t.dragToElement(
-    `[title~=Checkbox]`,
-    `.svd_container .svd_questions_editor`
-  );
   assert.equal(await getQuestionsAddedCount(), 2);
-  assert.ok(await checkCheckbox());
 });
