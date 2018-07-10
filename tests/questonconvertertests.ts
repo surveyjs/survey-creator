d7eb18c8b4d63df9d56bb32984858daa58735169
dmitrykurmanov <kurmanov.work@gmail.com>
2018-07-10 18:59:25 +0300

    fixed unit tests

--- tests/questonconvertertests.ts
+++ tests/questonconvertertests.ts
@@ -6,7 +6,7 @@ export default QUnit.module("questionconverter");
 
 QUnit.test("get converted classes", function(assert) {
   var classes = QuestionConverter.getConvertToClasses("radiogroup");
-  assert.equal(classes.length, 2, "There are two classes for radiogroup");
+  assert.equal(classes.length, 3, "There are two classes for radiogroup");
   assert.equal(classes.indexOf("dropdown") > -1, true, "dropdown is here");
   classes = QuestionConverter.getConvertToClasses("text");
   assert.equal(classes.length, 1, "There is one class for text");
