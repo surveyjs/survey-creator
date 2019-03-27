c9fe04497795751ab09ac3ce1bfaab34e6aec20a
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-03-27 17:47:03 +0300

    Add automatic push docs to service

--- doc_generator/editor_docupdater.js
+++ doc_generator/editor_docupdater.js
@@ -9,35 +9,21 @@ var github = new Github({
   auth: 'basic'
 });
 
-var errorHandler = function(err) {
-      if(!err) {
-         console.log('Success');
-      }
-      else {
-         console.log(err);
-      }
-   };
-
 var repository = github.getRepo('surveyjs', 'service');
 
-var classesContent = fs.readFileSync('doc_generator/classes.json', 'utf8');
-
-repository.writeFile(
-   'master',
-   'surveyjs.io/App_Data/DocsEditor/classes.json',
-   classesContent,
-   'Updated Editor API documentation',
-   errorHandler
-).then(function(val) {
-
-   var pmesContent = fs.readFileSync('doc_generator/pmes.json', 'utf8');
-
+function commit(inputPath, outputPath, comment) {
+   var content = fs.readFileSync(inputPath, 'utf8');
    repository.writeFile(
       'master',
-      'surveyjs.io/App_Data/DocsEditor/pmes.json',
-      pmesContent,
-      'Updated Editor API documentation',
-      errorHandler
+      outputPath,
+      content,
+      comment,
+      function(err) {
+         console.log(err);
+      }
    );
+}
 
-});
+commit('doc_generator/classes.json','surveyjs.io/App_Data/DocsEditor/classes.json', 'Updated Editor Classes API documentation');
+commit('doc_generator/pmes.json','surveyjs.io/App_Data/DocsEditor/pmes.json', 'Updated Editor Properties API documentation');
+commit('docs/sidebar.json', 'surveyjs.io/App_Data/DocsEditor/sidebar.json', 'Updated SideBar');
