b0ce32740955ab2ec9134535349334c050e714e8
gologames <gologames@users.noreply.github.com>
2019-03-27 11:05:01 +0300

    Add automatic documentation update

--- doc_generator/editor_docgenerator.ts
+++
@@ -1,6 +0,0 @@
-import * as doc from "surveyjs-doc-generator";
-import * as ts from "typescript";
-
-doc.generateDocumentation(process.argv.slice(2), <any>{
-    target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS
-});
