479813e53d2426ec00415da6dc0b1a0bedbbcf68
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-03-28 15:13:24 +0300

    Fix async documentation commit to github.

--- doc_generator/editor_docupdater.js
+++ doc_generator/editor_docupdater.js
@@ -4,33 +4,36 @@ var Github = require('github-api');
 require('dotenv').config();
 
 var github = new Github({
-  username: process.env.GIT_USER,
-  password: process.env.GIT_PASS,
+   username: process.env.GIT_USER,
+   password: process.env.GIT_PASS,
   auth: 'basic'
 });
 
-var repository = github.getRepo('surveyjs', 'service');
+ var repository = github.getRepo('surveyjs', 'service');
 
 function commit(inputPath, outputPath, comment) {
-   var content = fs.readFileSync(inputPath, 'utf8');
-   repository.writeFile(
-      'master',
-      outputPath,
-      content,
-      comment,
-      function(err) {
-         let log = 'inputPath=' + inputPath +
+   return new Promise(function(resolve, reject) {
+      var content = fs.readFileSync(inputPath, 'utf8');
+      repository.writeFile(
+         'master',
+         outputPath,
+         content,
+         comment,
+         function(err) {
+            let log = 'inputPath=' + inputPath +
             ', outputPath=' + outputPath + ', comment=' + comment;
-         if (!err) {
-            console.log('Success: ' + log);
+            if (!err) {
+               console.log('Success: ' + log);
+            }
+            else{ 
+               console.log('Failure: ' + log + "\n" + err);
+            }
+            resolve();
          }
-         else{ 
-            console.log('Failure: ' + log + "\n" + err);
-         }
-      }
-   );
+      );
+   });
 }
 
-commit('doc_generator/classes.json','surveyjs.io/App_Data/DocsEditor/classes.json', 'Updated Editor Classes API documentation');
-commit('doc_generator/pmes.json','surveyjs.io/App_Data/DocsEditor/pmes.json', 'Updated Editor Properties API documentation');
-commit('docs/sidebar.json', 'surveyjs.io/App_Data/DocsEditor/sidebar.json', 'Updated SideBar');
+commit('docs/sidebar.json', 'surveyjs.io/App_Data/DocsEditor/sidebar.json', 'Updated SideBar').then(
+  () => commit('doc_generator/pmes.json','surveyjs.io/App_Data/DocsEditor/pmes.json', 'Updated Editor Properties API documentation')).then(
+  () => commit('doc_generator/classes.json','surveyjs.io/App_Data/DocsEditor/classes.json', 'Updated Editor Classes API documentation'));
