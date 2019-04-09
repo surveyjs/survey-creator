70ef04e638e36e1d86e55eca20a3de2c8ec49d87
gologames <gologames@users.noreply.github.com>
2019-04-09 16:41:55 +0300

    Fix docs/images

--- docupdate_npm.sh
+++ docupdate_npm.sh
@@ -2,7 +2,9 @@ mkdir __update_docs_temp
 cd ./__update_docs_temp
 mkdir service
 git clone https://$GTTOKEN@github.com/surveyjs/service
+mv ../docs/images .
 cp -a ../docs/. ./service/surveyjs.io/App_Data/DocsEditor
+mv ./images ../docs
 cd ./service
 git add ./surveyjs.io/App_Data/DocsEditor
 git commit -m "Updated Editor API documentation"
