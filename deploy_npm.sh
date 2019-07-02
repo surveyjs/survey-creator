6d6131dea1e0bad5f7bd4df796df129a3491d393
tsv2013 <tsv2013@users.noreply.github.com>
2019-07-02 15:06:23 +0300

    Set up auto-publishing for the survey-creator package only

--- deploy_npm.sh
+++ deploy_npm.sh
@@ -1,6 +1,3 @@
-cd package
-printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
-npm publish .
-cd ../build
+cd build
 printf "//registry.npmjs.org/:_authToken=${NPM_TOKEN}\n" >> ./.npmrc
 npm publish .
