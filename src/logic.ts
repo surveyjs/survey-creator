360ecad96aa460870b29897c9080df0c33c2f990
Andrew Telnov <andrew.telnov@gmail.com>
2019-07-03 16:58:54 +0300

    Add visibility types

--- src/logic.ts
+++ src/logic.ts
@@ -1,13 +1,77 @@
 import * as ko from "knockout";
 import * as Survey from "survey-knockout";
 
-export class SurveyLogicItem {}
+export interface ISurveyLogicType {
+  name: string;
+  baseClass: string;
+  propertyName: string;
+  showIf?: (survey: Survey.SurveyModel) => boolean;
+}
+
+export class SurveyLogicType {
+  constructor(
+    public logicType: ISurveyLogicType,
+    public survey: Survey.SurveyModel
+  ) {}
+  public get name(): string {
+    return this.logicType.name;
+  }
+  public get visible(): boolean {
+    return !this.logicType.showIf || this.logicType.showIf(this.survey);
+  }
+}
+
+export class SurveyLogicItem {
+  constructor(public logicType: ISurveyLogicType) {}
+}
 
 export class SurveyLogic {
+  public static types = [
+    {
+      name: "page_visibility",
+      baseClass: "page",
+      propertyName: "visibleIf",
+      showIf: function(survey: Survey.SurveyModel) {
+        return survey.pages.length > 1;
+      }
+    },
+    {
+      name: "question_visibility",
+      baseClass: "question",
+      propertyName: "visibleIf",
+      showIf: function(survey: Survey.SurveyModel) {
+        return survey.getAllQuestions().length > 0;
+      }
+    },
+    {
+      name: "panel_visibility",
+      baseClass: "panel",
+      propertyName: "visibleIf",
+      showIf: function(survey: Survey.SurveyModel) {
+        return survey.getAllPanels().length > 0;
+      }
+    }
+  ];
   public items: any;
+  public logicTypes: Array<SurveyLogicType>;
   constructor(public survey: Survey.SurveyModel) {
+    this.logicTypes = this.createLogicTypes();
     this.items = ko.observableArray();
     this.buildItems();
   }
+  public getTypeByName(name: string): SurveyLogicType {
+    for (var i = 0; i < this.logicTypes.length; i++) {
+      if (this.logicTypes[i].name == name) return this.logicTypes[i];
+    }
+    return null;
+  }
+  public update() {}
   protected buildItems() {}
+  protected createLogicTypes(): Array<SurveyLogicType> {
+    var res = [];
+    for (var i = 0; i < SurveyLogic.types.length; i++) {
+      res.push(new SurveyLogicType(SurveyLogic.types[i], this.survey));
+    }
+    return res;
+  }
 }
--- tests/logicTest.ts
+++ tests/logicTest.ts
@@ -2,8 +2,31 @@ import * as Survey from "survey-knockout";
 import { SurveyLogic, SurveyLogicItem } from "../src/logic";
 export default QUnit.module("LogicTabTests");
 
-QUnit.test("Get list of logic item", function(assert) {
-  var survey = new Survey.SurveyModel();
+QUnit.test("Page visibility logic", function(assert) {
+  var survey = new Survey.SurveyModel({});
   var logic = new SurveyLogic(survey);
-  assert.equal(logic.items.length, 0, "There are no items");
+  var pageVis = logic.getTypeByName("page_visibility");
+  assert.ok(pageVis);
+  assert.equal(pageVis.visible, false, "There is no pages");
+  survey.addNewPage("p1");
+  logic.update();
+  pageVis = logic.getTypeByName("page_visibility");
+  assert.equal(pageVis.visible, false, "There is one page only");
+  survey.addNewPage("p2");
+  logic.update();
+  pageVis = logic.getTypeByName("page_visibility");
+  assert.equal(pageVis.visible, true, "There are two pages");
+});
+QUnit.test("Question visibility logic", function(assert) {
+  var survey = new Survey.SurveyModel({});
+  survey.addNewPage("p1");
+  var logic = new SurveyLogic(survey);
+  var questionVis = logic.getTypeByName("question_visibility");
+  assert.ok(questionVis);
+  assert.equal(questionVis.visible, false, "There is no questions");
+  survey.pages[0].addNewQuestion("text", "q1");
+  assert.equal(survey.getAllQuestions().length, 1, "There is one question now");
+  logic.update();
+  questionVis = logic.getTypeByName("question_visibility");
+  assert.equal(questionVis.visible, true, "There is a question");
 });
