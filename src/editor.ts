aadff056fee7a2e587351fc6cb5608619418bdd1
dmitrykurmanov <kurmanov.work@gmail.com>
2020-01-15 13:56:36 +0300

    deprecate calling "InitSurvey" when switch navbar tab without changes;

--- src/editor.ts
+++ src/editor.ts
@@ -1699,6 +1699,7 @@ export class SurveyCreator implements ISurveyObjectEditorOptions {
   }
 
   private setTextValue(value: string) {
+    this.jsonEditor.isInitialJSON = true;
     this.jsonEditor.text = value;
   }
   /**
@@ -1865,7 +1866,7 @@ export class SurveyCreator implements ISurveyObjectEditorOptions {
   private canSwitchViewType(newType: string): boolean {
     if (newType && this.koViewType() == newType) return false;
     if (this.koViewType() == "designer") {
-      this.jsonEditor.text = this.getSurveyTextFromDesigner();
+      this.setTextValue(this.getSurveyTextFromDesigner());
     }
     if (
       (this.koViewType() == "translation" || this.koViewType() == "logic") &&
@@ -1878,10 +1879,11 @@ export class SurveyCreator implements ISurveyObjectEditorOptions {
       alert(this.getLocString("ed.correctJSON"));
       return false;
     }
-    if (!this.readOnly) {
+    if (!this.readOnly && this.jsonEditor.isJSONChanged) {
       this.initSurvey(
         new Survey.JsonObject().toJsonObject(this.jsonEditor.survey)
       );
+
       this.setModified({ type: "VIEW_TYPE_CHANGED", newType: newType });
     }
     return true;
