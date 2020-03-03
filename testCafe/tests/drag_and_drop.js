a781e798d7b025cad3a98b7dfef6a47a697e5fc3
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-03-03 12:00:33 +0300

    added d&d functional tests (all toolbox items)

--- testCafe/tests/drag_and_drop.js
+++ testCafe/tests/drag_and_drop.js
@@ -7,7 +7,7 @@ fixture`surveyjseditor: ${title}`.page`${url}`.beforeEach(async ctx => {
   await init();
 });
 
-test(`check d&d`, async t => {
+test(`check d&d all toolbox elements`, async t => {
   const getQuestionsAddedCount = ClientFunction(
     () => document.querySelectorAll(".svd_container .svd_question ").length
   );
@@ -17,6 +17,102 @@ test(`check d&d`, async t => {
     `.svd_container .svd_questions_editor`
   );
   assert.equal(await getQuestionsAddedCount(), 1);
+
+  await t.dragToElement(
+    `[title~="Checkbox"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 2);
+
+  await t.dragToElement(
+    `[title="Single Input"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 3);
+
+  await t.dragToElement(
+    `[title="Dropdown"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 4);
+
+  await t.dragToElement(
+    `[title="Comment"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 5);
+
+  await t.dragToElement(
+    `[title="Rating"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 6);
+
+  await t.dragToElement(
+    `[title="Image picker"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 7);
+
+  await t.dragToElement(
+    `[title="Boolean"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 8);
+
+  await t.dragToElement(
+    `[title="Html"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 9);
+
+  await t.dragToElement(
+    `[title="Expression (read-only)"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 10);
+
+  await t.dragToElement(
+    `[title="File"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 11);
+
+  await t.dragToElement(
+    `[title="Matrix (single choice)"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 12);
+
+  await t.dragToElement(
+    `[title="Matrix (multiple choice)"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 13);
+
+  await t.dragToElement(
+    `[title="Matrix (dynamic rows)"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 14);
+
+  await t.dragToElement(
+    `[title="Multiple Text"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 15);
+
+  await t.dragToElement(
+    `[title="Panel"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 16);
+
+  await t.dragToElement(
+    `[title="Panel (dynamic panels)"]`,
+    `.svd_container .svd_questions_editor`
+  );
+  assert.equal(await getQuestionsAddedCount(), 17 + 1); //panel dynamic adds two questions that's why 17+1
 });
 
 test(`check drop to the bottom`, async t => {
@@ -31,15 +127,17 @@ test(`check drop to the bottom`, async t => {
   );
 
   await t.dragToElement(
-    `[title~=Checkbox]`,
+    `[title~="Checkbox"]`,
     `.svd_container .svd_questions_editor`
   );
   assert.equal(await getQuestionsAddedCount(), 1);
   assert.ok(await checkCheckbox());
 
   await t.dragToElement(
-    `[title~=Radiogroup]`,
+    `[title="Radiogroup"]`,
     `.svd_container .svd_questions_editor`
   );
+
+  assert.equal(await getQuestionsAddedCount(), 2);
   assert.equal(await getQuestionsAddedCount(), 2);
 });
