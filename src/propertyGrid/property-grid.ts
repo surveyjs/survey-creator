181db229e8981eb921575d1bbb1c5064e26df8d9
dmitrykurmanov <kurmanov.work@gmail.com>
2020-05-28 12:20:33 +0300

    propertyGrid: refactored property-grid component (added class and correct params)

--- src/propertyGrid/property-grid.ts
+++ src/propertyGrid/property-grid.ts
@@ -1,14 +1,26 @@
 import * as ko from "knockout";
+
 import "./property-grid.scss";
 const templateHtml = require("./property-grid.html");
 
-export let PropertyGridViewModel;
+import { SurveyElementPropertyGrid } from "../questionEditors/questionEditor";
+
+export class PropertyGrid {
+  constructor(
+    public koObjects: any,
+    public koSelectedObject: any,
+    public selectedElementPropertyGrid: SurveyElementPropertyGrid
+  ) {}
+}
 
 ko.components.register("property-grid", {
   viewModel: {
     createViewModel: (params, componentInfo) => {
-      const creator = params.data;
-      return creator;
+      return new PropertyGrid(
+        params.koObjects,
+        params.koSelectedObject,
+        params.selectedElementPropertyGrid
+      );
     },
   },
   template: templateHtml,
