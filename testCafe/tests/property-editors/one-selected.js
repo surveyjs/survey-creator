426e6c56ca71ea21fe4d76e70271ca1c92272842
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-07-03 22:10:17 +0300

    work for the https://github.com/surveyjs/survey-creator/issues/878

--- testCafe/tests/property-editors/one-selected.js
+++ testCafe/tests/property-editors/one-selected.js
@@ -1,7 +1,7 @@
 import { url, init } from "../../settings";
 import { Selector, ClientFunction } from "testcafe";
+const assert = require("assert");
 const title = `one-selected`;
-
 
 fixture`surveyjseditor: ${title}`.page`${url}`.beforeEach(async (ctx) => {
   await init();
@@ -70,7 +70,7 @@ test(`survey logic (triggers property editor)`, async (t) => {
   await t.click(removeBtn);
 });
 
-test(`element validators property editor`, async (t) => {
+test(`radiogroup validators property editor`, async (t) => {
   const validationAccorditionTab = Selector(
     ".svd-accordion-tab-header[data-title='Validation']"
   ).withText("Validation");
@@ -113,4 +113,100 @@ test(`email validator from json`, async (t) => {
   await t.click(select).click(option).click(validationAccorditionTab);
 
   await t.click(".propertyeditor-validators .svd_custom_select select");
+});
+
+// https://github.com/surveyjs/survey-creator/issues/878
+test(`single input validators property editor`, async (t) => {
+  const getSurveyElementEditorContentModelClassName = ClientFunction(() => {
+    const el = document.querySelector(".propertyeditor-validators");
+    return ko.dataFor(el).selectedObjectEditor().className;
+  });
+
+  const getValidatorsSelectOptionsCount = ClientFunction(() => {
+    const els = document.querySelectorAll(
+      ".propertyeditor-validators .input-group select option"
+    );
+    return els.length;
+  });
+
+  const validationAccorditionTab = Selector(
+    ".svd-accordion-tab-header[data-title='Validation']"
+  ).withText("Validation");
+  const addBtn = Selector(".propertyeditor-validators .glyphicon-plus");
+  const removeBtn = Selector(".propertyeditor-validators .btn-danger");
+  const validationContent = Selector(
+    ".propertyeditor-validators .svd-propertyeditor-condition"
+  );
+  const expressionValidator = Selector(
+    ".propertyeditor-validators .svd-available-classes-container"
+  )
+    .child("li")
+    .withText("expression");
+  const numericValidator = Selector(
+    ".propertyeditor-validators .svd-available-classes-container"
+  )
+    .child("li")
+    .withText("numeric");
+  const textValidator = Selector(
+    ".propertyeditor-validators .svd-available-classes-container"
+  )
+    .child("li")
+    .withText("text");
+  const regexValidator = Selector(
+    ".propertyeditor-validators .svd-available-classes-container"
+  )
+    .child("li")
+    .withText("regex");
+  const emailValidator = Selector(
+    ".propertyeditor-validators .svd-available-classes-container"
+  )
+    .child("li")
+    .withText("e-mail");
+
+  await t.click(`[title~=Single]`);
+
+  await t.click(validationAccorditionTab);
+
+  await t.click(addBtn).click(expressionValidator);
+  await t.click(addBtn).click(numericValidator);
+  await t.click(addBtn).click(textValidator);
+  await t.click(addBtn).click(regexValidator);
+  await t.click(addBtn).click(emailValidator);
+
+  assert.equal(await getValidatorsSelectOptionsCount(), 5);
+
+  assert.equal(
+    await getSurveyElementEditorContentModelClassName(),
+    "emailvalidator"
+  );
+  await t.click(removeBtn);
+  assert.equal(await getValidatorsSelectOptionsCount(), 4);
+
+  assert.equal(
+    await getSurveyElementEditorContentModelClassName(),
+    "regexvalidator"
+  );
+  await t.click(removeBtn);
+  assert.equal(await getValidatorsSelectOptionsCount(), 3);
+
+  assert.equal(
+    await getSurveyElementEditorContentModelClassName(),
+    "textvalidator"
+  );
+  await t.click(removeBtn);
+  assert.equal(await getValidatorsSelectOptionsCount(), 2);
+
+  assert.equal(
+    await getSurveyElementEditorContentModelClassName(),
+    "numericvalidator"
+  );
+  await t.click(removeBtn);
+  assert.equal(await getValidatorsSelectOptionsCount(), 1);
+
+  assert.equal(
+    await getSurveyElementEditorContentModelClassName(),
+    "expressionvalidator"
+  );
+  await t.click(removeBtn);
+  assert.equal(await getValidatorsSelectOptionsCount(), 1);
 });
