9202019826e317db06560958108dd3834e16abbf
Andrew Telnov <andrew.telnov@gmail.com>
2020-07-28 22:20:21 +0300

    hide category in Property Grid in API, #892

--- src/questionEditors/properties.ts
+++ src/questionEditors/properties.ts
@@ -9,6 +9,15 @@ export class SurveyCreatorProperties {
   public static get instance() {
     return SurveyCreatorProperties.instanceValue;
   }
+  public hideCategory(categoryName: string) {
+    var def = SurveyQuestionEditorDefinition.definition;
+    for (var key in def) {
+      var tab = this.getTabFromElementDef(def[key], categoryName);
+      if (!!tab) {
+        tab.visible = false;
+      }
+    }
+  }
   public hideProperty(propertyName: string, objType: string = undefined) {
     //Survey.Serializer.fin
   }
@@ -19,5 +28,12 @@ export class SurveyCreatorProperties {
   ) {}
   public moveProperty(propertyName: string, objType: string, options: any) {}
   private changeProperty(propertyName: string, objType: string, options: any) {}
+  private getTabFromElementDef(elDef: any, tabName: string): any {
+    if (!elDef.tabs) return null;
+    for (var i = 0; i < elDef.tabs.length; i++) {
+      if (elDef.tabs[i].name == tabName) return elDef.tabs[i];
+    }
+    return null;
+  }
 }
 export var creatorProperties = SurveyCreatorProperties.instance;
--- tests/creatorPropertiesTests.ts
+++ tests/creatorPropertiesTests.ts
@@ -1,8 +1,31 @@
 import { creatorProperties } from "../src/questionEditors/properties";
 import { SurveyQuestionProperties } from "../src/questionEditors/questionEditor";
+import { SurveyQuestionEditorDefinition } from "../src/questionEditors/questionEditorDefinition";
 import * as Survey from "survey-knockout";
 
 export default QUnit.module("creatorPropertiesTests");
+
+function createCopy(obj: any) {
+  let res, value, key;
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+  res = Array.isArray(obj) ? [] : {};
+  for (key in obj) {
+    value = obj[key];
+    res[key] = createCopy(value);
+  }
+  return res;
+}
+var copyDefinition = null;
+function storePropertyGridDefinition() {
+  copyDefinition = createCopy(SurveyQuestionEditorDefinition.definition);
+}
+function restorePropertyGridDefinition() {
+  if (!copyDefinition) return;
+  SurveyQuestionEditorDefinition.definition = copyDefinition;
+  copyDefinition = null;
+}
 
 QUnit.test("Hide/show property", function (assert) {
   //TODO require some functionality from Survey Library
@@ -12,4 +35,15 @@ QUnit.test("Hide/show property", function (assert) {
   creatorProperties.showProperty("hasOther");
   var model = new SurveyQuestionProperties(new Survey.QuestionDropdown("q1"));
   assert.ok(model.getProperty("hasOther"), "The hasOther is visible now");
+});
+QUnit.test("Hide category", function (assert) {
+  storePropertyGridDefinition();
+  var model = new SurveyQuestionProperties(new Survey.QuestionDropdown("q1"));
+  assert.ok(model.getTabByName("data"), "data category is here");
+  creatorProperties.hideCategory("data");
+  model = new SurveyQuestionProperties(new Survey.QuestionDropdown("q1"));
+  assert.notOk(model.getTabByName("data"), "data category is hidden");
+  restorePropertyGridDefinition();
+  model = new SurveyQuestionProperties(new Survey.QuestionDropdown("q1"));
+  assert.ok(model.getTabByName("data"), "data category is here again");
 });
