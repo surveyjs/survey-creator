75b1f39c8b363ed74b9c0841e7461ba3a443011b
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-12-21 14:34:59 +0300

    Integration choices fast entry with modal: working storybook example (simple textarea)

--- packages/survey-creator-v2/stories/context-menu.stories.ts
+++ packages/survey-creator-v2/stories/context-menu.stories.ts
@@ -38,8 +38,7 @@ export const ContextMenuOrdinary = () => {
   };
 
   return {
-    template:
-      `<div style="margin-left: 200px; margin-top: 200px; width: max-content; position: relative">
+    template: `<div style="margin-left: 200px; margin-top: 200px; width: max-content; position: relative">
         <svc-button params="title: title, action: action">
         </svc-button>
         <sv-popup params="contentComponentName: name, contentComponentData: model,
@@ -77,8 +76,7 @@ export const ContextMenuWithPointer = () => {
   };
 
   return {
-    template:
-      `<div style="margin-left: 200px; margin-top: 200px; width: max-content; position: relative">
+    template: `<div style="margin-left: 200px; margin-top: 200px; width: max-content; position: relative">
         <svc-button params="title: title, action: action">
         </svc-button>
         <sv-popup params="contentComponentName: name, contentComponentData: model,
@@ -100,53 +98,66 @@ export const ContextMenuWithPointer = () => {
 };
 
 export const FastEntry = () => {
-  var isVisible = ko.observable(false);
-  ko.components.register("temp-modal", {	
-    viewModel: function(params) {
-      return {
-        contentComponentName: params.contentComponentName,
-        contentComponentData: params.contentComponentData,
-        isVisible: params.isVisible,
-        applyData: params.applyData
-      };
-    },	
-    template: `<!-- ko component: { name: contentComponentName, params: contentComponentData } --><!-- /ko -->
-              <button onclick="function() { isVisible(false); }">Cancel</button>
-              <button onclick="function() { isVisible(false); applyData(); }">OK</button>`	
+  var koIsVisible = ko.observable(false);
+  var koValue = ko.observable("default");
+
+  class TempModalViewModel {
+    public koValueTemp: ko.Observable<string>;
+    private showSubscription: ko.Computed<void>;
+
+    constructor(
+      public koValue: ko.Observable<string>,
+      public koIsVisible: ko.Observable<boolean>,
+      targetElement: HTMLElement
+    ) {
+      this.koValueTemp = ko.observable(koValue());
+
+      this.showSubscription = ko.computed(() => {
+        if (this.koIsVisible()) {
+          this.koValueTemp(koValue());
+        }
+      });
+    }
+
+    public dispose() {
+      this.showSubscription.dispose();
+    }
+  }
+
+  ko.components.register("temp-modal", {
+    viewModel: {
+      createViewModel: (params: any, componentInfo: any) => {
+        return new TempModalViewModel(
+          params.koValue,
+          params.koIsVisible,
+          componentInfo.element.parentElement
+        );
+      },
+    },
+    template: `       
+      <div><textarea data-bind="value: koValueTemp"></textarea></div>
+      <button data-bind="click: function() { koIsVisible(false); }">Cancel</button>
+      <button data-bind="click: function() { koIsVisible(false); koValue(koValueTemp()); }">OK</button>
+    `,
   });
 
-  var action = () => {
-    isVisible(true);
-  };
-
-  const textareaModel = {
-    onItemSelect: () => {
-      isVisible(false);
-    },
-    items: [
-      { title: "Question 1", isEnabled: true },
-      { title: "Question 2", isEnabled: true },
-      { title: "Question 3", isEnabled: true },
-    ],
-  };
-
   return {
-    template:
-      `<div style="margin-left: 200px; margin-top: 200px; width: max-content; position: relative">
-        <svc-button params="title: title, action: action">
+    template: `
+      <div style="margin-left: 200px; margin-top: 200px; width: max-content; position: relative">
+        <div style="margin-bottom: 30px;">
+          The value of property editor is: <b><span data-bind="text: koValue"></span></b>
+        </div>
+        <svc-button params="title: title, action: function(){koIsVisible(true);}">
         </svc-button>
-        <sv-popup params="contentComponentName: name, contentComponentData: model,
-          isVisible: isVisible, verticalPosition: verticalPosition, horizontalPosition: horizontalPosition">
+        <sv-popup params="contentComponentName: name, contentComponentData: {koIsVisible: koIsVisible, koValue: koValue}, isVisible: koIsVisible, isHideByClickOutside: false">
         </sv-popup>
-      </div>`,
+      </div>
+    `,
     context: {
       name: "temp-modal",
       title: text("Title", "Fast Entry"),
-      action: action,
-      model: textareaModel,
-      isVisible: isVisible,
-      verticalPosition: text("Vertical position", "bottom"),
-      horizontalPosition: text("Horizontal position", "right"),
+      koIsVisible: koIsVisible,
+      koValue: koValue,
     },
   };
 };
