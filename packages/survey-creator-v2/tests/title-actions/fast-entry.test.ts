023f728b57b90632eaeb5aafd0c33c398f27589c
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2020-12-29 12:41:05 +0300

    work for the https://github.com/surveyjs/survey-creator/issues/1027

--- packages/survey-creator-v2/tests/title-actions/fast-entry.test.ts
+++ packages/survey-creator-v2/tests/title-actions/fast-entry.test.ts
@@ -6,11 +6,11 @@ test("Create survey with editingObj", () => {
   originalElement.choices = [1, 2, 3];
 
   var fastEntryEditor = new FastEntryEditor(originalElement.choices);
-  expect(fastEntryEditor.survey.comment.value).toEqual("1\n2\n3");
-  fastEntryEditor.survey.comment.value = "1|item1\n2\n3\n4|item4";
+  expect(fastEntryEditor.comment.value).toEqual("1\n2\n3");
+  fastEntryEditor.comment.value = "1|item1\n2\n3\n4|item4";
   fastEntryEditor.apply();
   expect(originalElement.choices).toHaveLength(4);
   expect(originalElement.choices[0].text).toEqual("item1");
-  expect(originalElement.choices[3].value).toEqual(4);
+  expect(originalElement.choices[3].value).toEqual("4");
   expect(originalElement.choices[3].text).toEqual("item4");
 });
