f4bf0a82eb6f00da0998b4668dc1f6c89eba5f39
Andrew Telnov <andrew.telnov@gmail.com>
2021-02-25 13:34:07 +0300

    Remove unneeded files in knockout creator v2, fix build

--- packages/survey-creator-knockout/src/components/property-panel/property-panel.ts
+++ packages/survey-creator-knockout/src/components/property-panel/property-panel.ts
@@ -1,8 +1,6 @@
 import { PropertyGridViewModel } from "@survey/creator";
 import * as ko from "knockout";
 import { Base, ImplementorBase, property, SurveyModel } from "survey-knockout";
-//import "./property-panel.scss";
-//import "../../survey-theme/survey.scss";
 const template = require("./property-panel.html");
 
 ko.components.register("svc-property-panel", {
