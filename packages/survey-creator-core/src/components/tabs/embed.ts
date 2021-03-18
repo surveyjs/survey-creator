6c81d008e526ee2e6507700a5be416500e5005ca
gologames <gologames@bk.ru>
2021-03-18 15:35:15 +0300

    Changed embed tab implementation to string interpolation

--- packages/survey-creator-core/src/components/tabs/embed.ts
+++ packages/survey-creator-core/src/components/tabs/embed.ts
@@ -10,51 +10,49 @@ export class EmbedModel extends Base {
     super();
   }
   public init(creator: CreatorBase<SurveyModel>): void {
-    FunctionFactory.Instance.register("scriptsMarkup", (params: any) => {
-      //change to string interpolation
-      let result: string = "<!-- Your platform (" + params[0] + ") scripts -->\n\n";
-      if (params[1] !== "bootstrap") {
-          result += '<link href="https://unpkg.com/survey-' + params[0] + '@' + Version + '/' + (params[1] === "modern" ? "modern" : "survey") + '.css" type="text/css" rel="stylesheet"/>';
+    FunctionFactory.Instance.register("scriptsMarkup", (params: [string, string]) => {
+      const [framework, theme]: [string, string] = params;
+      let result: string = `<!-- Your platform (${framework}) scripts -->\n\n`;
+      if (theme !== "bootstrap") {
+          result += `<link href="https://unpkg.com/survey-${framework}@${Version}/${theme === "modern" ? "modern" : "survey"}.css" type="text/css" rel="stylesheet"/>`;
       }
-      return result + '\n<script src="https://unpkg.com/survey-' + params[0] + '@' + Version + '/survey.' + params[0] + '.min.js"></script>';
+      return result + `\n<script src="https://unpkg.com/survey-${framework}@${Version}/survey.${framework}.min.js"></script>`;
     });
-    FunctionFactory.Instance.register("htmlMarkup", (params: any) => {
-      switch(params[0]) { 
+    FunctionFactory.Instance.register("htmlMarkup", (params: [string, string]) => {
+      const [framework, show]: [string, string] = params;
+      switch(framework) { 
         case "angular": return "<ng-app></ng-app>";
-        case "vue": return '<div id="surveyContainer"><survey' + (params[1] === "window" ? "-window" : "") + ' :survey=\"survey\"/></div>';
+        case "vue": return `<div id="surveyContainer"><survey${show === "window" ? "-window" : ""} :survey=\"survey\"/></div>`;
         default: return '<div id="surveyContainer"></div>';
       };
     });
-    FunctionFactory.Instance.register("javascriptMarkup", (params: any) => {
-      let framework = params[0];
-      let theme = params[1];
-      let show = params[2];
-      let result = 'Survey.StylesManager.applyTheme("' + theme + '");\n\n';
-      result += "var surveyJSON = ";
-      result += JSON.stringify(creator.JSON) + ";\n\n";
-      result += "function sendDataToServer(survey) {\n";
-      result += "    //send Ajax request to your web server\n";
-      result += '    alert("The results are: " + JSON.stringify(survey.data));';
-      result += "\n}\n\n";
-      if (framework === "angular") {
+    FunctionFactory.Instance.register("javascriptMarkup", (params: [string, string, string]) => {
+      const [framework, theme, show]: [string, string, string] = params;
+      let result: string = `Survey.StylesManager.applyTheme("${theme}");\n\n`;
+      result += `var surveyJSON = ${JSON.stringify(creator.JSON)};\n\n`;
+      result += 'function sendDataToServer(survey) {\n    //send Ajax request to your web server\n    alert("The results are: " + JSON.stringify(survey.data));\n}\n\n';
+      const window: string = show === "window" ? "Window" : "";
+      switch(framework) { 
+        case "angular":
           result += `@Component({\n    selector: "ng-app",\n    template: '<div id="surveyElement"></div>'\n})`;
-          result += '\nexport class AppComponent {\n    ngOnInit() {\n        var survey = new Survey.Model(surveyJSON);\n        survey.onComplete.add(sendDataToServer);\n        Survey.Survey' + (show === "window" ? "Window" : "") + 'NG.render("surveyElement", { model: survey });\n    }\n}';
-      }
-      if (framework === "jquery") {
-        result += "var survey = new Survey.Model(surveyJSON);\n";
-        result += `$("#surveyContainer").Survey${show === "window" ? "Window" : ""}({\n    model: survey,\n    onComplete: sendDataToServer\n});`;
-      }
-      if (framework === "knockout") {
-        result += `var survey = new Survey.Model${show === "window" ? "Window" : ""}(surveyJSON${show === "page" ? ', "surveyContainer"' : ""});\n`;
-        result += show === "window" ? "survey.show();\n" : "";
-        result += "survey.onComplete.add(sendDataToServer);";
-      }
-      if (framework === "react") {
-        result += `ReactDOM.render(<Survey.Survey${show === "window" ? "Window" : ""} json={ surveyJSON } onComplete={ sendDataToServer } />, document.getElementById("surveyContainer"));`
-      }
-      if (framework === "vue") {
-        result += 'var survey = new Survey.Model(surveyJSON);\nsurvey.onComplete.add(sendDataToServer);\nnew Vue({ el: "#surveyContainer", data: { survey: survey } });';
-      }
+          result += `\nexport class AppComponent {\n    ngOnInit() {\n        var survey = new Survey.Model(surveyJSON);\n        survey.onComplete.add(sendDataToServer);\n        Survey.Survey${window}NG.render("surveyElement", { model: survey });\n    }\n}`;
+          break;
+        case "jquery":
+          result += "var survey = new Survey.Model(surveyJSON);\n";
+          result += `$("#surveyContainer").Survey${window}({\n    model: survey,\n    onComplete: sendDataToServer\n});`;
+          break;
+        case "knockout":
+          result += `var survey = new Survey.Model${window}(surveyJSON${show === "page" ? ', "surveyContainer"' : ""});\n`;
+          result += show === "window" ? "survey.show();\n" : "";
+          result += "survey.onComplete.add(sendDataToServer);";
+          break;
+        case "react":
+          result += `ReactDOM.render(<Survey.Survey${window} json={ surveyJSON } onComplete={ sendDataToServer } />, document.getElementById("surveyContainer"));`
+          break;
+        case "vue":
+          result += 'var survey = new Survey.Model(surveyJSON);\nsurvey.onComplete.add(sendDataToServer);\nnew Vue({ el: "#surveyContainer", data: { survey: survey } });';
+          break;
+      };
       return result;
     });
     this.survey = creator.createSurvey(json, "embed");
