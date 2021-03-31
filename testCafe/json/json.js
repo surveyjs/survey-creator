34af9956858941b24423a5e41662ab5a7d28616c
gologames <gologames@bk.ru>
2021-03-31 18:02:41 +0300

    Implemented json tab functional test for #1110

--- testCafe/json/json.js
+++ testCafe/json/json.js
@@ -1,6 +1,5 @@
 import { frameworks, url, initCreator } from "../helper";
-import { ClientFunction } from "testcafe";
-const assert = require("assert");
+import { Selector } from "testcafe";
 const title = `JSON tab`;
 
 const json = {
@@ -18,10 +17,17 @@ const options = {
 
 frameworks.forEach((framework) => {
     fixture`${framework} ${title}`.page`${url}${framework}.html`.beforeEach(
-        async () => { await initCreator(framework, json, options); }
+        async (t) => {
+            await initCreator(framework, json, options);
+            await t.maximizeWindow();
+        }
     );
 
     test(`Change title of text question in JSON tab`, async (t) => {
-        await t.wait(5000);
+        await t.click(Selector(".svc-tabbed-menu-item").withText("JSON Editor"));
+        await t.selectTextAreaContent(Selector(".svc-json-editor-tab__content-area"),
+            8, 15, 8, 24).pressKey("backspace").pressKey("I space a m space c h a n g e d");
+        await t.click(Selector(".svc-tabbed-menu-item").withText("Survey Designer"));
+        await t.expect(Selector("h5").withText("I am changed").exists).ok();
     });
 });
