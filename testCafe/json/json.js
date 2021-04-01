0a347cf783eaec9386a89f777908b01e015e5e77
gologames <gologames@bk.ru>
2021-04-01 15:47:30 +0300

    Added embed tab functional test for #1110

--- testCafe/json/json.js
+++ testCafe/json/json.js
@@ -1,6 +1,6 @@
 import { frameworks, url, initCreator } from "../helper";
 import { Selector } from "testcafe";
-const title = `JSON tab`;
+const title = "JSON tab";
 
 const json = {
     questions: [
@@ -12,18 +12,15 @@ const json = {
     ]
 };
 
-const options = {
-};
-
 frameworks.forEach((framework) => {
     fixture`${framework} ${title}`.page`${url}${framework}.html`.beforeEach(
         async (t) => {
-            await initCreator(framework, json, options);
+            await initCreator(framework, json);
             await t.maximizeWindow();
         }
     );
 
-    test(`Change title of text question in JSON tab`, async (t) => {
+    test("Change title of text question", async (t) => {
         await t.click(Selector(".svc-tabbed-menu-item").withText("JSON Editor"));
         await t.selectTextAreaContent(Selector(".svc-json-editor-tab__content-area"),
             8, 15, 8, 24).pressKey("backspace").pressKey("I space a m space c h a n g e d");
