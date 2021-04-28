13d2707c478e68fd845fe55301fa0cbb80043719
gologames <gologames@bk.ru>
2021-04-28 18:19:34 +0300

    Fixed invisible cursor in string editor for #1155. Returned and skiped drag'n'drop func tests

--- packages/survey-creator-react/src/components/StringEditor.tsx
+++ packages/survey-creator-react/src/components/StringEditor.tsx
@@ -3,9 +3,11 @@ import { LocalizableString } from "survey-core";
 import { ReactElementFactory, SvgIcon } from "survey-react-ui";
 
 export class SurveyLocStringEditor extends React.Component<any, any> {
+  private svStringEditorRef: React.RefObject<HTMLDivElement>;
   constructor(props: any) {
     super(props);
     this.state = { changed: 0 };
+    this.svStringEditorRef = React.createRef();
   }
   private get locStr(): LocalizableString {
     return this.props.locStr;
@@ -27,13 +29,24 @@ export class SurveyLocStringEditor extends React.Component<any, any> {
   onInput = (event: any) => {
     this.locStr.text = event.target.innerText;
   };
-  onClick = (event: any) => {
+  onKeyDown = (event: React.KeyboardEvent<HTMLSpanElement>) => {
+    if(event.keyCode === 13) {
+      this.svStringEditorRef.current.blur();
+      this.done(event);
+    }
+    if(event.keyCode === 27) {
+      this.svStringEditorRef.current.blur();
+      this.done(event);
+    }
+    return true;
+  };
+  done = (event: any) => {
     event.preventDefault();
     event.stopPropagation();
   };
   edit = (event: MouseEvent) => {
-    //this.focusEditor && this.focusEditor();
-    this.onClick(event);
+    this.svStringEditorRef.current.focus();
+    this.done(event);
   };
   render(): JSX.Element {
     if (!this.locStr) {
@@ -44,37 +57,40 @@ export class SurveyLocStringEditor extends React.Component<any, any> {
       const htmlValue = { __html: this.locStr.renderedHtml };
       control = (
         <span
+          ref={this.svStringEditorRef}
           className="sv-string-editor"
           contentEditable="true"
           suppressContentEditableWarning={true}
           style={this.style}
           dangerouslySetInnerHTML={htmlValue}
           onBlur={this.onInput}
-          onClick={this.onClick}
+          onKeyDown={this.onKeyDown}
+          onClick={this.done}
         />
       );
     }
     control = (
       <span
+        ref={this.svStringEditorRef}
         className="sv-string-editor"
         contentEditable="true"
         suppressContentEditableWarning={true}
         style={this.style}
         onBlur={this.onInput}
-        onClick={this.onClick}
+        onClick={this.done}
       >
         {this.locStr.renderedHtml}
       </span>
     );
     return (
-      <span className="svc-string-editor" onClick={this.onClick}>
+      <span className="svc-string-editor" onClick={this.done}>
         <span className="svc-string-editor__content">
           <div className="svc-string-editor__border">
           </div>
           {control}
           <div className="svc-string-editor__controls">
             <SvgIcon className="svc-string-editor__button svc-string-editor__button--edit" onClick={this.edit} size={16} iconName={'icon-pencil'}></SvgIcon>
-            <SvgIcon className="svc-string-editor__button svc-string-editor__button--done" onClick={this.onClick} size={22} iconName={'icon-check'}></SvgIcon>
+            <SvgIcon className="svc-string-editor__button svc-string-editor__button--done" onClick={this.done} size={22} iconName={'icon-check'}></SvgIcon>
           </div>
         </span>
       </span>
