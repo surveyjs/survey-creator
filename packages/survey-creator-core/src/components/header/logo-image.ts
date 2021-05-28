4fb6c730e8d112065042e7ef2f7abf0cf5ab1b1c
gologames <gologames@bk.ru>
2021-05-28 16:08:59 +0300

    Added change and remove image controls for logo image for #1168

--- packages/survey-creator-core/src/components/header/logo-image.ts
+++ packages/survey-creator-core/src/components/header/logo-image.ts
@@ -6,7 +6,10 @@ export class LogoImageViewModel extends Base {
   @property() isLogoImageChoosen: boolean;
   constructor(private creator: CreatorBase<SurveyModel>, private root: HTMLDivElement) {
     super();
-    this.isLogoImageChoosen = !!creator.survey.logo;
+    this.updateIsLogoImageChoosen();
+  }
+  private updateIsLogoImageChoosen(): void {
+    this.isLogoImageChoosen = !!this.creator.survey.logo;
   }
   public chooseFile() {
     const fileInput: HTMLInputElement =
@@ -14,8 +17,12 @@ export class LogoImageViewModel extends Base {
     this.creator.chooseFiles(fileInput, (files: File[]) => {
       this.creator.uploadFiles(files, (_, link) => {
         this.creator.survey.logo = link;
-        this.isLogoImageChoosen = !!this.creator.survey.logo;
+        this.updateIsLogoImageChoosen();
       });
     });
+  }
+  public remove() {
+    this.creator.survey.logo = "";
+    this.updateIsLogoImageChoosen();
   }
 }
