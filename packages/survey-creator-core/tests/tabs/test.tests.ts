b2207fc75a6bbf29c9575f93b2939d3633585875
gologames <gologames@bk.ru>
2021-07-01 19:23:57 +0300

    Implemented click to next visible page in test tab page selector for #1438

--- packages/survey-creator-core/tests/tabs/test.tests.ts
+++ packages/survey-creator-core/tests/tabs/test.tests.ts
@@ -1,17 +1,15 @@
 import { CreatorTester } from "../creator-tester";
-import {
-  TabTestPlugin,
-  TestSurveyTabViewModel
-} from "../../src/components/tabs/test";
+import { TabTestPlugin, TestSurveyTabViewModel } from "../../src/components/tabs/test";
+import { IActionBarItem, ListModel } from "survey-core";
 
 function getTestModel(creator: CreatorTester): TestSurveyTabViewModel {
-  var testPlugin = <TabTestPlugin>creator.getPlugin("test");
+  const testPlugin: TabTestPlugin = <TabTestPlugin>creator.getPlugin("test");
   testPlugin.activate();
   return testPlugin.model;
 }
 
 test("Test language Bar Item", (): any => {
-  var creator = new CreatorTester();
+  const creator: CreatorTester = new CreatorTester();
   creator.JSON = {
     questions: [
       {
@@ -21,15 +19,15 @@ test("Test language Bar Item", (): any => {
       }
     ]
   };
-  var model = getTestModel(creator);
+  const model: TestSurveyTabViewModel = getTestModel(creator);
   expect(model.showDefaultLanguageInTestSurveyTab).toBeTruthy();
   expect(model.languages).toHaveLength(2);
   expect(model.languages[0].id).toEqual("en");
   expect(model.languages[1].id).toEqual("de");
   expect(model.languages[1].title).toEqual("deutsch");
   expect(model.activeLanguage).toEqual("en");
-  var langAction;
-  for (var i = 0; i < model.actions.length; i++) {
+  let langAction : any;
+  for (let i: number = 0; i < model.actions.length; i++) {
     if (model.actions[i].id == "languageSelector") {
       langAction = model.actions[i];
       break;
@@ -40,4 +38,62 @@ test("Test language Bar Item", (): any => {
   model.activeLanguage = "de";
   expect(model.survey.locale).toEqual("de");
   expect(langAction.title()).toEqual("deutsch");
+});
+
+test("Check page list state after change page arrows click", (): any => {
+  const creator: CreatorTester = new CreatorTester();
+  creator.JSON = {
+    pages: [
+     {
+      name: "page1",
+      elements: [
+       {
+        type: "text",
+        name: "question1",
+        visibleIf: "{question2} notempty"
+       }
+      ]
+     },
+     {
+      name: "page2",
+      elements: [
+       {
+        type: "text",
+        name: "question2"
+       }
+      ]
+     },
+     {
+      name: "page3",
+      elements: [
+       {
+        type: "text",
+        name: "question3",
+        visibleIf: "{question2} notempty"
+       }
+      ]
+     },
+     {
+      name: "page4",
+      elements: [
+       {
+        type: "text",
+        name: "question4"
+       }
+      ]
+     }
+    ]
+   };
+  const model: TestSurveyTabViewModel = getTestModel(creator);
+  const pageList: ListModel = model.actions.filter((item: IActionBarItem) =>
+    item.id === "pageSelector")[0].popupModel.contentComponentData.model;
+  expect(pageList.selectedItem.data).toEqual(model.activePage);
+  const nextPage: IActionBarItem =
+    model.actions.filter((item: IActionBarItem) => item.id === "nextPage")[0];
+  nextPage.action();
+  expect(pageList.selectedItem.data).toEqual(model.activePage);
+  const prevPage: IActionBarItem =
+    model.actions.filter((item: IActionBarItem) => item.id === "prevPage")[0];
+  prevPage.action();
+  expect(pageList.selectedItem.data).toEqual(model.activePage);
 });
