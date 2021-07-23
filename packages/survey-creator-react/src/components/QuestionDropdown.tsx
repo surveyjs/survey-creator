b0d26dae56d89fd954f7ce56538f1006a350a5bd
Andrew Telnov <andrew.telnov@gmail.com>
2021-07-23 17:08:12 +0300

    Refactor react adorners and implement allowDragging, #1550

--- packages/survey-creator-react/src/components/QuestionDropdown.tsx
+++ packages/survey-creator-react/src/components/QuestionDropdown.tsx
@@ -1,91 +1,42 @@
-import { QuestionAdornerViewModel, toggleHovered } from "@survey/creator";
 import React from "react";
-import { ReactDragEvent, ReactMouseEvent } from "../events";
-import { Base, ItemValue } from "survey-core";
+import { ItemValue } from "survey-core";
+import { ReactElementFactory } from "survey-react-ui";
 import {
-  SurveyActionBar,
-  ReactElementFactory,
-  SurveyElementBase,
-  SurveyQuestion,
-  SurveyQuestionRadioItem
-} from "survey-react-ui";
-import { QuestionAdornerComponentProps } from "./Question";
+  QuestionAdornerComponent,
+  QuestionAdornerComponentProps
+} from "./Question";
 
-export class QuestionDropdownAdornerComponent extends SurveyElementBase<
-  QuestionAdornerComponentProps,
-  any
-> {
-  model: QuestionAdornerViewModel;
-  constructor(props: QuestionAdornerComponentProps) {
-    super(props);
-    this.model = new QuestionAdornerViewModel(
-      this.props.componentData,
-      this.props.question,
-      null
-    );
-  }
-  protected getStateElement(): Base {
-    return this.model;
-  }
-
-  render(): JSX.Element {
-    if (this.model.isDragged) {
-      return null;
-    }
+export class QuestionDropdownAdornerComponent extends QuestionAdornerComponent {
+  renderElementPlaceholder(): JSX.Element {
     const question = this.props.question;
     return (
-      <React.Fragment>
-        <div
-          data-svc-drop-target-element-name={this.model.surveyElement.name}
-          className={"svc-question__adorner"}
-          onMouseOut={e => toggleHovered(e.nativeEvent, e.currentTarget)}
-          onMouseOver={e => toggleHovered(e.nativeEvent, e.currentTarget)}
-        >
-          <div
-            className={"svc-question__content " + this.model.css()}
-            onClick={(e) => this.model.select(this.model, new ReactMouseEvent(e))}
-          >
-            <div className={"svc-question__drag-area"}>
-                <div
-                  className={"svc-question__drag-element"}
-                  onPointerDown={(event:any) => this.model.startDragSurveyElement(event)}
-                ></div>
+      <div className="svc-question__dropdown-choices">
+        {(question.visibleChoices || []).map(
+          (item: ItemValue, index: number) => (
+            <div
+              className="svc-question__dropdown-choice"
+              key={`editable_choice_${index}`}
+            >
+              {question.survey["wrapItemValue"](
+                ReactElementFactory.Instance.createElement(
+                  "survey-radiogroup-item",
+                  {
+                    question: question,
+                    cssClasses: question.cssClasses,
+                    isDisplayMode: true,
+                    item: item,
+                    textStyle: question.textStyle,
+                    index: index,
+                    isChecked: question.value === item.value
+                  }
+                ),
+                question,
+                item
+              )}
             </div>
-
-            {this.props.element}
-
-            <div className="svc-question__dropdown-choices">
-              {(question.visibleChoices || []).map((item: ItemValue, index: number) => (
-                <div
-                  className="svc-question__dropdown-choice"
-                  key={`editable_choice_${index}`}
-                >
-                  {question.survey["wrapItemValue"](
-                    ReactElementFactory.Instance.createElement(
-                      "survey-radiogroup-item",
-                      {
-                        question: question,
-                        cssClasses: question.cssClasses,
-                        isDisplayMode: true,
-                        item: item,
-                        textStyle: question.textStyle,
-                        index: index,
-                        isChecked: question.value === item.value
-                      }
-                    ),
-                    question,
-                    item
-                  )}
-                </div>
-              ))}
-            </div>
-
-            <div className="svc-question__content-actions">
-              <SurveyActionBar model={this.model.actionContainer}></SurveyActionBar>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
+          )
+        )}
+      </div>
     );
   }
 }
--- packages/survey-creator-react/src/components/QuestionRating.tsx
+++ packages/survey-creator-react/src/components/QuestionRating.tsx
@@ -1,89 +1,51 @@
-import { QuestionAdornerViewModel, QuestionImageAdornerViewModel, QuestionRatingAdornerViewModel, toggleHovered } from "@survey/creator";
-import React from "react";
-import { ReactDragEvent, ReactMouseEvent } from "../events";
-import { Base, Question, QuestionImageModel, QuestionRatingModel, SurveyElement } from "survey-core";
 import {
-  SurveyActionBar,
-  ReactElementFactory,
-  SurveyElementBase,
-  SurveyQuestion,
-  SvgIcon,
-} from "survey-react-ui";
+  QuestionAdornerViewModel,
+  QuestionRatingAdornerViewModel
+} from "@survey/creator";
+import React from "react";
+import {
+  QuestionAdornerComponent,
+  QuestionAdornerComponentProps
+} from "./Question";
+import { ReactElementFactory, SvgIcon } from "survey-react-ui";
 
-export interface QuestionRatingAdornerComponentProps {
-  element: JSX.Element;
-  question: QuestionRatingModel;
-  componentData: any;
-}
-
-export class QuestionRatingAdornerComponent extends SurveyElementBase<
-QuestionRatingAdornerComponentProps,
-  any
-> {
-  model: QuestionRatingAdornerViewModel;
-
-  constructor(props: QuestionRatingAdornerComponentProps) {
-    super(props);
-    this.model = new QuestionRatingAdornerViewModel(
+export class QuestionRatingAdornerComponent extends QuestionAdornerComponent {
+  protected createQuestionViewModel(): QuestionAdornerViewModel {
+    return new QuestionRatingAdornerViewModel(
       this.props.componentData,
       this.props.question as any,
       null
     );
   }
-
-  protected getStateElement(): Base {
-    return this.model;
+  public get ratingModel(): QuestionRatingAdornerViewModel {
+    return this.model as QuestionRatingAdornerViewModel;
   }
 
-  render(): JSX.Element {
-    if (this.model.isDragged) {
-      return null;
-    }
-
+  renderElementPlaceholder(): JSX.Element {
+    const question = this.props.question;
+    const model = this.ratingModel;
     return (
-      <React.Fragment>
-        <div
-          data-svc-drop-target-element-name={this.model.surveyElement.name}
-          className={"svc-question__adorner"}
-          onMouseOut={e => toggleHovered(e.nativeEvent, e.currentTarget)}
-          onMouseOver={e => toggleHovered(e.nativeEvent, e.currentTarget)}
+      <div className="svc-rating-question-controls svc-item-value-controls">
+        <span
+          className="svc-item-value-controls__button svc-item-value-controls__remove"
+          onClick={() => model.removeItem(model)}
         >
-          <div
-            className={"svc-question__content " + this.model.css()}
-            onClick={(e) => this.model.select(this.model, new ReactMouseEvent(e))}
-          >
-
-            <div className={"svc-question__drag-area"}>
-                <div
-                  className={"svc-question__drag-element"}
-                  onPointerDown={(event:any) => this.model.startDragSurveyElement(event)}
-                ></div>
-            </div>
-
-            {this.props.element}
-
-            <div className="svc-rating-question-controls svc-item-value-controls">
-              <span className="svc-item-value-controls__button svc-item-value-controls__remove" onClick={() => this.model.removeItem(this.model)}>
-                <SvgIcon size={16} iconName={'icon-remove-item-value'}></SvgIcon>
-              </span>
-              <span className="svc-item-value-controls__button svc-item-value-controls__add" onClick={() => this.model.addItem(this.model)}>
-                <SvgIcon size={16} iconName={'icon-add-item-value'}></SvgIcon>
-              </span>
-            </div>
-
-            <div className="svc-question__content-actions">
-              <SurveyActionBar model={this.model.actionContainer}></SurveyActionBar>
-            </div>
-          </div>
-        </div>
-      </React.Fragment >
+          <SvgIcon size={16} iconName={"icon-remove-item-value"}></SvgIcon>
+        </span>
+        <span
+          className="svc-item-value-controls__button svc-item-value-controls__add"
+          onClick={() => model.addItem(model)}
+        >
+          <SvgIcon size={16} iconName={"icon-add-item-value"}></SvgIcon>
+        </span>
+      </div>
     );
   }
 }
 
 ReactElementFactory.Instance.registerElement(
   "svc-rating-question",
-  (props: QuestionRatingAdornerComponentProps) => {
+  (props: QuestionAdornerComponentProps) => {
     return React.createElement(QuestionRatingAdornerComponent, props);
   }
 );
