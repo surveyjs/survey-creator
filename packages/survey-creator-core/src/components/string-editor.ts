81458c28d94ae473877df13c69379376144b6bd9
T_S_V <tsv2013@noreply.github.com>
2021-08-09 13:22:31 +0300

    Fixed #1608 - V2: inplace string editor doesn't respect maxLength attribute

--- packages/survey-creator-core/src/components/string-editor.ts
+++ packages/survey-creator-core/src/components/string-editor.ts
@@ -5,12 +5,29 @@ export class StringEditorViewModelBase extends Base {
   constructor(private locString: LocalizableString) {
     super();
   }
-  public get placeholder(): string {
-    if (!(<any>this.locString.owner).getType) return "";
+  public checkConstraints(event: any) {
+    if(this.maxLength > 0 && event.keyCode >= 32) {
+      var text: string = (event.target as any).innerText || "";
+      if(text.length >= this.maxLength) {
+        event.preventDefault();
+      }
+    }
+  }
+  public findProperty() {
+    if (!(<any>this.locString.owner).getType) return undefined;
     const ownerType: string = (<any>this.locString.owner).getType();
-    if (!this.locString.name) return "";
+    if (!this.locString.name) return undefined;
     const property: JsonObjectProperty = Serializer.findProperty(ownerType, this.locString.name);
-    if (!property.placeholder) return "";
+    return property;
+  }
+  public get maxLength(): number {
+    const property: JsonObjectProperty = this.findProperty();
+    if (!property || property.maxLength <= 0) return -1;
+    return property.maxLength;
+  }
+  public get placeholder(): string {
+    const property: JsonObjectProperty = this.findProperty();
+    if (!property || !property.placeholder) return "";
     return editorLocalization.getString(property.placeholder);
   }
 }
