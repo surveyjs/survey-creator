b66831ee0f143ec54f0801d308291d67254f5e65
OlgaLarina <olga.larina@devexpress.com>
2021-09-27 20:34:34 +0300

    fix #1871 (#1879)
    Co-authored-by: Olga Larina <olga.larina.dev@gmail.com>

--- packages/survey-creator-react/src/components/tabs/Logic.tsx
+++ packages/survey-creator-react/src/components/tabs/Logic.tsx
@@ -1,7 +1,35 @@
 import React, { Fragment } from "react";
-import { Base } from "survey-core";
+import { Action, Base } from "survey-core";
 import { ReactElementFactory, SurveyElementBase, Survey, attachKey2click } from "survey-react-ui";
 import { SurveyLogicUI } from "@survey/creator";
+
+export class TabLogicAddButtonComponent extends SurveyElementBase<any, any> {
+  private get model(): Action {
+    return this.props.button;
+  }
+  protected getStateElement(): Base {
+    return this.model;
+  }
+
+  renderElement(): JSX.Element {
+    const buttonClassName = "svc-logic-tab__content-action" + ((this.model.enabled !== undefined && !this.model.enabled) ? " svc-logic-tab__content-action--disabled" : "");
+    return attachKey2click(<div
+      onClick={(e) => {
+        e.stopPropagation();
+        this.model.action();
+      }}
+      className={buttonClassName}
+      title={this.model.title}
+    >
+      <span className="svc-text svc-text--normal svc-text--bold">
+        {this.model.title}
+      </span>
+    </div>);
+  }
+}
+ReactElementFactory.Instance.registerElement("svc-tab-logic-add-button", (props) => {
+  return React.createElement(TabLogicAddButtonComponent, props);
+});
 
 export class TabLogicComponent extends SurveyElementBase<any, any> {
   private get model(): SurveyLogicUI {
@@ -19,6 +47,7 @@ export class TabLogicComponent extends SurveyElementBase<any, any> {
   }
   private renderViewContent(): JSX.Element {
     const logicTabClassName = "svc-plugin-tab__content svc-logic-tab__content " + (this.model.hasItems ? "" : "svc-logic-tab__empty");
+    const buttonClassName = "svc-logic-tab__content-action" + ((this.model.addNewButton.enabled !== undefined && !this.model.addNewButton.enabled) ? " svc-logic-tab__content-action--disabled" : "");
     return (
       <Fragment>
         <div className={logicTabClassName}>
@@ -27,20 +56,7 @@ export class TabLogicComponent extends SurveyElementBase<any, any> {
             <div role="button" className="svc-logic-tab__content-empty" >
               <span className="svc-text">{this.model.emptyTabPlaceHolder}</span>
             </div>)}
-
-          {attachKey2click(<div
-            onClick={(e) => {
-              e.stopPropagation();
-              this.model.addNewButton.action();
-            }}
-            className="svc-logic-tab__content-action"
-            title={this.model.addNewButton.title}
-          >
-            <span className="svc-text svc-text--normal svc-text--bold">
-              {this.model.addNewButton.title}
-            </span>
-          </div>)}
-
+          <TabLogicAddButtonComponent button={this.model.addNewButton} />
         </div>
       </Fragment>
     );
