7ad6ad714f2658031e6cda20aad473794ac32a7c
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2021-09-27 16:04:44 +0300

    added func test (property grid editing choices )

---
+++ testCafe/property-grid/choices.js
@@ -0,0 +1,48 @@
+import {
+  url, setJSON, getJSON
+} from "../helper";
+import { ClientFunction, Selector } from "testcafe";
+const title = "Property Grid";
+
+
+
+fixture`${title}`.page`${url}`.beforeEach(async (t) => {
+  await t.maximizeWindow();
+});
+
+test("Rename choice", async (t) => {
+  const json = {
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [
+          {
+            "type": "checkbox",
+            "name": "question1",
+            "title": "question1",
+            "choices": [
+              "item1",
+              "item2",
+              "item3"
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  await setJSON(json);
+
+  const expectedNewChoiceName = "newItem1";
+  const question1 = Selector(`[name="question1"]`);
+  const choicesTab = Selector("h4").withExactText("Choices");
+  const item1PGEditorInput = Selector(
+    `[name="choices"] [data-sv-drop-target-matrix-row]`
+  ).nth(0).find(`td`).nth(1).find(`input`);
+
+  await t.click(question1).click(choicesTab);
+
+  await t.typeText(item1PGEditorInput, expectedNewChoiceName, { replace: true }).pressKey('enter');
+
+  const resultJson = await getJSON();
+  await t.expect(resultJson.pages[0].elements[0].choices[0]).eql(expectedNewChoiceName);
+});
