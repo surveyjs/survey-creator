bc324d77c40195061cee1cd0bf49c118a76db939
Yaroslav K <steep1580@yandex.ru>
2021-10-12 13:01:55 +0300

    Work for #1937 - added tests

--- testCafe/json/json.ts
+++ testCafe/json/json.ts
@@ -29,3 +29,41 @@ test("Change title of text question", async (t) => {
     .click(getTabbedMenuItemByText("Survey Designer"))
     .expect(Selector("h5").withText("I am changed").exists).ok();
 });
+test("Check show/hide errors button visibility", async (t) => {
+  await setJSON(json);
+  await t
+    .click(getTabbedMenuItemByText("JSON Editor"))
+    .expect(Selector("svc-json-editor-tab__errros_button").exists).notOk();
+
+});
+test("Check show/hide errors button functionality", async (t) => {
+  await setJSON(json);
+  await t
+    .click(getTabbedMenuItemByText("JSON Editor"))
+    .selectTextAreaContent(Selector(".svc-json-editor-tab__content-area"))
+    .pressKey("backspace")
+    .pressKey("I space a m space e r r o r space s t r i n g )")
+
+    .expect(Selector(".svc-json-editor-tab__errros_button").withText("Show errors").exists).ok()
+    .expect(Selector(".svc-json-editor-tab__content-errors").exists).notOk()
+
+    .click(Selector(".svc-json-editor-tab__errros_button"))
+    .expect(Selector(".svc-json-editor-tab__errros_button").withText("Hide errors").exists).ok()
+    .expect(Selector(".svc-json-editor-tab__content-errors").exists).ok()
+    .expect(Selector(".svc-json-editor-tab__content-errors > span:last-child")
+              .withText("Expected 'n' instead of ' '").exists).ok()
+
+    .click(Selector(".svc-json-editor-tab__errros_button"))
+    .expect(Selector(".svc-json-editor-tab__errros_button").withText("Show errors").exists).ok();
+});
+test("Check deactivating other tabs when json has errros", async (t) => {
+  await setJSON(json);
+  await t
+    .click(getTabbedMenuItemByText("JSON Editor"))
+    .selectTextAreaContent(Selector(".svc-json-editor-tab__content-area"))
+    .pressKey("backspace")
+    .pressKey("I space a m space e r r o r space s t r i n g )")
+
+    .click(getTabbedMenuItemByText("Survey Designer"))
+    .expect(Selector("h5").withText("Change me").exists).notOk();
+});
