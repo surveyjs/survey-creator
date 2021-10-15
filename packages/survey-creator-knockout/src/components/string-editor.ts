90a27566aa6145ea5f28aaa975d780d227b5d30e
Andrew <andrew.telnov@gmail.com>
2021-10-15 14:58:51 +0300

    V2: Knockout, column title is empty on adding a new column fixed #2021 (#2064)

--- packages/survey-creator-knockout/src/components/string-editor.ts
+++ packages/survey-creator-knockout/src/components/string-editor.ts
@@ -18,6 +18,7 @@ export class StringEditorViewModel {
       this.getEditorElement(element).focus();
     };
     this.baseModel.blurEditor = () => this.getEditorElement(element).blur();
+    locString.strChanged();
   }
 
   public setLocString(locString: LocalizableString): LocalizableString {
