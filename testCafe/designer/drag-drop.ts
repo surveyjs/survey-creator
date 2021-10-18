f549c56957623f8bcd43079e68248aa03ff987e8
Dmitry Kurmanov <kurmanov.work@gmail.com>
2021-10-18 17:48:25 +0300

    Bug/1936 imagepicker (#2066)
    * #1936 fix layout
    
    * fixed #1936
    
    * work for the #1936
    
    * work for the #1936
    
    Co-authored-by: novikov82 <novikov82@gmail.com>

--- testCafe/designer/drag-drop.ts
+++ testCafe/designer/drag-drop.ts
@@ -38,7 +38,7 @@ test("Drag Drop Toolbox Item and Empty Page", async (t) => {
 });
 
 test("Drag Drop Question", async (t) => {
-  await t.resizeWindow(2560, 1440); //TODO why do we need it?
+  await t.resizeWindow(2560, 1440);
   const json = {
     pages: [
       {
@@ -89,7 +89,7 @@ test("Drag Drop Question", async (t) => {
   await t.expect(name).eql(questionName);
 });
 test("Drag Drop to Panel", async (t) => {
-  await t.resizeWindow(2560, 1440); //TODO why do we need it?
+  await t.resizeWindow(2560, 1440);
   const json = {
     pages: [
       {
@@ -373,10 +373,10 @@ test("Animation (choices)", async (t) => {
   const animationClassesCount = await ClientFunction(() => {
     let result = 0;
     const itemValueNodes = document.querySelector("[name='question1']").querySelectorAll(".svc-item-value-wrapper");
-    itemValueNodes.forEach(itemValueNode=>{
-      if (itemValueNode.classList.contains("svc-item-value--movedown") || 
+    itemValueNodes.forEach(itemValueNode => {
+      if (itemValueNode.classList.contains("svc-item-value--movedown") ||
         itemValueNode.classList.contains("svc-item-value--moveup")) {
-          result++;
+        result++;
       }
     });
     return result;
@@ -425,14 +425,13 @@ test("Drag Drop ImagePicker (choices)", async (t) => {
   const PandaItem = Selector("[data-sv-drop-target-item-value=\"panda\"]");
   const CamelItem = Selector("[data-sv-drop-target-item-value=\"camel\"]");
 
-  //TODO change after the https://github.com/surveyjs/survey-creator/issues/1936
-  const DragZoneLionItem = GiraffeItem; /*GiraffeItem.find(".svc-item-value-controls__drag");*/
+  const DragZoneGiraffeItem = GiraffeItem.find(".svc-image-item__drag-element");
 
   await t
     .click(Question1, { speed: 0.5 })
-    .hover(PandaItem).hover(LionItem).hover(CamelItem).hover(GiraffeItem).hover(DragZoneLionItem)
+    .hover(PandaItem).hover(LionItem).hover(CamelItem).hover(GiraffeItem).hover(DragZoneGiraffeItem)
 
-    .dragToElement(DragZoneLionItem, LionItem, {
+    .dragToElement(DragZoneGiraffeItem, LionItem, {
       offsetX: 5,
       offsetY: 5,
       destinationOffsetY: -40,
@@ -443,7 +442,9 @@ test("Drag Drop ImagePicker (choices)", async (t) => {
   const expectedValue = "giraffe";
   await t.expect(value).eql(expectedValue);
 
-  await t.dragToElement(DragZoneLionItem, PandaItem, {
+  await t.click(Question1, { speed: 0.5 }).hover(GiraffeItem, { speed: 0.5 });
+
+  await t.dragToElement(DragZoneGiraffeItem, PandaItem, {
     offsetX: 5,
     offsetY: 5,
     destinationOffsetY: 30,
@@ -473,7 +474,6 @@ test("Drag Drop MatrixRows (property grid)", async (t) => {
   const Question1 = Selector("[name=\"question1\"]");
   await t.click(Question1, { speed: 0.5 });
 
-  // TODO uncomment after the fix https://github.com/surveyjs/survey-creator/issues/1618
   const ChoicesTab = Selector("h4").withExactText("Choices");
   await t.click(ChoicesTab);
 
@@ -565,7 +565,7 @@ test("Drag Drop Pages MatrixRows (property grid Pages)", async (t) => {
 });
 
 test("Drag Drop to Panel Dynamic Question", async (t) => {
-  await t.resizeWindow(2560, 1440); //TODO why do we need it?
+  await t.resizeWindow(2560, 1440);
   const json = {
     pages: [
       {
@@ -651,7 +651,7 @@ test("Drag Drop to Panel Dynamic Question", async (t) => {
 });
 
 test("Drag Drop from Panel Dynamic Question", async (t) => {
-  await t.resizeWindow(2560, 1440); //TODO why do we need it?
+  await t.resizeWindow(2560, 1440);
   const json = {
     pages: [
       {
