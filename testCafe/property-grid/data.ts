6048f297b5291fa92e390e751ff3c060df237750
novikov82 <novikov82@gmail.com>
2021-10-26 09:46:30 +0300

    #1775 add f-test

---
+++ testCafe/property-grid/data.ts
@@ -0,0 +1,50 @@
+import { url, setJSON, getJSON } from "../helper";
+import { Selector } from "testcafe";
+const title = "Property Grid";
+
+fixture`${title}`.page`${url}`.beforeEach(async (t) => {
+  await t.maximizeWindow();
+});
+
+test("Default value", async (t) => {
+  const json = {
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [
+          {
+            "type": "checkbox",
+            "name": "question1",
+            "title": "question1",
+            "choices": [
+              "item1",
+              "item2",
+              "item3"
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  await setJSON(json);
+
+  const question1 = Selector("[name=\"question1\"]");
+  const dataTab = Selector("h4").withExactText("Data");
+  const item1PGEditorInput = Selector("[name=\"choices\"] [data-sv-drop-target-matrix-row]").nth(0).find("td").nth(1).find("input");
+
+  await t
+    .click(question1)
+    .click(dataTab)
+    .click(Selector("a").withExactText("Set Default value"))
+    .click(Selector(".sv-popup--modal span").withExactText("item1"))
+    .click(Selector(".sv-popup--modal button").withExactText("Apply"))
+    .expect(Selector("a").withExactText("Change Default value").visible).ok();
+  const resultJson = await getJSON();
+  await t
+    .expect(resultJson.pages[0].elements[0].defaultValue).eql(["item1"])
+    .click(Selector("a").withExactText("Clear"))
+    .expect(Selector("a").withExactText("Set Default value").visible).ok();
+  const resultJson2 = await getJSON();
+  await t
+    .expect(resultJson2.pages[0].elements[0].defaultValue).eql(undefined);
+});
