810c6fcf8fa468664962729c6dcd3ec162dc8e56
novikov82 <novikov82@gmail.com>
2021-11-26 15:39:25 +0300

    #2258 - remove object selector dropdown icon

--- packages/survey-creator-core/src/property-grid/property-grid-view-model.ts
+++ packages/survey-creator-core/src/property-grid/property-grid-view-model.ts
@@ -123,7 +123,6 @@ export class PropertyGridViewModel<T extends SurveyModel> extends Base {
       id: "svd-grid-object-selector",
       title: this.selectedElementName,
       css: "sv-action--last sv-action-bar-item--secondary",
-      iconName: "icon-more",
       component: "sv-action-bar-item-dropdown",
       action: () => {
         this.selectorPopupModel.displayMode = this.creator.isMobileView ? "overlay" : "popup";
