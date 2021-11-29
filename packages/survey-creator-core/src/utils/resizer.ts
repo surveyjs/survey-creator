d35246b0f8d98198a8b6f28594eef580d20e2c79
OlgaLarina <olga.larina@devexpress.com>
2021-11-29 16:38:12 +0300

    resolve #2235 : improve ResizeManager, add ability to resize content on the right side

--- packages/survey-creator-core/src/utils/resizer.ts
+++ packages/survey-creator-core/src/utils/resizer.ts
@@ -3,20 +3,36 @@ export class ResizeManager {
   private originalWidth: number;
   private minWidth: number;
   private maxWidth: number;
-  private resizer: HTMLElement;
+  private westResizer: HTMLElement;
+  private eastResizer: HTMLElement;
+  private currentResizer: HTMLElement;
   private mouseDevice: "mouse" | "pointer" | "touch";
   private events = {
     "mousedown": { "touch": "touchend", "mouse": "mousedown", "pointer": "pointerdown" },
     "mouseup": { "touch": "touchstart", "mouse": "mouseup", "pointer": "pointerup" },
     "mousemove": { "touch": "touchmove", "mouse": "mousemove", "pointer": "pointermove" },
   }
-  constructor(private container: HTMLElement) {
+
+  constructor(private container: HTMLElement, handles: string) {
     this.init(container);
+    this.setHandles(handles);
+  }
+  public setHandles(newVal: string) {
+    let isThereWestResizer = false;
+    let isThereEastResizer = false;
+    let inputVal = newVal.split(",");
+    inputVal.forEach(str => str.trim());
+    inputVal = inputVal.filter(str => !!str);
+    (inputVal || []).forEach(handrail => {
+      if (handrail == "w") isThereWestResizer = true;
+      if (handrail == "e") isThereEastResizer = true;
+    })
+
+    this.westResizer.style["display"] = isThereWestResizer ? "block" : "none";
+    this.eastResizer.style["display"] = isThereEastResizer ? "block" : "none";
   }
   private getStyleValue(element: HTMLElement, style: string): number {
-    return parseFloat(
-      window.getComputedStyle(element).getPropertyValue(style).replace("px", "")
-    );
+    return parseFloat(window.getComputedStyle(element).getPropertyValue(style).replace("px", ""));
   }
   private isEventSupported(eventName: string) {
     let el = document.createElement("div");
@@ -50,11 +66,18 @@ export class ResizeManager {
     e.preventDefault();
     this.originalWidth = this.getStyleValue(this.container, "width");
     this.originalMouseX = e.pageX;
+    this.currentResizer = e.target;
     window.addEventListener(this.getMouseEvent("mousemove"), this.resize);
     window.addEventListener(this.getMouseEvent("mouseup"), this.stopResize);
   };
   private resize = (e: any) => {
-    let width = this.originalWidth - (e.pageX - this.originalMouseX);
+    let width = this.originalWidth;
+    const horizontalDistinction = e.pageX - this.originalMouseX;
+    if (this.currentResizer == this.westResizer) {
+      width -= horizontalDistinction;
+    } else {
+      width += horizontalDistinction;
+    }
     if (width > this.maxWidth) {
       width = this.maxWidth;
     } else if (width < this.minWidth) {
@@ -67,16 +90,27 @@ export class ResizeManager {
     window.removeEventListener(this.getMouseEvent("mouseup"), this.stopResize);
   };
   private init(container: HTMLElement) {
-    const resizer = (this.resizer = document.createElement("div"));
-    resizer.className = "svc-resizer";
+    this.westResizer = this.createrResizerElement("svc-resizer svc-resizer-west");
+    this.eastResizer = this.createrResizerElement("svc-resizer svc-resizer-east");
     this.minWidth = this.getStyleValue(container, "min-width");
     this.maxWidth = this.getStyleValue(container, "max-width");
-    container.insertBefore(resizer, container.children[0]);
-    resizer.addEventListener(this.getMouseEvent("mousedown"), this.onMouseDownListener);
+    container.insertBefore(this.westResizer, container.children[0]);
+    container.appendChild(this.eastResizer);
   }
+  private createrResizerElement(className: string) {
+    const resizer = document.createElement("div");
+    resizer.className = className;
+    resizer.addEventListener(this.getMouseEvent("mousedown"), this.onMouseDownListener);
+    return resizer;
+  }
+
   dispose() {
-    this.resizer.removeEventListener(this.getMouseEvent("mousedown"), this.onMouseDownListener);
-    this.resizer.remove();
-    this.resizer = undefined;
+    this.eastResizer.removeEventListener(this.getMouseEvent("mousedown"), this.onMouseDownListener);
+    this.eastResizer.remove();
+    this.eastResizer = undefined;
+
+    this.westResizer.removeEventListener(this.getMouseEvent("mousedown"), this.onMouseDownListener);
+    this.westResizer.remove();
+    this.westResizer = undefined;
   }
 }
