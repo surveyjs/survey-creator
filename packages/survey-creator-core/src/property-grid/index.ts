c447730728615fd239ce86cc1cedb9771ccd8275
Dmitry Kuzin (DevExpress) <dmitry.kuzin@devexpress.com>
2021-12-04 12:56:52 +0300

    Work for #2282: hot fix for logic popup

--- packages/survey-creator-core/src/property-grid/index.ts
+++ packages/survey-creator-core/src/property-grid/index.ts
@@ -1050,7 +1050,9 @@ export abstract class PropertyGridEditor implements IPropertyGridEditor {
       options
     );
     if (!surveyPropertyEditor) return null;
+    if (property.type !== "condition") {
     surveyPropertyEditor.editSurvey.css = defaultV2Css;
+    }
     surveyPropertyEditor.editSurvey.onGetMatrixRowActions.add((_, opt) => { updateMatrixRemoveAction(opt.question, opt.actions, opt.row); });
     if(!settings.showModal) return surveyPropertyEditor;
     settings.showModal(
