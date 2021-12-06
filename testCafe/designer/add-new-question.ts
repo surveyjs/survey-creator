34b84ba66595318ae5eaf1bbccc19b3f39bf21ca
novikov82 <novikov82@gmail.com>
2021-12-06 15:11:55 +0300

    #2318 - question add markup

--- testCafe/designer/add-new-question.ts
+++ testCafe/designer/add-new-question.ts
@@ -11,8 +11,15 @@ fixture`${title}`.page`${url}`.beforeEach(
 test("Add New Question", async t => {
   await t
     .maximizeWindow()
-    .expect(Selector("span").withText("Add Question").visible).ok()
-    .click(Selector("span").withText("Add Question"))
+    .expect(Selector(".svc-page__add-new-question > span").withText("Add Question").visible).ok()
+    .click(Selector(".svc-page__add-new-question > span").withText("Add Question"))
     .expect(Selector("span").withText("question1").visible).ok()
-    .expect(Selector('[class^="svc-question__content svc-question__content--selec"]').visible).ok();
+    .expect(Selector('[class^="svc-question__content svc-question__content--selec"]').visible).ok()
+    .click(Selector(".svc-page__add-new-question > button.svc-page__question-type-selector"))
+    .wait(100)
+    .pressKey("p")
+    .expect(Selector(".sv-popup:not(.sv-popup--overlay) li").withText("Panel").visible).ok()
+    .click(Selector(".sv-popup:not(.sv-popup--overlay) li").withText("Panel"))
+    .click(Selector(".svc-panel__add-new-question > span").withText("Add Question"))
+    .expect(Selector("span").withText("question1").visible).ok();
 });
