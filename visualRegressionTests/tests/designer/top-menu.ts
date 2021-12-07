19289d27eebe3bb3ae079dc7ed7657368bd334ba
Yaroslav K <steep1580@yandex.ru>
2021-12-07 13:07:18 +0300

    Work for #2267 - added template for screenshot tests

--- visualRegressionTests/tests/designer/top-menu.ts
+++ visualRegressionTests/tests/designer/top-menu.ts
@@ -4,80 +4,14 @@ import { createScreenshotsComparer } from "devextreme-screenshot-comparer";
 import { url, screenshotComparerOptions, getSurveyJSFramework } from "../../helper";
 import { changeToolboxLocation } from "../../utils";
 
-const title = "Page Navigator Screenshot";
+const title = "Top Menu Screenshot";
 
 fixture`${title}`.page`${url}`.beforeEach(async (t) => {
 });
 
-const json = {
-  "logoPosition": "right",
-  "pages": [
-    {
-      "name": "page1",
-      "elements": [
-        {
-          "type": "text",
-          "name": "question1"
-        }
-      ]
-    },
-    {
-      "name": "page2",
-      "elements": [
-        {
-          "type": "text",
-          "name": "question2"
-        }
-      ]
-    }
-  ]
-};
+const json = {};
 
-test("On the right side (default)", async (t) => {
+test("Top menu tests", async (t) => {
   await t.resizeWindow(1920, 1080);
 
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  await ClientFunction((json) => { window["creator"].JSON = json; })(json);
-  const pageNavigatorElement = Selector(".svc-tab-designer__page-navigator");
-
-  await t.expect(pageNavigatorElement.visible).ok();
-  await t.wait(1000);
-  await takeScreenshot("page-navigator-right.png", pageNavigatorElement, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-
-  await t
-    .hover(Selector(".svc-page-navigator-item-content:not(.svc-page-navigator-item--selected)"));
-  await t.wait(1000);
-  await takeScreenshot("page-navigator-right-hovered.png", pageNavigatorElement, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-});
-
-test("On the left side", async (t) => {
-  await t.resizeWindow(1920, 1080);
-
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  await ClientFunction((json) => { window["creator"].JSON = json; })(json);
-  const pageNavigatorElement = Selector(".svc-tab-designer__page-navigator");
-  await changeToolboxLocation("right");
-
-  await t.expect(pageNavigatorElement.visible).ok();
-  await t.wait(1000);
-  await takeScreenshot("page-navigator-left.png", pageNavigatorElement, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-
-  await t
-    .hover(Selector(".svc-page-navigator-item-content:not(.svc-page-navigator-item--selected)"));
-  await t.wait(1000);
-  await takeScreenshot("page-navigator-left-hovered.png", pageNavigatorElement, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
 });
