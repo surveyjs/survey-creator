29e21ce947ea016dda10ce9d8236a2438e15bf5a
OlgaLarina <olga.larina@devexpress.com>
2021-12-21 17:24:35 +0300

    fix selector & refactoring

--- visualRegressionTests/tests/designer/pg-data-action-button.ts
+++ visualRegressionTests/tests/designer/pg-data-action-button.ts
@@ -1,7 +1,5 @@
 import { Selector } from "testcafe";
-import { createScreenshotsComparer } from "devextreme-screenshot-comparer";
-
-import { url, screenshotComparerOptions, setJSON } from "../../helper";
+import { url, setJSON, checkElementScreenshot } from "../../helper";
 
 const title = "ValueLink Actions in Data section Screenshot";
 
@@ -26,34 +24,18 @@ const json = {
 test("Check states", async (t) => {
   await t.resizeWindow(1920, 1080);
 
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
   await setJSON(json);
 
   // await t
   //   .click(Selector(".svd-grid-expand"));
-  await t
-    .click(Selector(".svc-question__content"), { offsetX: -10, offsetY: -10 });
+  await t.click(Selector(".svc-question__content"), { offsetX: -10, offsetY: -10 });
   await t.wait(500);
-  await t
-    .click(Selector("h4[aria-label=General]"));
-  await t
-    .click(Selector("h4[aria-label=Data]"));
+  await t.click(Selector("h4[aria-label=General]"));
+  await t.click(Selector("h4[aria-label=Data]"));
 
-  const buttonElement = Selector(".svc-action-button.spg-link-value-button.spg-question-link__set-button");
+  const buttonElement = Selector(".svc-action-button.svc-link-value-button.svc-question-link__set-button");
+  await checkElementScreenshot("action-button-default.png", buttonElement, t);
 
-  await t.expect(buttonElement.visible).ok();
-  await t.wait(1000);
-  await takeScreenshot("action-button-default.png", buttonElement, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-
-  await t
-    .hover(buttonElement);
-  await t.wait(1000);
-  await takeScreenshot("action-button-hovered.png", buttonElement, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
+  await t.hover(buttonElement);
+  await checkElementScreenshot("action-button-hovered.png", buttonElement, t);
 });
