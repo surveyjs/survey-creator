7c40fb9999fa59bebaac28817f6bd07829f29d5c
tsv2013 <tsv2013@users.noreply.github.com>
2021-12-21 17:01:26 +0300

    Resolved #2428 - CV2: Introduce a way to override survey element (question) adorners (#2429)
    Co-authored-by: T_S_V <tsv2013@noreply.github.com>

---
+++ packages/survey-creator-react/src/adorners/QuestionHeader.tsx
@@ -0,0 +1,42 @@
+import { QuestionAdornerViewModel, toggleHovered } from "@survey/creator";
+import React from "react";
+import { ReactDragEvent, ReactMouseEvent } from "../events";
+import { Base, Question } from "survey-core";
+import {
+  SurveyActionBar,
+  ReactElementFactory,
+  SurveyElementBase,
+  SurveyQuestion,
+  attachKey2click,
+  SvgIcon,
+  Popup
+} from "survey-react-ui";
+
+export interface QuestionWrapperHeaderProps {
+  className?: string;
+  model: QuestionAdornerViewModel;
+}
+
+export class QuestionWrapperHeader extends React.Component<QuestionWrapperHeaderProps, any> {
+  render(): JSX.Element {
+    if (!this.props.model.allowDragging) return null;
+    return (
+      <div className={"svc-question__drag-area"}
+        onPointerDown={(event: any) =>
+          this.props.model.startDragSurveyElement(event)
+        }
+      >
+        <div
+          className={"svc-question__drag-element"}
+        ></div>
+      </div>
+    );
+  }
+}
+
+ReactElementFactory.Instance.registerElement(
+  "svc-question-header",
+  (props: QuestionWrapperHeaderProps) => {
+    return React.createElement(QuestionWrapperHeader, props);
+  }
+);
