f1a303f47795e22e3c0eefa732aa1fba8e5bf18f
OlgaLarina <olga.larina@devexpress.com>
2021-12-23 12:20:15 +0300

    fix padddings and margins and alignments in logic, add bottom padding on logic tab (#2443)
    * resolve #2345
    
    * work for #2345 : extract QuestionLinkValueModel
    
    * revert
    
    * work for #2345 : fix selector & remove redundant tests
    
    * work for #2345 : extract checkElementScreenshot function & refactoring f-tests
    
    * fix selector & refactoring
    
    * work for #2345 : update etalon
    
    * update etalon
    
    * work for #2345 : add resize window
    
    * work for #2345 : update etalons by new window size
    
    * work for #2345 : rewrite test
    
    * add mask for #2345
    
    * resolved #2337
    
    * work for #2345 : update visual tests
    
    * work for #2337 : update visual tests
    
    * update etalons
    
    * work for #2337 : update etalons
    
    * work for #2337 : remove masks
    
    * add masks
    
    Co-authored-by: Olga Larina <olga.larina.dev@gmail.com>

--- visualRegressionTests/tests/designer/logic-tab.ts
+++ visualRegressionTests/tests/designer/logic-tab.ts
@@ -14,7 +14,45 @@ test("empty view", async (t) => {
   await checkElementScreenshot("logic-tab-empty.png", tabContent, t);
 });
 
+const jsonOneRule = {
+  "logoPosition": "right",
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "text",
+          "name": "q1"
+        },
+        {
+          "type": "text",
+          "name": "q2",
+          "visibleIf": "{q1} = 1"
+        },
+        {
+          "type": "text",
+          "name": "q3"
+        },
+        {
+          "type": "text",
+          "name": "q4"
+        }
+      ]
+    }
+  ]
+};
 const logicDetailButtonElement = Selector(".sl-table__cell--detail-button").filterVisible();
+
+test("one rule view", async (t) => {
+  await t.resizeWindow(1920, 900);
+  await setJSON(jsonOneRule);
+  await t
+    .click(getTabbedMenuItemByText(creatorTabLogicName))
+    .click(logicDetailButtonElement);
+  const tabContent = Selector(".svc-creator-tab__content");
+  await checkElementScreenshot("logic-tab-one-rule-content.png", tabContent, t);
+});
+
 const jsonMultipleConditionsMultipleActions = {
   "logoPosition": "right",
   "pages": [
@@ -43,8 +81,7 @@ const jsonMultipleConditionsMultipleActions = {
     }
   ]
 };
-
-test.skip("rule content", async (t) => {
+test("rule content", async (t) => {
   await t.resizeWindow(1920, 900);
   const ruleContent = Selector(".sl-table__cell--detail-panel");
   await setJSON(jsonMultipleConditionsMultipleActions);
