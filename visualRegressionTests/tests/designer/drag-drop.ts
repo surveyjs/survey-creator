1af00562feaaf38c146f491b14e67b47ec738a9a
Dmitriy Kurmanov <kurmanov.work@gmail.com>
2021-12-30 15:17:17 +0300

    fixed #2312(vrt tests)

--- visualRegressionTests/tests/designer/drag-drop.ts
+++ visualRegressionTests/tests/designer/drag-drop.ts
@@ -1,16 +1,18 @@
 import { Selector, ClientFunction } from "testcafe";
-import { createScreenshotsComparer } from "devextreme-screenshot-comparer";
-
-import { url, screenshotComparerOptions, setJSON } from "../../helper";
+import { url, setJSON, checkElementScreenshot } from "../../helper";
 
 const title = "DragDrop Screenshot";
 
 fixture`${title}`.page`${url}`.beforeEach(async (t) => {
 });
 
+const patchDragDropToDisableDrop = ClientFunction(() => {
+  window["creator"].dragDropSurveyElements.drop = () => { };
+  window["creator"].dragDropChoices.drop = () => { };
+});
+
 test("Ghost Survey Element", async (t) => {
   await t.resizeWindow(2560, 1440);
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
   const RatingToolboxItem = Selector("[aria-label='Rating toolbox item']");
   const EmptyPage = Selector("[data-sv-drop-target-survey-element='page1']");
@@ -25,15 +27,11 @@ test("Ghost Survey Element", async (t) => {
     .hover(RatingToolboxItem)
     .dragToElement(RatingToolboxItem, EmptyPage, { speed: 0.5 });
 
-  await takeScreenshot("drag-drop-survey-element-ghost.png", Selector(".sv-drag-drop-ghost"), screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
+  await checkElementScreenshot("drag-drop-survey-element-ghost.png", Selector(".sv-drag-drop-ghost"), t);
 });
 
 test("Empty Panel Styles", async (t) => {
   await t.resizeWindow(2560, 1440);
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
   const json = {
     pages: [
@@ -50,9 +48,6 @@ test("Empty Panel Styles", async (t) => {
   };
   await setJSON(json);
 
-  const patchDragDropToDisableDrop = ClientFunction(() => {
-    window["creator"].dragDropSurveyElements.drop = () => { };
-  });
   await patchDragDropToDisableDrop();
 
   const Panel = Selector("[data-sv-drop-target-survey-element=\"panel1\"]");
@@ -62,15 +57,11 @@ test("Empty Panel Styles", async (t) => {
     .hover(RatingToolboxItem)
     .dragToElement(RatingToolboxItem, Panel, { speed: 0.5 });
 
-  await takeScreenshot("drag-drop-survey-element-empty-panel.png", Panel, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
+  await checkElementScreenshot("drag-drop-survey-element-empty-panel.png", Panel, t);
 });
 
 test("Empty Panel Dynamic Styles", async (t) => {
   await t.resizeWindow(2560, 1440);
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
   const json = {
     pages: [
@@ -86,10 +77,6 @@ test("Empty Panel Dynamic Styles", async (t) => {
     ]
   };
   await setJSON(json);
-
-  const patchDragDropToDisableDrop = ClientFunction(() => {
-    window["creator"].dragDropSurveyElements.drop = () => { };
-  });
   await patchDragDropToDisableDrop();
 
   const PanelDynamic = Selector("[data-sv-drop-target-survey-element=\"question1\"]");
@@ -99,8 +86,40 @@ test("Empty Panel Dynamic Styles", async (t) => {
     .hover(RatingToolboxItem)
     .dragToElement(RatingToolboxItem, PanelDynamic, { speed: 0.5 });
 
-  await takeScreenshot("drag-drop-survey-element-empty-panel-dynamic.png", PanelDynamic, screenshotComparerOptions);
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
+  await checkElementScreenshot("drag-drop-survey-element-empty-panel-dynamic.png", PanelDynamic, t);
+});
+
+test("Choices: Ranking", async (t) => {
+  await t.resizeWindow(2560, 1440);
+
+  const json = {
+    pages: [
+      {
+        name: "page1",
+        elements: [
+          {
+            "type": "ranking",
+            "name": "question1",
+            "choices": [
+              "item1",
+              "item2"
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  await setJSON(json);
+  await patchDragDropToDisableDrop();
+
+  const QRoot = Selector(".svc-question__adorner").filterVisible();
+  const FirtItem = QRoot.find(".svc-item-value-wrapper").nth(0);
+
+  await checkElementScreenshot("drag-drop-item-values-ranking.png", QRoot, t);
+
+  await t.hover(FirtItem);
+  await checkElementScreenshot("drag-drop-item-values-ranking--hover.png", QRoot, t);
+
+  await t.dragToElement(FirtItem.find(".svc-item-value-controls__drag"), FirtItem);
+  await checkElementScreenshot("drag-drop-item-values-ranking--dragging.png", QRoot, t);
 });
