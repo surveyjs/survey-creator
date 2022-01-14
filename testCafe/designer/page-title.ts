d31d4bbddfb59f83793c8f9dfacb215950e1b8ee
Andrew Telnov <andrew.telnov@gmail.com>
2022-01-14 17:12:26 +0300

    Fix eslint issue

--- testCafe/designer/page-title.ts
+++ testCafe/designer/page-title.ts
@@ -21,7 +21,7 @@ test("Edit page title", async (t) => {
   await setJSON(json);
   const title = "PageTitle";
   await t
-    .click(Selector(`span[aria-placeholder='Page 1']`))
+    .click(Selector("span[aria-placeholder='Page 1']"))
     .pressKey(title.split("").join(" "))
     .pressKey("enter")
     .expect(Selector("textarea[aria-label=Title]").value).eql(title, "Page title in property grid is updated");
