ecb292de07b9b358a632c4bb62a75baa4ba23cfa
Yaroslav K <steep1580@yandex.ru>
2022-01-24 14:08:58 +0300

    Work for #2435 - added unit test

--- visualRegressionTests/tests/designer/complete-survey.ts
+++ visualRegressionTests/tests/designer/complete-survey.ts
@@ -20,23 +20,11 @@ const json = {
             {
               "Column 1": [
                 1
-              ],
-              "Column 2": [
-                2
-              ],
-              "Column 3": [
-                3
               ]
             },
             {
               "Column 1": [
-                4
-              ],
-              "Column 2": [
-                5
-              ],
-              "Column 3": [
-                1
+                2
               ]
             }
           ],
@@ -53,10 +41,7 @@ const json = {
           ],
           "choices": [
             1,
-            2,
-            3,
-            4,
-            5
+            2
           ],
           "cellType": "checkbox"
         }
