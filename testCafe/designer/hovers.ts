f74b731ee50d59a0165fa6ad4c7ef41f8a2e00f9
novikov82 <novikov82@gmail.com>
2022-02-05 23:32:48 +0300

    try to fix blinking hover test

--- testCafe/designer/hovers.ts
+++ testCafe/designer/hovers.ts
@@ -1,4 +1,4 @@
-import { Selector } from "testcafe";
+import { ClientFunction, Selector } from "testcafe";
 import { setJSON, url } from "../helper";
 const title = "Page hover";
 
@@ -48,20 +48,28 @@ const json_pages = {
 
 test("Page hover test", async t => {
   await setJSON(json);
+  await ClientFunction(() => {
+    window["creator"].pageHoverDelay = 2000;
+  })();
   await t
     .maximizeWindow();
+
   const page = Selector(".svc-page__content");
   const question = Selector(".svc-question__adorner");
   await t.hover(page, { offsetX: 5, offsetY: 5 })
-    .expect(page.hasClass("svc-hovered-ready")).ok({ timeout: 50 })
-    .expect(page.hasClass("svc-hovered")).notOk({ timeout: 20 })
-    .expect(page.hasClass("svc-hovered")).ok({ timeout: 300 })
+    .expect(page.hasClass("svc-hovered-ready")).ok({ timeout: 500 })
+    .expect(page.hasClass("svc-hovered")).notOk({ timeout: 200 })
+    .expect(page.hasClass("svc-hovered")).ok({ timeout: 3000 })
     .hover(Selector(".svc-toolbox"))
-    .expect(page.hasClass("svc-hovered")).notOk({ timeout: 50 })
+    .expect(page.hasClass("svc-hovered")).notOk({ timeout: 500 })
     .hover(question, { offsetX: 5, offsetY: 5 })
-    .expect(question.hasClass("svc-hovered")).ok({ timeout: 50 })
-    .wait(300)
+    .expect(question.hasClass("svc-hovered")).ok({ timeout: 500 })
+    .wait(3000)
     .expect(page.hasClass("svc-hovered")).notOk();
+
+  await ClientFunction(() => {
+    window["creator"].pageHoverDelay = 200;
+  })();
 });
 
 test("Page hover on Add Question button", async t => {
