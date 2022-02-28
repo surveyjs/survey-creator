f8b20885e51021f0bf814eeafa4284226e974528
Daniel Simon <d.simon@onventis.de>
2022-02-28 08:29:46 +0100

    Add Czech and Japanese localization

--- packages/survey-creator/src/entries/index.ts
+++ packages/survey-creator/src/entries/index.ts
@@ -93,29 +93,31 @@ export { StylesManager } from "../stylesmanager";
 
 //localization
 import "../localization/arabic";
+import "../localization/croatian";
+import "../localization/czech";
 import "../localization/danish";
+import "../localization/dutch";
+import "../localization/finnish";
 import "../localization/french";
 import "../localization/german";
-import "../localization/italian";
+import "../localization/hungarian";
 import "../localization/indonesian";
+import "../localization/italian";
+import "../localization/japanese";
 import "../localization/korean";
 import "../localization/malay";
+import "../localization/norwegian";
 import "../localization/persian";
 import "../localization/polish";
 import "../localization/portuguese";
+import "../localization/russian";
 import "../localization/simplified-chinese";
 import "../localization/slovak";
 import "../localization/spanish";
 import "../localization/swedish";
+import "../localization/tajik";
 import "../localization/traditional-chinese";
 import "../localization/turkish";
-import "../localization/norwegian";
-import "../localization/hungarian";
-import "../localization/russian";
-import "../localization/tajik";
-import "../localization/dutch";
-import "../localization/croatian";
-import "../localization/finnish";
 
 export {
   Translation,
