1c6f767e85f66499d7add50732359f1b18025fd9
OlgaLarina <olga.larina.developer@gmail.com>
2022-03-21 08:27:16 +0300

    Fix Image design in Creator (#2811)
    * resolve #2729 : Fix Image design in Creator
    
    * work for #2729 : add testcafe test
    
    * work for #2729 : update test
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

---
+++ testCafe/designer/surface.ts
@@ -0,0 +1,49 @@
+import { url, setJSON, collapseButtonSelector, getTabbedMenuItemByText, creatorTabPreviewName } from "../helper";
+import { Selector } from "testcafe";
+const title = "Designer surface";
+
+fixture`${title}`.page`${url}`;
+
+test("Image question", async (t) => {
+  const json = {
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [
+          {
+            "type": "image",
+            "name": "question1",
+            "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+          }
+        ]
+      }
+    ]
+  };
+  const imageQuestionSelector = Selector(".sd-image__image");
+  await setJSON(json);
+
+  await t
+    .resizeWindow(1280, 900)
+    .expect(imageQuestionSelector.exists).ok()
+    .expect(imageQuestionSelector.classNames).contains("sd-image__image--adaptive")
+    .expect(imageQuestionSelector.clientWidth).eql(540)
+    .expect(imageQuestionSelector.clientHeight).eql(359)
+
+    .click(imageQuestionSelector)
+    .expect(Selector("div [data-name=\"imageHeight\"] input").value).eql("")
+    .expect(Selector("div [data-name=\"imageHeight\"] input").getAttribute("placeholder")).eql("auto")
+    .expect(Selector("div [data-name=\"imageWidth\"] input").value).eql("")
+    .expect(Selector("div [data-name=\"imageWidth\"] input").getAttribute("placeholder")).eql("auto")
+
+    .click(collapseButtonSelector)
+    .expect(imageQuestionSelector.exists).ok()
+    .expect(imageQuestionSelector.classNames).contains("sd-image__image--adaptive")
+    .expect(imageQuestionSelector.clientWidth).eql(540)
+    .expect(imageQuestionSelector.clientHeight).eql(359)
+
+    .click(getTabbedMenuItemByText(creatorTabPreviewName))
+    .expect(imageQuestionSelector.exists).ok()
+    .expect(imageQuestionSelector.classNames).contains("sd-image__image--adaptive")
+    .expect(imageQuestionSelector.clientWidth).eql(640)
+    .expect(imageQuestionSelector.clientHeight).eql(426);
+});
