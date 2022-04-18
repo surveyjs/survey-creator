2d19dabd2ded91e0e10d653bc6630044533c9aac
Andrew <andrew.telnov@gmail.com>
2022-04-18 12:02:32 +0300

    Support Markdown in localization strings for property helpp in property grid fixed #2918 (#2922)

--- packages/survey-creator-core/src/property-grid/index.ts
+++ packages/survey-creator-core/src/property-grid/index.ts
@@ -32,13 +32,13 @@ import {
   ICollectionItemAllowOperations,
   settings as creatorSettings
 } from "../settings";
-import { PropertiesHelpTexts } from "./properties-helptext";
 import { QuestionFactory } from "survey-core";
 import { defaultV2Css } from "survey-core";
 import { updateMatrixRemoveAction } from "../utils/actions";
 import { SurveyHelper } from "../survey-helper";
 import { CreatorBase } from "../creator-base";
 import { IPropertyEditorInfo, SurveyQuestionEditorDefinition } from "../question-editor/definition";
+import { parsePropertyDescription } from "./description-parser";
 
 function propertyVisibleIf(params: any): boolean {
   if (!this.question || !this.question.obj || !this.question.property) return false;
@@ -484,7 +484,7 @@ export class PropertyJSONGenerator {
         q.visibleIf = eventVisibility ? "propertyVisibleIf() = true" : "";
       }
       q.descriptionLocation = "hidden";
-      var helpText = PropertiesHelpTexts.instance.getHelpText(this.obj, prop);
+      let helpText = editorLocalization.getPropertyHelpInEditor(this.obj.getType(), prop.name, prop.type);
       if (!!helpText) {
         q.description = helpText;
       }
@@ -805,6 +805,11 @@ export class PropertyGridModel {
     });
     this.survey.onAfterRenderQuestion.add((sender, options) => {
       this.onAfterRenderQuestion(options);
+    });
+    this.survey.onTextMarkdown.add((sender, options) => {
+      if(options.name === "description") {
+        options.html = parsePropertyDescription(options.text);
+      }
     });
     this.survey.editingObj = this.obj;
     if (this.objValueChangedCallback) {
