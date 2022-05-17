9c832a28e677e59b944a888d81a86ffea14fbaca
RomanTsukanov <RomanTsukanov@users.noreply.github.com>
2022-05-17 16:33:02 +0400

    Update packages/survey-creator-core/src/creator-base.ts
    Co-authored-by: DirkPieterse <dirk.pieterse@devexpress.com>

--- packages/survey-creator-core/src/creator-base.ts
+++ packages/survey-creator-core/src/creator-base.ts
@@ -619,7 +619,7 @@ export class CreatorBase extends Base
     any
   > = new Survey.Event<(sender: CreatorBase, options: any) => any, any>();
   /**
-   * An event that is raised on each edit to a property value in a survey object.
+   * An event that is raised each time a user edits a survey object property.
    * Use this event to correct or validate the property value while the user enters it.
    * 
    * The event handler accepts the following arguments:
