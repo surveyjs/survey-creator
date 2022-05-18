8df64b9c1008e6cb5f76d0e3ea0773250edbf71f
OlgaLarina <olga.larina.developer@gmail.com>
2022-05-18 10:50:29 +0300

    override screenSizeBreakpoints (#3015)
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- packages/survey-creator-core/src/creator-responsivity-manager.ts
+++ packages/survey-creator-core/src/creator-responsivity-manager.ts
@@ -5,7 +5,7 @@ export class CreatorResponsivityManager {
   private currentWidth;
   private prevShowToolbox;
   private prevShowPageNavigator;
-  private screenWidth: { [key: string]: number } = {
+  public static screenSizeBreakpoints: { [key: string]: number } = {
     "xxl": 1800,
     "xl": 1500,
     "l": 1200,
@@ -14,8 +14,9 @@ export class CreatorResponsivityManager {
   }
   private getScreenWidth(): string {
     let res;
-    Object.keys(this.screenWidth).forEach((mode: string) => {
-      if (!res && this.container && !!this.screenWidth[mode] && this.container.offsetWidth >= this.screenWidth[mode]) {
+    Object.keys(CreatorResponsivityManager.screenSizeBreakpoints).forEach((mode: string) => {
+      const breakpoint = CreatorResponsivityManager.screenSizeBreakpoints[mode];
+      if (!res && this.container && !!breakpoint && this.container.offsetWidth >= breakpoint) {
         res = mode;
       }
     });
@@ -41,7 +42,7 @@ export class CreatorResponsivityManager {
     }
   }
   private procesShowPageNavigator(pageNavigatorVisibility: boolean) {
-    if(this.creator.pageEditMode === "bypage") {
+    if (this.creator.pageEditMode === "bypage") {
       this.creator.showPageNavigator = true;
       return;
     }
