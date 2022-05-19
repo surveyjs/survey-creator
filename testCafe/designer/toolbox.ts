6ad07e9bd8ff2fdc8976ce716be3c3be1d8b8dc3
OlgaLarina <olga.larina.dev@gmail.com>
2022-05-19 16:06:44 +0300

    fix f-test

--- testCafe/designer/toolbox.ts
+++ testCafe/designer/toolbox.ts
@@ -39,15 +39,21 @@ async function setupCategories(t, windowWidth = 1910) {
 }
 
 async function setupToolboxProperty(propertyName: string, propertyValue: any) {
-  ClientFunction(() => {
+  await ClientFunction((propertyName: string, propertyValue: any) => {
     window["creator"].toolbox[propertyName] = propertyValue;
-  })();
+  })(propertyName, propertyValue);
 }
+
+const categories = Selector(".svc-toolbox__category-header");
+const visibleToolboxItems = Selector(".svc-toolbox__tool").filterVisible();
+const collapsedCategories = Selector(".svc-string-editor__button--expand").parent(".svc-toolbox__category-header");
+const expandedCategories = Selector(".svc-string-editor__button--collapse").parent(".svc-toolbox__category-header");
+const getCollapsedCategories = (index = 0) => { return categories.nth(index).find(".svc-string-editor__button--expand"); };
+const getExpandedCategories = (index = 0) => { return categories.nth(index).find(".svc-string-editor__button--collapse"); };
 
 test("Categories check hover icons", async (t) => {
   await setupCategories(t);
 
-  const categories = Selector(".svc-toolbox__category-header");
   await t
     .expect(categories.count).eql(3)
     .expect(categories.nth(0).innerText).eql("General")
@@ -55,23 +61,21 @@ test("Categories check hover icons", async (t) => {
     .expect(categories.nth(2).innerText).eql("Panels")
 
     .hover(categories.nth(0), { speed: 0.5 })
-    .expect(categories.nth(0).find(".svc-string-editor__button--collapse").visible).ok()
-    .expect(categories.nth(0).find(".svc-string-editor__button--expand").visible).notOk()
+    .expect(getExpandedCategories(0).visible).ok()
+    .expect(getCollapsedCategories(0).visible).notOk()
 
     .hover(categories.nth(1), { speed: 0.5 })
-    .expect(categories.nth(1).find(".svc-string-editor__button--collapse").visible).notOk()
-    .expect(categories.nth(1).find(".svc-string-editor__button--expand").visible).ok()
+    .expect(getExpandedCategories(1).visible).notOk()
+    .expect(getCollapsedCategories(1).visible).ok()
 
     .hover(categories.nth(2), { speed: 0.5 })
-    .expect(categories.nth(2).find(".svc-string-editor__button--collapse").visible).notOk()
-    .expect(categories.nth(2).find(".svc-string-editor__button--expand").visible).ok();
+    .expect(getExpandedCategories(2).visible).notOk()
+    .expect(getCollapsedCategories(2).visible).ok();
 });
 
 test("Categories large mode", async (t) => {
   await setupCategories(t);
 
-  const categories = Selector(".svc-toolbox__category-header");
-  const visibleToolboxItems = Selector(".svc-toolbox__tool").filterVisible();
   await t
     .expect(categories.count).eql(3)
     .expect(visibleToolboxItems.count).eql(15)
@@ -86,8 +90,7 @@ test("Categories large mode", async (t) => {
 test.before(async (t) => {
   await t.resizeWindow(1200, 600);
 })("Categories Responsiveness small -> large", async (t) => {
-  const categories = Selector(".svc-toolbox__category-header");
-  const visibleToolboxItems = Selector(".svc-toolbox__tool").filterVisible();
+
   await setupCategories(t, 1110);
   await t
     .expect(Selector(".svc-toolbox .sv-dots__item").visible).ok()
@@ -108,8 +111,6 @@ test.before(async (t) => {
 test.before(async (t) => {
   await t.resizeWindow(1900, 600);
 })("Categories Responsiveness large -> small", async (t) => {
-  const categories = Selector(".svc-toolbox__category-header");
-  const visibleToolboxItems = Selector(".svc-toolbox__tool").filterVisible();
 
   await setupCategories(t);
   await t
@@ -127,24 +128,25 @@ test("Categories allowExpandMultipleCategories property", async (t) => {
   await setupToolboxProperty("forceCompact", false);
   await setupToolboxProperty("allowExpandMultipleCategories", true);
 
-  const categories = Selector(".svc-toolbox__category-header");
-  const collapsedCategories = Selector(".svc-toolbox__category-header--collapsed");
-  const visibleToolboxItems = Selector(".svc-toolbox__tool").filterVisible();
   await t
     .expect(categories.count).eql(3)
     .expect(collapsedCategories.count).eql(3)
+    .expect(expandedCategories.count).eql(0)
     .expect(visibleToolboxItems.count).eql(0)
 
     .click(categories.nth(0))
     .expect(collapsedCategories.count).eql(2)
+    .expect(expandedCategories.count).eql(1)
     .expect(visibleToolboxItems.count).eql(15)
 
     .click(categories.nth(1))
     .expect(collapsedCategories.count).eql(1)
+    .expect(expandedCategories.count).eql(2)
     .expect(visibleToolboxItems.count).eql(18)
 
     .click(categories.nth(2))
     .expect(collapsedCategories.count).eql(0)
+    .expect(expandedCategories.count).eql(3)
     .expect(visibleToolboxItems.count).eql(20);
 });
 
@@ -153,38 +155,37 @@ test("Categories keepAllCategoriesExpanded property", async (t) => {
   await setupToolboxProperty("forceCompact", false);
   await setupToolboxProperty("keepAllCategoriesExpanded", true);
 
-  const categories = Selector(".svc-toolbox__category-header");
-  const collapsedCategories = Selector(".svc-toolbox__category-header--collapsed");
-  const visibleToolboxItems = Selector(".svc-toolbox__tool").filterVisible();
+  const collapsibleCategories = Selector(".svc-toolbox__category-header--collapsed");
+
   await t
     .expect(categories.count).eql(3)
-    .expect(collapsedCategories.count).eql(0)
+    .expect(collapsibleCategories.count).eql(0)
     .expect(visibleToolboxItems.count).eql(20)
 
     .hover(categories.nth(0), { speed: 0.5 })
-    .expect(categories.nth(0).find(".svc-string-editor__button--collapse").exists).notOk()
-    .expect(categories.nth(0).find(".svc-string-editor__button--expand").exists).notOk()
+    .expect(getExpandedCategories(0).exists).notOk()
+    .expect(getCollapsedCategories(0).exists).notOk()
 
     .click(categories.nth(0))
     .expect(categories.count).eql(3)
-    .expect(collapsedCategories.count).eql(0)
+    .expect(collapsibleCategories.count).eql(0)
     .expect(visibleToolboxItems.count).eql(20)
 
     .hover(categories.nth(1), { speed: 0.5 })
-    .expect(categories.nth(1).find(".svc-string-editor__button--collapse").exists).notOk()
-    .expect(categories.nth(1).find(".svc-string-editor__button--expand").exists).notOk()
+    .expect(getExpandedCategories(1).exists).notOk()
+    .expect(getCollapsedCategories(1).exists).notOk()
 
     .click(categories.nth(1))
     .expect(categories.count).eql(3)
-    .expect(collapsedCategories.count).eql(0)
+    .expect(collapsibleCategories.count).eql(0)
     .expect(visibleToolboxItems.count).eql(20)
 
     .hover(categories.nth(2), { speed: 0.5 })
-    .expect(categories.nth(2).find(".svc-string-editor__button--collapse").exists).notOk()
-    .expect(categories.nth(2).find(".svc-string-editor__button--expand").exists).notOk()
+    .expect(getExpandedCategories(2).exists).notOk()
+    .expect(getCollapsedCategories(2).exists).notOk()
 
     .click(categories.nth(2))
     .expect(categories.count).eql(3)
-    .expect(collapsedCategories.count).eql(0)
+    .expect(collapsibleCategories.count).eql(0)
     .expect(visibleToolboxItems.count).eql(20);
 });
