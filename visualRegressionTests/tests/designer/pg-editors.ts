3872fb82f3b670ac4c749634be44eb2395fbf76b
Dmitry <dk981234@gmail.com>
2022-06-17 14:48:38 +0400

    Work for #3134: fix pg checkbox multi line caption alignment (#3142)
    * Work for #3134: fix pg checkbox multi line caption alignment
    
    * Fix vrt tests

--- visualRegressionTests/tests/designer/pg-editors.ts
+++ visualRegressionTests/tests/designer/pg-editors.ts
@@ -145,4 +145,8 @@ test("Property grid checkbox - all states", async (t) => {
   await setCheckboxProperty("readOnly", false);
   await t.click(checkbox).hover(Selector(".sv-string-viewer").withText("Description"));
   await checkElementScreenshot("pg-checkbox-checked-focused.png", checkbox, t);
+
+  await ClientFunction(() => { document.body.focus(); })();
+  await setCheckboxProperty("title", "Very very very very very very very very very very long checkbox caption");
+  await checkElementScreenshot("pg-checkbox-long-caption.png", checkbox, t);
 });
