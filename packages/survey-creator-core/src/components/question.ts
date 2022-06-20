fa97c858a671dd75945d51630ce8fda7f1a10b9f
T_S_V <tsv2013@noreply.github.com>
2022-06-20 16:03:39 +0300

    Fixed #3117 - Check Question Converter icon size

--- packages/survey-creator-core/src/components/question.ts
+++ packages/survey-creator-core/src/components/question.ts
@@ -247,7 +247,7 @@ export class QuestionAdornerViewModel extends SurveyElementAdornerBase {
     return new Action({
       id: "convertTo",
       css: "sv-action--first sv-action-bar-item--secondary",
-      iconName: "icon-drop-down-arrow",
+      iconName: "icon-drop-down-arrow_16x16",
       iconSize: 16,
       title: actionTitle,
       visibleIndex: 0,
