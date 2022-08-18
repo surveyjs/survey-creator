459b4c8d1881df2d4a9542dd6df5514b15b8e7f2
Andrew Telnov <andrew.telnov@gmail.com>
2022-08-18 16:00:41 +0300

    Add survey.width property into property grid

--- packages/survey-creator-core/src/question-editor/definition.ts
+++ packages/survey-creator-core/src/question-editor/definition.ts
@@ -525,6 +525,7 @@ export class SurveyQuestionEditorDefinition {
         "mode",
         "cookieName",
         "widthMode",
+        "widthMode",
 
         { name: "showPreviewBeforeComplete", tab: "navigation" },
         { name: "pagePrevText", tab: "navigation" },
