2190e91cc0451d6b8a922fb9fde459d601c6f544
T_S_V <tsv2013@noreply.github.com>
2022-09-02 15:23:22 +0300

    Setup unit test infrastructure

--- packages/survey-creator-angular/karma.conf.js
+++ packages/survey-creator-angular/karma.conf.js
@@ -48,9 +48,10 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    //autoWatch: true,
     browsers: ["Chrome"],
-    singleRun: false,
+    //singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
