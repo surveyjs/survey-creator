44667d04a238dceb49ab589b94b8dc4b39142af2
Andrew <andrew.telnov@gmail.com>
2022-09-27 10:25:26 +0300

    Bug/3468 toolbox order customwidgets fixed #3468 (#3469)
    * Impossible to reorder Toolbox items using the options | questionTypes collection fixed #3468
    
    * Fix unit test #3468

--- packages/survey-creator-core/src/toolbox.ts
+++ packages/survey-creator-core/src/toolbox.ts
@@ -722,7 +722,13 @@ export class QuestionToolbox
       isCopied: false,
       category: category
     });
-    this.actions.push(this.getActionByItem(item));
+    const index = Array.isArray(this.supportedQuestions) ? this.supportedQuestions.indexOf(item.id) : -1;
+    const action = this.getActionByItem(item);
+    if(index > -1) {
+      this.actions.splice(index, 0, action);
+    } else {
+      this.actions.push(action);
+    }
   }
   private getQuestionJSON(question: any): any {
     var json = new JsonObject().toJsonObject(question);
