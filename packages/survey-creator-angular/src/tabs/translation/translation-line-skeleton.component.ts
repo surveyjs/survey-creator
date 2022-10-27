ae8d9b67c0c07583f5fdf9be5aeb7dcc824f9e38
Dmitry Kuzin <dk981234@gmail.com>
2022-10-27 17:46:31 +0400

    Fix vrt tests: fix translation skeleton component

--- packages/survey-creator-angular/src/tabs/translation/translation-line-skeleton.component.ts
+++ packages/survey-creator-angular/src/tabs/translation/translation-line-skeleton.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { AngularComponentFactory } from "survey-angular-ui";
+import { AngularComponentFactory, EmbeddedViewContentComponent } from "survey-angular-ui";
 
 @Component({
   selector: "sd-translation-line-skeleton",
   templateUrl: "./translation-line-skeleton.component.html",
   styles: [":host { display: none; }"]
 })
-export class TranslationSkeletonComponent { }
+export class TranslationSkeletonComponent extends EmbeddedViewContentComponent { }
 AngularComponentFactory.Instance.registerComponent("sd-translation-line-skeleton", TranslationSkeletonComponent);
 
