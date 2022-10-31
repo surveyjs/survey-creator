1bdb14f9b005b337607a0aca40c5979f7466aa85
Dmitry Kuzin <dk981234@gmail.com>
2022-10-31 17:52:32 +0400

    Fix import

--- packages/survey-creator-angular/example/angular-ui/src/app/app.module.ts
+++ packages/survey-creator-angular/example/angular-ui/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { SurveyCreatorModule } from "src/angular-ui.module";
+import { SurveyCreatorModule } from "survey-creator-angular";
 import { SurveyModule } from "survey-angular-ui";
 import { ExampleComponent } from "./example.component";
 import { AppRoutingModule } from "./router.module";
