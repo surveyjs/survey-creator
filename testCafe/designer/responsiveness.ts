2018c9bc63c41fd83d9ec2e682f6c154a94bca75
TSV <tsv2013@users.noreply.github.com>
2022-11-14 17:43:12 +0300

    Fixed #3632 - Creator bar. Settings button. - Fixed f-tests

--- testCafe/designer/responsiveness.ts
+++ testCafe/designer/responsiveness.ts
@@ -189,13 +189,13 @@ test("property grid for mobile devices", async (t) => {
     .resizeWindow(370, 400)
     .expect(mobilePropertGrid.visible).notOk()
 
-    .click(getBarItemByTitle("Survey Settings").filterVisible())
+    .click(getBarItemByTitle("Settings").filterVisible())
     .expect(mobilePropertGrid.visible).ok()
     .expect(mobilePropertGridTitle.textContent).eql("Survey")
 
     .click(mobileCloseButton)
     .click(Selector("span").withText("Add Question"))
-    .click(getBarItemByTitle("Survey Settings").filterVisible().nth(0))
+    .click(getBarItemByTitle("Settings").filterVisible().nth(0))
     .expect(mobilePropertGridTitle.textContent).eql("question1")
 
     .resizeWindow(1920, 900)
