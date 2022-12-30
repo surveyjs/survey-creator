6522290d696b9dac1a19b56abd843e1e114f6a2b
TSV <tsv2013@users.noreply.github.com>
2022-12-30 12:15:46 +0300

    Work for #3725 - Localization strings are missing for survey element properties - try to fix f-tests

--- testCafe/designer/drag-drop.ts
+++ testCafe/designer/drag-drop.ts
@@ -954,7 +954,7 @@ test("Drag Drop Pages MatrixRows (property grid Pages)", async (t) => {
   };
   await setJSON(json);
 
-  const Settings = Selector("[title=\"Survey Settings\"]");
+  const Settings = Selector("[title=\"Open survey settings\"]");
   const PagesTab = Selector("h4").withExactText("Pages");
   await t
     .click(Settings, { speed: 0.5 })
--- testCafe/designer/responsiveness.ts
+++ testCafe/designer/responsiveness.ts
@@ -189,7 +189,7 @@ test("property grid for mobile devices", async (t) => {
     .resizeWindow(370, 400)
     .expect(mobilePropertGrid.visible).notOk()
 
-    .click(getBarItemByTitle("Survey Settings").filterVisible())
+    .click(getBarItemByTitle("Open survey settings").filterVisible())
     .expect(mobilePropertGrid.visible).ok()
     .expect(mobilePropertGridTitle.textContent).eql("Survey")
 
@@ -333,7 +333,7 @@ test("Responsive creator: property grid - click the shadow", async (t) => {
     .expect(flyoutPropertyGrid.exists).ok()
     .expect(objectSelectorButton.withText("page1").visible).ok()
     .click(collapseButtonSelector)
-    .click(getBarItemByTitle("Survey Settings").filterVisible())
+    .click(getBarItemByTitle("Open survey settings").filterVisible())
     .expect(objectSelectorButton.withText("Survey").visible).ok()
     .click(Selector(".svc-creator"), { offsetX: 237, offsetY: 273 })
 
