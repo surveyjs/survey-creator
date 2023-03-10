98c9dd112e8f0f2577bb074ac25ddd68b5365b15
Jane <zhanna.gaidukova@gmail.com>
2023-03-10 18:01:21 +0300

    Update options description (#3886)
    * Update options description
    
    * Apply suggestions from code review
    
    * Update creator-base.ts
    
    ---------
    
    Co-authored-by: RomanTsukanov <RomanTsukanov@users.noreply.github.com>

--- packages/survey-creator-core/src/creator-base.ts
+++ packages/survey-creator-core/src/creator-base.ts
@@ -614,16 +614,15 @@ export class CreatorBase extends Base
    */
   public onPropertyValidationCustomError: CreatorEvent = new CreatorEvent();
   /**
-   * An event that is raised each time a user edits a survey object property.
-   * Use this event to correct or validate the property value while the user enters it.
+   * An event that is raised each time a user modifies a survey object property. Use this event to validate or correct a property value while the user enters it.
    *
    * The event handler accepts the following arguments:
    *
    * - `sender`- A Survey Creator instance that raised the event.
    * - `options.obj` - A survey object instance (question or panel) whose property is being edited.
    * - `options.propertyName` - The name of the property.
-   * - `options.value` - A property value entered by a user.
-   * - `options.newValue` - A corrected property value. Specify this field if you want to override the `options.value`.
+   * - `options.value` - An old property value.
+   * - `options.newValue` - A new property value. Specify this field if you want to override the entered value.
    * @see onPropertyValidationCustomError
    * @see onSurveyPropertyValueChanged
    */
@@ -1614,7 +1613,7 @@ export class CreatorBase extends Base
    */
   public get isInitialSurveyEmpty(): boolean { return this.isInitialSurveyEmptyValue; }
   protected initSurveyWithJSON(json: any, clearState: boolean): void {
-    if(!json) {
+    if (!json) {
       json = { "logoPosition": "right" };
     }
     // currentPlugin.deactivate && currentPlugin.deactivate();
@@ -1877,7 +1876,7 @@ export class CreatorBase extends Base
    */
   public changeText(value: string, clearState = false): void {
     this.setTextValue(value);
-    if(!value) {
+    if (!value) {
       this.initSurveyWithJSON(undefined, clearState);
     } else {
       const textWorker = new SurveyTextWorker(value);
