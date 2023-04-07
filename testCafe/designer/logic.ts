52a5e8e457949a2e5582aa03b385a844958edd22
Dmitry Kuzin <dk981234@gmail.com>
2023-04-07 14:31:26 +0400

    Remove debug and test.only

--- testCafe/designer/logic.ts
+++ testCafe/designer/logic.ts
@@ -382,7 +382,7 @@ async function check2Rule(t: TestController) {
     .expect(logicQuestionSelector.nth(1).textContent).contains("q3");
 }
 
-test.only("Modified rules without saving", async (t) => {
+test("Modified rules without saving", async (t) => {
   const rule1Condition = "If 'q1' == 'Item 1', make question 'q2' visible";
   const additinalClass = "sl-table__row--additional";
 
@@ -396,9 +396,7 @@ test.only("Modified rules without saving", async (t) => {
     .click(logicDetailButtonElement.nth(0))
     .click(logicQuestionSelector)
     .click(getListItemByText("q3"))
-    .debug()
     .typeText(logicQuestionValueSelector, "45", { replace: true })
-    .debug()
     .click(logicActionSelector)
     .click(getListItemByText("Complete survey"));
   await check1Rule(t, rule1Condition);
