54ade828ccdb419ce6bc7c7542d9312e493f751d
TSV <tsv2013@users.noreply.github.com>
2023-04-13 11:23:22 +0300

    Fixed #3894 - Type errors from survey-creator-react

--- webpack-plugins/webpack-dts-generator.js
+++ webpack-plugins/webpack-dts-generator.js
@@ -5,14 +5,18 @@ const child_process = require('child_process');
 module.exports = function DtsGeneratorProgressPlugin(options) {
   return new options.webpack.ProgressPlugin(function(progress) {
     if (progress === 1) {
+      console.log("typings start");
       console.log("typescript start");
       child_process.execSync("tsc --p tsconfig.typing.json");
       console.log("typescript end");
 
       const importName = options.importName || "entries/index";
-      const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + importName + "\"); export = main; }";
+      // const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + importName + "\"); export = main; }";
+      const content = "\nexport * from \"./typings/" + importName + "\";";
       console.log("\"" + content + "\"");
-      fs.writeFile(options.filePath, content, { flag: 'a' }, err => { if(err) { console.error(err); } });
+      console.log(options.filePath);
+      fs.writeFile(options.filePath, content, { flag: 'w' }, err => { if (err) { console.error(err); } });
+      console.log("typings end");
     }
   });
 };
