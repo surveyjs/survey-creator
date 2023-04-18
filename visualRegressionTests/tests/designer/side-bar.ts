cf1dae0eb1fc8d14af04d37615e0ae664d90122c
OlgaLarina <olga.larina.dev@gmail.com>
2023-04-18 18:24:59 +0300

    resolve #3972  Property Grid Element Selector - The focus is accidentally jumped to the search box

--- visualRegressionTests/tests/designer/side-bar.ts
+++ visualRegressionTests/tests/designer/side-bar.ts
@@ -1,4 +1,6 @@
-import { url, objectSelectorButton, propertyGridSelector, expandButtonSelector, setJSON, takeElementScreenshot, wrapVisualTest } from "../../helper";
+import { Selector } from "testcafe";
+import { url, objectSelectorButton, propertyGridSelector, expandButtonSelector, setJSON, takeElementScreenshot, wrapVisualTest, pageNavigator, getListItemByText } from "../../helper";
+import { largeSurvey } from "./surveys/large-survey";
 
 const title = "Sidebar Screenshot";
 
@@ -13,6 +15,25 @@ test("object selector popup", async (t) => {
       .click(objectSelectorButton);
 
     await takeElementScreenshot("side-bar-object-selector.png", propertyGridSelector, t, comparer);
+    await t.resizeWindow(1920, 900);
+  });
+});
+
+test("object selector with search and selected item", async (t) => {
+  await wrapVisualTest(t, async (t, comparer) => {
+    const objectSelectorContent = ".sv-popup.svc-object-selector .sv-popup__body-content";
+
+    await setJSON(largeSurvey);
+    await t
+      .resizeWindow(1280, 900)
+      .click(pageNavigator)
+      .click(getListItemByText("educationalNeeds"))
+      .click(Selector(".svc-string-editor__input").withText("Do you live?"))
+
+      .click(objectSelectorButton)
+      .wait(1000);
+
+    await takeElementScreenshot("side-bar-object-selector-with-selected-item.png", objectSelectorContent, t, comparer);
     await t.resizeWindow(1920, 900);
   });
 });
