aa27ef995cd3587845ba6fb538d4db6cdcbe51b0
Aleksey Novikov <novikov82@gmail.com>
2023-04-26 10:06:22 +0300

    #4044 Rating: disable, not hide "Add/Remove" button if action is not allowed (#4046)
    * #4044 Rating: disable, not hide "Add/Remove" button if action is not allowed
    Fixes #4044
    
    * #4044 - update screenshots

--- packages/survey-creator-react/src/adorners/QuestionRating.tsx
+++ packages/survey-creator-react/src/adorners/QuestionRating.tsx
@@ -36,14 +36,14 @@ export class QuestionRatingAdornerComponent extends CreatorModelElement<Question
       <div className="svc-rating-question-content">
         <div className="svc-rating-question-controls svc-item-value-controls">
           {model.allowRemove ? attachKey2click(<span
-            className="svc-item-value-controls__button svc-item-value-controls__remove"
+            className={model.removeClassNames}
             aria-label={model.removeTooltip}
             onClick={() => model.removeItem(model)}
           >
             <SvgIcon size={16} iconName={"icon-remove_16x16"} title={model.removeTooltip}></SvgIcon>
           </span>) : null}
           {model.allowAdd ? attachKey2click(<span
-            className="svc-item-value-controls__button svc-item-value-controls__add"
+            className={model.addClassNames}
             aria-label={model.addTooltip}
             onClick={() => model.addItem(model)}
           >
