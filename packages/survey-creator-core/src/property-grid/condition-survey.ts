8a44f5cbcf409576d7c49470822d8fd4b1769bc9
Dmitry Kuzin <dk981234@gmail.com>
2023-06-06 13:25:25 +0400

    Work for https://github.com/surveyjs/survey-library/issues/6099: fix svc-logic-question-value styles

--- packages/survey-creator-core/src/property-grid/condition-survey.ts
+++ packages/survey-creator-core/src/property-grid/condition-survey.ts
@@ -928,7 +928,7 @@ export class ConditionEditor extends PropertyEditorSetupValue {
     // options.cssClasses.mainRoot += "sd-question sd-row__question";
     if (options.question.name === "questionValue" || options.question.isContentElement) {
       assignDefaultV2Classes(options.cssClasses, options.question.getType());
-      options.cssClasses.mainRoot += " svc-logic-question-value";
+      options.cssClasses.mainRoot += " svc-logic-question-value sd-element--with-frame";
       options.cssClasses.error.root = "svc-logic-operator__error";
     }
     if (options.question.name === "panel") {
