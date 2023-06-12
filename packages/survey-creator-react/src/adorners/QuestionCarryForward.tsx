dec1de37ac4cec382e2741e02ef60b75fdeed403
Dmitry Kuzin <dk981234@gmail.com>
2023-06-12 18:53:58 +0400

    Work for #4180: apply changes in React

--- packages/survey-creator-react/src/adorners/QuestionCarryForward.tsx
+++ packages/survey-creator-react/src/adorners/QuestionCarryForward.tsx
@@ -5,9 +5,11 @@ import { ActionButton } from "../ActionButton";
 
 export class QuestionCarrayForwardBanner extends React.Component<QuestionCarryForwardParams, any> {
   render(): JSX.Element {
-    return (<div className="svc-question__carryforward">
-      <span>{this.props.text}</span>
-      <ActionButton click={() => this.props.onClick()} text={this.props.question.name}></ActionButton>
+    return (<div className="svc-carry-forward-panel">
+      <span>{this.props.text}{" "}</span>
+      <span className="svc-carry-forward-panel__link">
+        <ActionButton click={() => this.props.onClick()} text={this.props.question.name}></ActionButton>
+      </span>
     </div>);
   }
 }
