3533d849446de86a4e1167780e605692d3478f2f
tsv2013 <tsv2013@users.noreply.github.com>
2023-06-19 22:24:03 +0300

    Fixed #4142 - Unhandled error after remove page id (#4245)
    * Fixed #4142 - Unhandled error after remove page id
    
    * Try to fix u-tests

--- packages/survey-creator-core/jest.config.js
+++ packages/survey-creator-core/jest.config.js
@@ -16,7 +16,8 @@ module.exports = {
     "\\.html?$": "<rootDir>/tests/empty-module.js",
     "survey.i18n": "<rootDir>/node_modules/survey-core/survey.i18n.js",
     "survey-creator-core": "<rootDir>/src/editorLocalization.ts",
-    "survey-core": "<rootDir>/node_modules/survey-core/survey.core.js"
+    "survey-core": "<rootDir>/node_modules/survey-core/survey.core.js",
+    "tslib": "<rootDir>/node_modules/tslib",
   },
   testRegex: "\/tests\/.*\.(tests?|spec)\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
