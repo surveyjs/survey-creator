bfcd6115a135188c3fd2307b1c064e4124685fe7
OlgaLarina <olga.larina.dev@gmail.com>
2023-08-29 16:32:46 +0300

    work for #4487 : fix angular

--- packages/survey-creator-angular/src/toolbox/toolbox-item.component.ts
+++ packages/survey-creator-angular/src/toolbox/toolbox-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from "@angular/core";
-import { ToolboxToolViewModel, CreatorBase, IQuestionToolboxItem, editorLocalization } from "survey-creator-core";
+import { ToolboxToolViewModel, CreatorBase, IQuestionToolboxItem } from "survey-creator-core";
 import { BaseAngular, AngularComponentFactory } from "survey-angular-ui";
-import { Action } from "survey-core";
 
 @Component({
   selector: "svc-toolbox-item",
@@ -10,7 +9,7 @@ import { Action } from "survey-core";
 })
 export class ToolboxItemComponent extends BaseAngular<ToolboxToolViewModel> {
   @Input() creator!: CreatorBase;
-  @Input() model!: Action;
+  @Input() model!: IQuestionToolboxItem;
   @Input() isCompact: boolean = false;
   @Input() viewModel!: ToolboxToolViewModel
   protected getModel(): ToolboxToolViewModel {
