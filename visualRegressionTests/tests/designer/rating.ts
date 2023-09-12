fd95d5b20f8b40aa412d869cb0c38eb2ebdbfa04
Aleksey Novikov <novikov82@gmail.com>
2023-09-12 10:10:22 +0300

    Rating-item overflow on small sizes #4585

--- visualRegressionTests/tests/designer/rating.ts
+++ visualRegressionTests/tests/designer/rating.ts
@@ -193,3 +193,41 @@ test("Rating in matrix", async (t) => {
     await takeElementScreenshot("rating-in-matrix.png", question, t, comparer);
   });
 });
+
+test("Rating long item", async (t) => {
+  await wrapVisualTest(t, async (t, comparer) => {
+    await setJSON({
+      "logoPosition": "right",
+      "pages": [
+        {
+          "name": "page1",
+          "elements": [
+            {
+              "type": "rating",
+              "name": "question1",
+              "autoGenerate": false,
+              "rateValues": [
+                1,
+                {
+                  "value": 2,
+                  "text": "item 2"
+                },
+                {
+                  "value": 3,
+                  "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                },
+                4,
+                5
+              ]
+            }
+          ]
+        }
+      ]
+    });
+    await t.resizeWindow(1900, 1000);
+    const question = Selector("div[data-name=question1]");
+    await takeElementScreenshot("rating-long-item.png", question, t, comparer);
+    await t.click(Selector("span").withText("Lorem"));
+    await takeElementScreenshot("rating-long-item-edit.png", question, t, comparer);
+  });
+});
