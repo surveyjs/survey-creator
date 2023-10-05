3705a6c477d2abf6ea8332ea6f40c39cf31d79ba
Dmitry Kuzin <dk981234@gmail.com>
2023-10-05 17:36:15 +0400

    Work for #4694: add i18n dir in 'survey-creator-core' build

--- packages/survey-creator-core/webpack.i18n.js
+++ packages/survey-creator-core/webpack.i18n.js
@@ -1,7 +1,8 @@
 "use strict";
-
 const webpackCommonConfigCreator = require("./webpack.config");
-var path = require("path");
+const path = require("path");
+const fs = require("fs");
+
 const { merge } = require("webpack-merge");
 var packageJson = require("./package.json");
 
@@ -19,13 +20,31 @@ const config = {
   }
 };
 
+function patchEntries() {
+  fs.readdirSync(path.resolve(__dirname, "./src/localization")).forEach(file => {
+    var extension = path.extname(file);
+    if (extension.toLowerCase() === ".ts") {
+      config.entry[path.basename(file, extension)] = (path.resolve(__dirname, "./src/localization") + "/" + file);
+    }
+  });
+  config.entry.index = path.resolve(__dirname, "./src/entries/i18n.ts");
+}
+
+function patchFilename(options) {
+  config.output = {};
+  const isProductionBuild = options.buildType === "prod";
+  config.output.filename = (pathData) => {
+    return (pathData.chunk.name == "survey-creator-core.i18n" ? "[name]" : "i18n/[name]") + (isProductionBuild ? ".min" : "") + ".js";
+  };
+}
+
 module.exports = function (options) {
   options.platform = "i18n";
   options.libraryName = "SurveyCreatorLocales";
-
+  patchEntries();
+  patchFilename(options);
   const mainConfig = webpackCommonConfigCreator(options);
   delete mainConfig.entry[packageJson.name];
   mainConfig.plugins.shift();
- 
   return merge(mainConfig, config);
 };
