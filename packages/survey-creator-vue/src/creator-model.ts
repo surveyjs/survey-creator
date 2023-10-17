0423f2126fe31292b58fa7fd45eea3800f76bd93
Dmitry Kuzin <dk981234@gmail.com>
2023-10-17 19:28:27 +0400

    Small refactor

--- packages/survey-creator-vue/src/creator-model.ts
+++ packages/survey-creator-vue/src/creator-model.ts
@@ -4,12 +4,12 @@ import { onBeforeUnmount, shallowRef, type ShallowRef, watch } from "vue";
 
 export function useCreatorModel<T extends Base>(
   createModel: () => T | undefined,
-  getUpdatedModelProps: () => Array<() => any>,
+  getUpdatedModelProps: Array<() => any>,
   clean?: (value: T) => void
 ): ShallowRef<T | undefined> {
   const model = shallowRef<T>();
   const stopWatch = watch(
-    getUpdatedModelProps().map((propFunc) => () => propFunc()),
+    getUpdatedModelProps.map((propFunc) => () => propFunc()),
     () => {
       model.value = createModel();
     },
