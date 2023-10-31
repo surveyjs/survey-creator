e76a2bf4674dd18324d7a21959865e503c03311d
tsv2013 <tsv2013@noreply.github.com>
2023-10-31 16:06:35 +0300

    Fixed stopPropagation name typo

--- packages/survey-creator-vue/src/tabs/designer/PageAddQuestion.vue
+++ packages/survey-creator-vue/src/tabs/designer/PageAddQuestion.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="svc-page__add-new-question svc-btn" v-on:click="e => { item.data.addNewQuestion(model, e); e.stopPropagetios(); }" v-on:mouseleave="e => item.data.hoverStopper(e, e.currentTarget)"
+  <div class="svc-page__add-new-question svc-btn" v-on:click="e => { item.data.addNewQuestion(model, e); e.stopPropagation(); }" v-on:mouseleave="e => item.data.hoverStopper(e, e.currentTarget)"
     data-bind="click: addNewQuestion, key2click, clickBubble: false, event: { mouseover: function(m, e) { hoverStopper(e, $element); }}">
     <span class="svc-text svc-text--normal svc-text--bold" data-bind="text: $data.addNewQuestionText">
     {{item.data.addNewQuestionText}}
     </span>
     <!-- ko with: questionTypeSelectorModel -->
-    <button type="button" v-on:click="e => { item.data.questionTypeSelectorModel.action(); e.stopPropagetios(); }"
+    <button type="button" v-on:click="e => { item.data.questionTypeSelectorModel.action(); e.stopPropagation(); }"
       data-bind="click: action, key2click, clickBubble: false, attr: { title: title, 'aria-label': title }"
       class="svc-page__question-type-selector">
       <sv-svg-icon class="svc-page__question-type-selector-icon" v-bind="{ iconName: item.data.questionTypeSelectorModel.iconName, size: 24, title: item.data.questionTypeSelectorModel.title }"></sv-svg-icon>
