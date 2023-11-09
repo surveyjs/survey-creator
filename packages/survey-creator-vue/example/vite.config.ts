995c68d8398648b2bad17994c3f360597c66eb68
Dmitry Kuzin <dk981234@gmail.com>
2023-11-09 16:46:56 +0400

    Add watch command

--- packages/survey-creator-vue/example/vite.config.ts
+++ packages/survey-creator-vue/example/vite.config.ts
@@ -4,16 +4,30 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    preserveSymlinks: true,
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      "survey-core": fileURLToPath(new URL('./node_modules/survey-core', import.meta.url)),
-      "survey-creator-core": fileURLToPath(new URL('./node_modules/survey-creator-core', import.meta.url)),
+export default defineConfig(({ command }) => { 
+  return {
+    plugins: [
+      vue(),
+    ],
+    server: {
+      watch: {
+        ignored: ["survey-creator-vue", "survey-vue3-ui"].map((m) => `!**/node_modules/${m}/**`)
+      }
+    },
+    build: {
+      commonjsOptions: { exclude: ["survey-core", "survey-creator-core"] },
+    },
+    optimizeDeps: {
+      exclude: ["survey-creator-vue", "survey-vue3-ui"],
+      include: ["survey-creator-core", "survey-core"]
+    },
+    resolve: {
+      preserveSymlinks: command == "serve" ? false : true,
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        "survey-core": fileURLToPath(new URL('./node_modules/survey-core', import.meta.url)),
+        "survey-creator-core": fileURLToPath(new URL('./node_modules/survey-creator-core', import.meta.url)),
+      }
     }
-  }
+}
 })
