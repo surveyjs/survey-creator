5160a1acf456920b93324108e7aeac662d21f64a
Aleksey Novikov <novikov82@gmail.com>
2023-12-08 18:51:02 +0300

    #4967 Mobile drop-down (rating question) (#4968)
    * #4967 Mobile drop-down (rating question)
    Fixes #4967
    
    * #4967 - revert test html changes
    
    * #4967 - update etalon
    
    * #4967 - comment test html
    
    * Fixes #4967
    
    * #4967 - update screenshot and skip theme switcher tests
    
    * #4967 - update screenshot and skip theme switcher tests

--- packages/survey-creator-vue/example/src/components/test/ThemePreview.vue
+++ packages/survey-creator-vue/example/src/components/test/ThemePreview.vue
@@ -6,7 +6,7 @@ import Common from './Common.vue';
 document.body.style.setProperty("--sv-default-mark", "true");
 document.body.style.setProperty("--sv-modern-mark", "true");
 import ("survey-core/defaultV2.css");
-import ("survey-core/modern.css");
-import ("survey-core/survey.css");
+// import ("survey-core/modern.css");
+// import ("survey-core/survey.css");
 import ("survey-creator-core/survey-creator-core.css");
 </script>
