2d499e25fbc862b6dd10d4066bb8cd71e3d760b3
tsv2013 <tsv2013@noreply.github.com>
2023-12-13 11:23:01 +0300

    Added vr-test for #4672 - Page navigation does not work in 'pageEditMode="standard"' if designer has no scrollbar

--- visualRegressionTests/tests/designer/page-navigator.ts
+++ visualRegressionTests/tests/designer/page-navigator.ts
@@ -44,6 +44,10 @@ test("On the right side (default)", async (t) => {
     await t.hover(Selector(".svc-page-navigator-item-content:not(.svc-page-navigator-item--selected)"));
     await t.wait(1000);
     await takeElementScreenshot("page-navigator-right-hovered.png", pageNavigatorElement, t, comparer);
+
+    await t.resizeWindow(1920, 1080);
+    await t.wait(1000);
+    await takeElementScreenshot("page-navigator-right-hidden.png", pageNavigatorElement, t, comparer);
   });
 });
 
@@ -76,6 +80,10 @@ test("On the left side", async (t) => {
     await t.hover(Selector(".svc-page-navigator-item-content:not(.svc-page-navigator-item--selected)"));
     await t.wait(1000);
     await takeElementScreenshot("page-navigator-left-hovered.png", pageNavigatorElement, t, comparer);
+
+    await t.resizeWindow(1920, 1080);
+    await t.wait(1000);
+    await takeElementScreenshot("page-navigator-left-hidden.png", pageNavigatorElement, t, comparer);
   });
 });
 
