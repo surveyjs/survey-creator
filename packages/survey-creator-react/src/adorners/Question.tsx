c2f56d69aa83159392514e323c5603b5c8482f3c
Aleksey Novikov <novikov@abrisplatform.com>
2023-12-16 18:28:13 +0300

    #4986 - react

--- packages/survey-creator-react/src/adorners/Question.tsx
+++ packages/survey-creator-react/src/adorners/Question.tsx
@@ -79,7 +79,7 @@ export class QuestionAdornerComponent extends CreatorModelElement<
         {content}
         {this.renderFooter()}
       </div>,
-      undefined, { disableTabStop: this.model.element.isInteractiveDesignElement });
+      undefined, { disableTabStop: !this.model.element.isInteractiveDesignElement });
   }
   protected renderHeader(): JSX.Element {
     return ReactElementFactory.Instance.createElement("svc-question-header", { model: this.model });
