4f14a617c45b244ca236816431da4f9b95245079
Aleksey Novikov <novikov@abrisplatform.com>
2024-02-04 22:32:11 +0300

    https://github.com/surveyjs/service/issues/1907 add tests

---
+++ packages/survey-creator-angular/example/angular-ui/src/app/test/test-no-license.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+import { SurveyCreatorModel } from "survey-creator-core";
+import { TestDefaultComponent } from "./default.component";
+@Component({
+  selector: "test-theme-preview",
+  templateUrl: "./test.component.html",
+  encapsulation: ViewEncapsulation.None
+})
+export class NoLicenseComponent extends TestDefaultComponent {
+  constructor() {
+    super();
+  }
+  protected override createCreator(): void {
+    this.creator = new SurveyCreatorModel({ showThemeTab: true });
+  }
+}
---
+++ packages/survey-creator-vue/example/src/components/test/NoLicense.vue
@@ -0,0 +1,6 @@
+<template>
+    <Common :options="{ showThemeTab: true }" :use-slk="true"></Common>
+</template>
+<script lang="ts" setup>
+import Common from './Common.vue';
+</script>
