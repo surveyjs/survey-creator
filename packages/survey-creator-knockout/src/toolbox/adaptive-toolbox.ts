bf08336bb1be40edcee00ad00193d69b48b00bb8
Aleksey Novikov <novikov82@gmail.com>
2024-05-09 18:31:32 +0300

    #2231 change toolbox selector in knockout Fixes #2231

--- packages/survey-creator-knockout/src/toolbox/adaptive-toolbox.ts
+++ packages/survey-creator-knockout/src/toolbox/adaptive-toolbox.ts
@@ -7,7 +7,7 @@ ko.components.register("svc-adaptive-toolbox", {
   viewModel: {
     createViewModel: (params: any, componentInfo: any) => {
       const model: ToolboxViewModel = new ToolboxViewModel(params.model);
-      const container = componentInfo.element.querySelector(".svc-toolbox");
+      const container = componentInfo.element.querySelector(".svc-toolbox__scroller");
       const manager: VerticalResponsivityManager = new VerticalResponsivityManager(container, params.model.toolbox, ".svc-toolbox__tool:not(.sv-dots)");
       ko.utils.domNodeDisposal.addDisposeCallback(componentInfo.element, () => {
         manager.dispose();
