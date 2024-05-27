1e3e85242757515966c479506604bfe293e0b6d0
Aleksey Novikov <novikov82@gmail.com>
2024-05-27 17:31:21 +0300

    add toolbox tool styles

--- packages/survey-creator-core/src/toolbox.ts
+++ packages/survey-creator-core/src/toolbox.ts
@@ -105,6 +105,15 @@ export class QuestionToolboxItem extends Action implements IQuestionToolboxItem 
     const type = this.typeName;
     return !!type && Serializer.isDescendantOf(type, "panelbase");
   }
+
+  public get classNames(): string {
+    return new CssClassBuilder()
+      .append("svc-toolbox__tool")
+      .append(this.css)
+      .append("svc-toolbox__tool--hovered", this.isHovered)
+      .append("svc-toolbox__tool--pressed", this.isPressed)
+      .toString();
+  }
 }
 
 /**
