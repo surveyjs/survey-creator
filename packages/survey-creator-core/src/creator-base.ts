6f0d6857b14ba4c89bce92089a5d7d6b34348d85
dmitrykurmanov <kurmanov.work@gmail.com>
2024-06-24 13:08:48 +0400

    work for the https://github.com/surveyjs/survey-creator/issues/5377(survey themes + toolbox-search)

--- packages/survey-creator-core/src/creator-base.ts
+++ packages/survey-creator-core/src/creator-base.ts
@@ -2048,7 +2048,10 @@ export class SurveyCreatorModel extends Base
   public createSurvey(json: any, reason: string, model?: any, callback?: (survey: SurveyModel) => void, area?: string): SurveyModel {
     const survey = this.createSurveyCore(json, reason);
 
-    if (reason !== "designer" && reason !== "test" && reason !== "theme") { survey.fitToContainer = false; }
+    if (reason !== "designer" && reason !== "test" && reason !== "theme") {
+      survey.fitToContainer = false;
+      survey.applyTheme(designTabSurveyThemeJSON);
+    }
 
     if (reason === "designer" || reason === "modal-question-editor") {
       initializeDesignTimeSurveyModel(survey, this);
