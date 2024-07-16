a9f0fe0ab5244f7756ea5e98e268fd987971e785
tsv2013 <tsv2013@noreply.github.com>
2024-07-16 15:03:26 +0300

    Fixed #5684 - Expand/collapse panels/questions and lazy rendering issue

--- testCafe/designer/surface.ts
+++ testCafe/designer/surface.ts
@@ -1,5 +1,5 @@
 import { url, setJSON, collapseButtonSelector, getTabbedMenuItemByText, creatorTabPreviewName, explicitErrorHandler, generalGroupName, getPropertyGridCategory, getVisibleElement } from "../helper";
-import { Selector } from "testcafe";
+import { ClientFunction, Selector } from "testcafe";
 const title = "Designer surface";
 
 fixture`${title}`.page`${url}`;
@@ -211,4 +211,86 @@ test("Check sidebar shadow height", async (t) => {
   await setJSON({});
   await t.click('button[title="Survey settings"]');
   await t.expect(Selector(".svc-side-bar__shadow").offsetHeight).lte(480);
+});
+
+test("Question adorner - update other rows with lazy rendering on question collapsed", async (t) => {
+  await t.resizeWindow(1200, 800);
+  const jsonL = {
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [
+          {
+            "type": "imagepicker",
+            "name": "question1",
+            "choices": [
+              {
+                "value": "Image 1",
+                "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+              },
+              {
+                "value": "Image 2",
+                "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+              },
+              {
+                "value": "Image 3",
+                "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+              },
+              {
+                "value": "Image 4",
+                "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+              }
+            ],
+            "imageFit": "cover"
+          }
+        ]
+      },
+      {
+        "name": "page2",
+        "elements": [
+          {
+            "type": "panel",
+            "name": "panel1",
+            "elements": [
+              {
+                "type": "imagepicker",
+                "name": "question2",
+                "choices": [
+                  {
+                    "value": "Image 1",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg"
+                  },
+                  {
+                    "value": "Image 2",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg"
+                  },
+                  {
+                    "value": "Image 3",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg"
+                  },
+                  {
+                    "value": "Image 4",
+                    "imageLink": "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg"
+                  }
+                ],
+                "imageFit": "cover"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  await ClientFunction(() => {
+    window["creator"].expandCollapseButtonVisibility = "always";
+  })();
+  await setJSON(jsonL);
+  const qContent = Selector(".svc-question__content");
+  const qCollapseButton = Selector(".svc-question__content #collapse");
+  await t.expect(qContent.count).eql(1);
+  await t.expect(qCollapseButton.count).eql(1);
+
+  await t.click(qCollapseButton);
+  await t.expect(qContent.count).eql(3);
+  await t.expect(qCollapseButton.count).eql(3);
 });
