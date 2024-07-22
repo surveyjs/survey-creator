6c19f98dcb618331f0a3edc762f4c50a9d9a5940
Dmitry Kuzin <dk981234@gmail.com>
2024-07-22 14:06:53 +0400

    Remove dts generator plugin from build core

--- webpack-plugins/webpack-dts-generator.js
+++ webpack-plugins/webpack-dts-generator.js
@@ -5,18 +5,13 @@ const child_process = require('child_process');
 module.exports = function DtsGeneratorProgressPlugin(options) {
   return new options.webpack.ProgressPlugin(function(progress) {
     if (progress === 1) {
-      console.log("typings start");
-      console.log("typescript start");
-      child_process.execSync("tsc --p tsconfig.typing.json", { stdio: "inherit" });
-      console.log("typescript end");
-
+      console.log("typings entry point start");
       const importName = options.importName || "entries/index";
-      // const content = "\ndeclare module \"" + options.moduleName + "\" { import main = require(\"" + importName + "\"); export = main; }";
       const content = "\nexport * from \"./typings/" + importName + "\";";
       console.log("\"" + content + "\"");
       console.log(options.filePath);
       fs.writeFile(options.filePath, content, { flag: 'w' }, err => { if (err) { console.error(err); } });
-      console.log("typings end");
+      console.log("typings entry point end");
     }
   });
 };
