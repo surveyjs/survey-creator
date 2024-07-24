bd5b503c5ef9f2a511447146847f9ea1dd69cfcb
Andrew <andrew.telnov@gmail.com>
2024-07-24 18:20:27 +0300

    Properties presets: Edit category name in adorner actions #5215 (#5724)

--- packages/creator-presets-core/src/components/question.ts
+++ packages/creator-presets-core/src/components/question.ts
@@ -14,5 +14,16 @@ export class QuestionPresetAdornerViewModel extends QuestionAdornerViewModel {
         title: title
       }));
     }
+    if(this.element.isPanel) {
+      items.push(new Action({
+        id: "svc_category_name",
+        component: "sv-action-texteditor-item",
+        location: "start",
+        enabled: !(<any>this.element).isDefaultPanel,
+        visibleIndex: 0,
+        locTitle: this.element.getLocalizableString("categoryName"),
+        title: "Category name:",
+      }));
+    }
   }
 }
