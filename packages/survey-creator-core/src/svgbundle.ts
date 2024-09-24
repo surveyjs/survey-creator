64849c62f7c4e9af6c7df62d00a3af428487be30
dmitrykurmanov <kurmanov.work@gmail.com>
2024-09-24 16:01:04 +0400

    work for the https://github.com/surveyjs/private-tasks/issues/386

--- packages/survey-creator-core/src/svgbundle.ts
+++ packages/survey-creator-core/src/svgbundle.ts
@@ -1,4 +1,4 @@
 import { SvgRegistry } from "survey-core";
 export var svgBundle;
-const path = (<any>require).context("./images", true, /\.svg$/);
+const path = (<any>require).context("./images-v2", true, /\.svg$/);
 SvgRegistry.registerIconsFromFolder(path);
