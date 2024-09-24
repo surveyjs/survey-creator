8e8d55b62c07fb4ab69919927a1ccaee87ca59df
tsv2013 <tsv2013@noreply.github.com>
2024-09-24 10:14:41 +0300

    Resolved https://github.com/surveyjs/service/issues/2236 - Design Surface - Return to the last question in work after preview (single- and multi-page forms)

--- packages/survey-creator-core/src/creator-base.ts
+++ packages/survey-creator-core/src/creator-base.ts
@@ -1842,6 +1842,7 @@ export class SurveyCreatorModel extends Base
     }
     this.existingPages = {};
     const survey = this.createSurvey({}, "designer", undefined, (survey: SurveyModel) => {
+      survey.skeletonHeight = 188;
       survey.css = defaultV2Css;
       survey.setIsMobile(!!this.isMobileView);
       survey.setDesignMode(true);
@@ -2747,20 +2748,24 @@ export class SurveyCreatorModel extends Base
   private currentFocusTimeout: any;
   public focusElement(element: any, focus: string | boolean, selEl: any = null, propertyName: string = null, startEdit: boolean = null) {
     if (!selEl) selEl = this.getSelectedSurveyElement();
-    if(!selEl) return;
+    if (!selEl) return;
     clearInterval(this.currentFocusInterval);
     clearTimeout(this.currentFocusTimeout);
-    if(this.animationEnabled && this.survey.isLazyRendering) {
-      this.survey.disableLazyRenderingBeforeElement(selEl);
-    }
+    // if(this.animationEnabled && this.survey.isLazyRendering) {
+    //   this.survey.disableLazyRenderingBeforeElement(selEl);
+    // }
     this.currentFocusTimeout = setTimeout(() => {
       this.currentFocusInterval = setInterval(() => {
         const el = document.getElementById(selEl.id);
         if (!!selEl && (focus || startEdit && (!selEl.hasTitle || selEl.isPanel))) {
-          if(!el || this.rootElement.getAnimations({ subtree: true }).filter((animation => animation.effect.getComputedTiming().activeDuration !== Infinity && (animation.pending || animation.playState !== "finished")))[0]) return;
+          if (!el || this.rootElement.getAnimations({ subtree: true }).filter((animation => animation.effect.getComputedTiming().activeDuration !== Infinity && (animation.pending || animation.playState !== "finished")))[0]) return;
           clearInterval(this.currentFocusInterval);
           if (!!el) {
-            SurveyHelper.scrollIntoViewIfNeeded(el.parentElement ?? el, () => { return { block: "start", behavior: this.animationEnabled ? "smooth" : undefined }; }, true);
+            // SurveyHelper.scrollIntoViewIfNeeded(el.parentElement ?? el, () => { return { block: "start", behavior: this.animationEnabled ? "smooth" : undefined }; }, true);
+            const isNeedScroll = SurveyHelper.isNeedScrollIntoView(el.parentElement ?? el, true);
+            if (!!isNeedScroll) {
+              this.survey.scrollElementToTop(selEl, undefined, undefined, selEl.id, false, { block: "start", behavior: this.animationEnabled ? "smooth" : undefined });
+            }
             if (!propertyName && el.parentElement) {
               let elToFocus: HTMLElement = (typeof (focus) === "string") ? el.parentElement.querySelector(focus) : el.parentElement;
               elToFocus && elToFocus.focus({ preventScroll: true });
