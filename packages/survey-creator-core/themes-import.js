0895bb57a195ca11a1fbceb9879ef11f211d602e
OlgaLarina <olga.larina.dev@gmail.com>
2024-10-08 12:53:15 +0300

    work for https://github.com/surveyjs/private-tasks/issues/382 - refactoring theme modules

--- packages/survey-creator-core/themes-import.js
+++ packages/survey-creator-core/themes-import.js
@@ -51,12 +51,23 @@ function capitalizedFirstLetter(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-function writeTheme(themeName, palleteName, cssVariables) {
-  const fileName = [themeName, palleteName].join("-");
-  const variableName = [capitalizedFirstLetter(themeName), capitalizedFirstLetter(palleteName)].join("");
-  const theme = { themeName: fileName, cssVariables: cssVariables };
+function writeTheme(themeName, cssVariables, variableName) {
+  const theme = { themeName, cssVariables };
   const themeJson = JSON.stringify(theme, null, 2);
   const result = `const Theme = ${themeJson};\nexport default Theme;\nexport const ${variableName} = Theme;`;
+  fs.writeFileSync(_dirPath + themeName + ".ts", result);
+}
+
+function writeThemePalette(themeName, paletteName, cssVariables) {
+  const fileName = [themeName, paletteName].join("-");
+  const baseThemeVariable = capitalizedFirstLetter(themeName);
+  const variableName = [baseThemeVariable, capitalizedFirstLetter(paletteName)].join("");
+
+  const theme = { themeName: fileName, cssVariables: cssVariables };
+  const themeJson = JSON.stringify(theme, null, 2);
+  const importsString = `import { assign } from "./utils";\nimport { ${baseThemeVariable} } from "./${themeName}";\n`;
+  const useImportString = `const themeCssVariables = {};\nassign(themeCssVariables, ${baseThemeVariable}.cssVariables, Theme.cssVariables);\nassign(Theme, { cssVariables: themeCssVariables });\n`;
+  const result = `${importsString}\nconst Theme = ${themeJson};\n${useImportString}\nexport default Theme;\nexport const ${variableName} = Theme;`;
   fs.writeFileSync(_dirPath + fileName + ".ts", result);
 
   return `import ${variableName}Theme from "./${fileName}";\nexport const ${variableName} = ${variableName}Theme;\n`;
@@ -69,15 +80,12 @@ Object.keys(themeNameMap).forEach(themeName => {
   if(themeName !== baseThemeName && !!baseThemeCssVariable) {
     const curThemeCssVariables = getCssVariablesFormFile(themeName + ".css");
     const distinctions = {};
-    Object.keys(curThemeCssVariables || {}).forEach(cssVariable => {
-      const curVariable = curThemeCssVariables[cssVariable];
-      if(usedCssVariablesList.indexOf(cssVariable) !== -1) {
-        distinctions[cssVariable] = curVariable
-      }
-      if(curVariable !== baseThemeCssVariable[cssVariable]) {
-        distinctions[cssVariable] = curVariable;
-      } else if(usedCssVariablesList.indexOf(curVariable) !== -1) {
-        console.log("mis variable " + curVariable);
+    Object.keys(curThemeCssVariables || {}).forEach(variableKey => {
+      const variableValue = curThemeCssVariables[variableKey];
+      if(variableValue !== baseThemeCssVariable[variableKey]) {
+        distinctions[variableKey] = variableValue;
+      } else if(usedCssVariablesList.indexOf(variableKey) !== -1) {
+        distinctions[variableKey] = variableValue
       }
     });
     themeDistinctions[themeNameMap[themeName]] = distinctions;
@@ -89,14 +97,14 @@ Object.keys(themeNameMap).forEach(themeName => {
   let indexFileContent = "";
   if(themeName !== baseThemeName) {
     const currentTheme = themeNameMap[themeName];
-    console.log(currentTheme);
-    const palettes = creatorThemePalettes[currentTheme];
-    (palettes || []).forEach(palleteName => {
-      const curPaletteCssVariables = getCssVariablesFormFile(currentTheme + "/" + palleteName + ".css");
+    console.log("Theme - " + currentTheme);
+    writeTheme(currentTheme, themeDistinctions[currentTheme], capitalizedFirstLetter(currentTheme));
 
-      const newCssVariables = {};
-      Object.assign(newCssVariables, themeDistinctions[currentTheme], curPaletteCssVariables);
-      indexFileContent += writeTheme(currentTheme, palleteName, newCssVariables);
+    const palettes = creatorThemePalettes[currentTheme];
+    (palettes || []).forEach(paletteName => {
+      console.log("Palette - " + paletteName);
+      const curPaletteCssVariables = getCssVariablesFormFile(currentTheme + "/" + paletteName + ".css");
+      indexFileContent += writeThemePalette(currentTheme, paletteName, curPaletteCssVariables);
     });
   }
   fs.writeFileSync(_dirPath + "index.ts", indexFileContent);
