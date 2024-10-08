69fd1974666a97845f8bb44ee754a003777b97cd
OlgaLarina <olga.larina.dev@gmail.com>
2024-10-08 10:03:39 +0300

    work for https://github.com/surveyjs/private-tasks/issues/38 add used css variables inti theme

--- packages/survey-creator-core/themes-import.js
+++ packages/survey-creator-core/themes-import.js
@@ -4,6 +4,7 @@ const baseThemeName = "v2-20";
 const sourcePath = "./src/themes/predefined-themes/";
 const _dirPath = "./src/themes/";
 const regularExpression = /(?<var1>--\w*(-*\w*)*)\s?:\s?(?<var2>.*[^;]);/gi;
+const cssVariablesRegExp = /(?<variable>--\w*(-*\w*)*)/gi;
 var themeNameMap = {
   "v2-20": "sc2020",
   "v2-24": "default"
@@ -13,6 +14,24 @@ var creatorThemePalettes = {
 }
 const baseThemeCssVariable = getCssVariablesFormFile(baseThemeName + ".css");
 const themeDistinctions = {};
+
+function getUsedCssVariables(path) {
+  fs.readdirSync(path, {withFileTypes: true}).forEach(item => {
+    if(item.isDirectory()) {
+      getUsedCssVariables(path + item.name + "/");
+    } else if(item.name.indexOf(".scss") === (item.name.length - 5 )) {
+      const data = fs.readFileSync(path + item.name, "utf8");
+
+      const matches = data.matchAll(cssVariablesRegExp);
+      Array.from(matches, m => {
+        const variable = m.groups["variable"];
+        if(usedCssVariablesList.indexOf(variable) === -1) {
+          usedCssVariablesList.push(variable);
+        }
+      });    
+    }
+  });
+}
 
 function getCssVariablesFormFile(fileName) {
   try {
@@ -43,13 +62,22 @@ function writeTheme(themeName, palleteName, cssVariables) {
   return `import ${variableName}Theme from "./${fileName}";\nexport const ${variableName} = ${variableName}Theme;\n`;
 }
 
+const usedCssVariablesList = [];
+getUsedCssVariables("./src/");
+
 Object.keys(themeNameMap).forEach(themeName => {
   if(themeName !== baseThemeName && !!baseThemeCssVariable) {
     const curThemeCssVariables = getCssVariablesFormFile(themeName + ".css");
     const distinctions = {};
     Object.keys(curThemeCssVariables || {}).forEach(cssVariable => {
-      if(curThemeCssVariables[cssVariable] !== baseThemeCssVariable[cssVariable]) {
-        distinctions[cssVariable] = curThemeCssVariables[cssVariable];
+      const curVariable = curThemeCssVariables[cssVariable];
+      if(usedCssVariablesList.indexOf(cssVariable) !== -1) {
+        distinctions[cssVariable] = curVariable
+      }
+      if(curVariable !== baseThemeCssVariable[cssVariable]) {
+        distinctions[cssVariable] = curVariable;
+      } else if(usedCssVariablesList.indexOf(curVariable) !== -1) {
+        console.log("mis variable " + curVariable);
       }
     });
     themeDistinctions[themeNameMap[themeName]] = distinctions;
