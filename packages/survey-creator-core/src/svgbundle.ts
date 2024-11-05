0a2be427a95a32b31054bdc0084eb76f5dd3c6da
dmitrykurmanov <kurmanov.work@gmail.com>
2024-11-05 14:21:05 +0400

    work for the https://github.com/surveyjs/private-tasks/issues/386

--- packages/survey-creator-core/src/svgbundle.ts
+++ packages/survey-creator-core/src/svgbundle.ts
@@ -1,6 +1,6 @@
 export var svgBundle: {V1?: string, V2?: string} = {};
 
-if ((<any>require).context) {
+if (typeof jest === "undefined") {
   svgBundle.V1 = (<any>require).context("./images-v1", true, /\.svg$/);
   svgBundle.V2 = (<any>require).context("./images-v2", true, /\.svg$/);
 }
