a9b604480b8cbb2fb817b77e9b088c86ac08c245
Aleksey Novikov <novikov82@gmail.com>
2024-12-09 15:18:45 +0300

    #6196 fix for initial state Fixes #6196

--- packages/survey-creator-core/src/toolbox.ts
+++ packages/survey-creator-core/src/toolbox.ts
@@ -466,6 +466,9 @@ export class QuestionToolbox
   @property({ defaultValue: false }) isFlyoutToCompactRunning: boolean;
 
   private getAnimationOptions(): IAnimationConsumer {
+    const onBeforeRunAnimation = (el: HTMLElement) => {
+      el.style.setProperty("--animation-width", getComputedStyle(el).width);
+    };
     return {
       getAnimatedElement: () => {
         return this.rootElement?.querySelector(".svc-toolbox__panel");
@@ -475,13 +478,14 @@ export class QuestionToolbox
       getLeaveOptions: () => {
         return {
           cssClass: "svc-toolbox__panel--leave",
-          onAfterRunAnimation: () => { this.isFlyoutToCompactRunning = false; }
+          onAfterRunAnimation: () => { this.isFlyoutToCompactRunning = false; },
+          onBeforeRunAnimation: onBeforeRunAnimation
         };
       },
       getEnterOptions: () => {
         return {
           cssClass: "svc-toolbox__panel--enter",
-          onBeforeRunAnimation: (el) => { el.style.setProperty("--animation-width", getComputedStyle(el).width); }
+          onBeforeRunAnimation: onBeforeRunAnimation
         };
       }
     };
