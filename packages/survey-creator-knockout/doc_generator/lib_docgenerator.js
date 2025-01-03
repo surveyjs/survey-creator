491aeae3a19a0453d0b52a88172d4aed6eb97a8d
Dmitry <dk981234@gmail.com>
2025-01-03 20:45:21 +0700

    Remove v1, knockout (#6360)
    * Remove styles manager references + fix paths to library packages
    
    * Remove creator v1 and creator knockout packages
    
    * Fix package.json version
    
    * Format ymls
    
    * Delete V1 creator from pipelines
    
    * Delete creator v2 knockout from pipelines
    
    * Rename library core build artifact in pipelines
    
    * Fix pipelines
    
    * Fix vrt pipelines
    
    * Fix react build: make react build be independent of node modules from library project
    
    * Fix versions for package.json
    
    * Fix presets build

--- packages/survey-creator-knockout/doc_generator/lib_docgenerator.js
+++
@@ -1,6 +0,0 @@
-var doc = require("surveyjs-doc-generator");
-var ts = require("typescript");
-
-doc.generateDocumentation(process.argv.slice(2), {
-    target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS
-});
