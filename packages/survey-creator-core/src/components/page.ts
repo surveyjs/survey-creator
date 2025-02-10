610701b569dfc04af441a1b9edc5aa411a337dee
Aleksey Novikov <novikov82@gmail.com>
2025-02-10 12:48:37 +0300

    #6555 Empty page's placeholder doesn't collapse animated. Fixes #6555

--- packages/survey-creator-core/src/components/page.ts
+++ packages/survey-creator-core/src/components/page.ts
@@ -345,7 +345,7 @@ export class PageAdorner extends SurveyElementAdornerBase<PageModel> {
 
   protected getInnerAnimatedElements() {
     const cssClasses = this.surveyElement.cssClasses;
-    if (cssClasses.pageRow) return [].slice.call(this.rootElement?.querySelectorAll(`:scope .svc-page__footer, :scope ${classesToSelector(this.surveyElement.cssRoot)} > .svc-row`));
+    if (cssClasses.pageRow) return [].slice.call(this.rootElement?.querySelectorAll(`:scope .svc-page__footer, :scope .svc-page__placeholder_frame, :scope ${classesToSelector(this.surveyElement.cssRoot)} > .svc-row`));
     return null;
   }
   public onPageSelected() { }
