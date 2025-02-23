05071ea0f3f5343ca5508e5e95642f00d848796b
Dmitry Kuzin <dk981234@gmail.com>
2025-02-24 00:01:11 +0400

    Fix build icons (3)

--- packages/survey-creator-core/rollup.config.js
+++ packages/survey-creator-core/rollup.config.js
@@ -5,6 +5,7 @@ const commonjs = require("@rollup/plugin-commonjs");
 const bannerPlugin = require("rollup-plugin-license");
 
 const path = require("path");
+const svgLoader = require("svg-inline-loader");
 const fg = require("fast-glob");
 const readFile = require("fs").readFileSync;
 const VERSION = require("./package.json").version;
@@ -45,7 +46,7 @@ module.exports = (options) => {
           if (Object.keys(iconsMap).includes(id)) {
             const icons = {};
             for (const iconPath of await fg.glob(iconsMap[id])) {
-              icons[path.basename(iconPath).replace(/\.svg$/, "").toLocaleLowerCase()] = readFile(iconPath).toString();
+              icons[path.basename(iconPath).replace(/\.svg$/, "").toLocaleLowerCase()] = svgLoader.getExtractedSVG(readFile(iconPath).toString());
             }
             return `export default ${JSON.stringify(icons, undefined, "\t")}`;
           }
