7937c11ff4f22d1593e5194ab46d2788729841a7
Andrew Telnov <andrew.telnov@gmail.com>
2025-03-10 20:08:01 +0200

    Localization: "survey-creator-core" doesn't export the getLocaleStrings() method fix #6689

--- packages/survey-creator-core/src/entries/index.ts
+++ packages/survey-creator-core/src/entries/index.ts
@@ -5,7 +5,7 @@ Version = `${process.env.VERSION}`;
 import "../utils/context-button.scss";
 
 export { enStrings } from "../localization/english";
-export { editorLocalization, defaultStrings } from "../editorLocalization";
+export { editorLocalization, defaultStrings, getLocaleStrings } from "../editorLocalization";
 export { editorLocalization as localization } from "../editorLocalization";
 export * from "../creator-settings";
 export { ICreatorOptions } from "../creator-options";
