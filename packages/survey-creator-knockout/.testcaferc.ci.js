0b5a64250887b275cfea359f5a4ded31bcbb5dcb
tsv2013 <tsv2013@noreply.github.com>
2025-03-17 14:07:18 +0300

    Try to stabilize f-tests

---
+++ packages/survey-creator-knockout/.testcaferc.ci.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+  "browsers": [`chrome:${require("puppeteer").executablePath()}:headless`],
+  "nativeAutomation": "true",
+  "concurrency": 1,
+  "quarantineMode": {
+    "successThreshold": 1,
+    "attemptLimit": 3
+  }
+}
