4d72f6587d67d0e0aa712e1029ccb60b74f57a18
Andrew Telnov <andrew.telnov@gmail.com>
2025-03-22 13:42:58 +0200

    Add creator.translateStrings function

--- packages/survey-creator-core/src/utils/creator-locstrings.ts
+++ packages/survey-creator-core/src/utils/creator-locstrings.ts
@@ -1,16 +1,29 @@
-import { LocalizableString, SurveyModel, Base, JsonObjectProperty, Serializer } from "survey-core";
+import { LocalizableString, SurveyModel, Base, JsonObjectProperty, Serializer, MatrixCells, QuestionMatrixModel,
+  settings as surveySettings } from "survey-core";
 
 export interface ICollectLocalizableStringsOptions {
-  canShowProperty(obj: Base, property: JsonObjectProperty, isEmpty: boolean, isShowing: boolean): boolean;
-  onAddLocalizedString(obj: Base, property: JsonObjectProperty, locStr: LocalizableString): void;
+  canShowProperty(reason: string, obj: Base, property: JsonObjectProperty, locStr: LocalizableString, isShowing: boolean, defaultValue?: any): boolean;
+  onAddLocalizedString(obj: Base, property: JsonObjectProperty, locStr: LocalizableString, translatedObj: any): void;
+  onCreateNewTranslateObj(obj: Base, property: JsonObjectProperty, translateObj: any): any;
 }
 
 export function collectLocalizableStrings(survey: SurveyModel, options: ICollectLocalizableStringsOptions, translateObj?: any): void {
   collectStringsByObj(survey, options, translateObj);
 }
-export function getUnlocalizedStrings(survey: SurveyModel, options: any, ...locales: Array<string>): Array<LocalizableString> {
+export function getUnlocalizedStrings(survey: SurveyModel, creatorOptions: any, loc: string): Array<LocalizableString> {
   const res = new Array<LocalizableString>();
-
+  const options: ICollectLocalizableStringsOptions = {
+    canShowProperty: (reason: string, obj: Base, property: JsonObjectProperty, locStr: LocalizableString, isShowing: boolean, defaultValue?: any): boolean => {
+      return isShowing && (!locStr || !locStr?.isEmpty);// || !!defaultValue;
+    },
+    onAddLocalizedString: (obj: Base, property: JsonObjectProperty, locStr: LocalizableString, translatedObj: any): void => {
+      res.push(locStr);
+    },
+    onCreateNewTranslateObj: (obj: Base, property: JsonObjectProperty, translateObj: any): any => {
+      return undefined;
+    }
+  };
+  collectStringsByObj(survey, options, undefined);
   return res;
 }
 
@@ -22,28 +35,24 @@ function collectStringsByObj(obj: Base, options: ICollectLocalizableStringsOptio
   if (!obj || !obj.getType) return;
   let properties = getLocalizedProperties(obj, options);
   for (let i = 0; i < properties.length; i++) {
-    addLocalizationItem(obj, properties[i]);
+    addLocalizationItem(obj, options, properties[i], translateObj);
   }
-  properties = this.getArrayProperties(this.obj);
+  properties = getArrayProperties(obj);
   for (var i = 0; i < properties.length; i++) {
     var property = properties[i];
-    var value = this.obj[property.name];
+    var value = obj[property.name];
     //If ItemValue array?
     if (isItemValueArray(value)) {
-      if (this.canShowProperty(property, Array.isArray(value) && value.length > 0)) {
-        this.addNewGroup(new TranslationGroup(
-          property.name,
-          value,
-          this.translation,
-          editorLocalization.getPropertyName(property.name),
-          true
-        ));
+      if (canShowProperty("itemvalue-array", options, obj, property, Array.isArray(value) && value.length > 0)) {
+        createGroup(options, obj[property.name], property, translateObj);
       }
     } else {
-      createGroups(value, property);
+      for (let i = 0; i < value.length; i++) {
+        createGroup(options, value[i], property, translateObj);
+      }
     }
   }
-  createMatrixCellsGroup();
+  createMatrixCellsGroup(options, obj, translateObj);
 }
 function isItemValueArray(val: any): boolean {
   return (
@@ -56,60 +65,32 @@ function isItemValueArray(val: any): boolean {
     !!val[0]["setLocText"]
   );
 }
-function addLocalizationItem(obj: any, property: JsonObjectProperty): void {
-  const locStr = <LocalizableString>obj[property.serializationProperty];
-  if (!!locStr) {
-
-  }
-  const defaultValue = this.getDefaultValue(obj, property);
-  if (!this.showAllStrings && !defaultValue && locStr.isEmpty) return null;
-  const editLocale = this.translation?.getEditLocale();
-  if (!!editLocale && !!locStr.getLocaleText(editLocale)) return null;
-  if (!this.canShowProperty(property, locStr.isEmpty)) return null;
-  return new TranslationItem(
-    property.name,
-    locStr,
-    defaultValue,
-    this.translation,
-    obj,
-    property
-  );
-}
-
-function createItemValuesLocale(obj: Base, options: ICollectLocalizableStringsOptions, translateObj: any): void {
-  const editMode = this.translation?.getEditLocale();
-  for (let i = 0; i < this.obj.length; i++) {
-    const val = this.obj[i];
-
-    var canAdd =
-      this.showAllStrings || !val.locText.isEmpty || isNaN(val.value);
-    if (!!editMode && val.locText.getLocaleText(editMode)) {
-      canAdd = false;
-    }
-    if (!canAdd) continue;
-    var item = new TranslationItem(
-      val.value,
-      val.locText,
-      val.value,
-      this.translation,
-      val
-    );
-    this.itemValues.push(item);
-    this.addCustomPropertiesForItemValue(this.obj[i], item);
+function addLocalizationItem(obj: any, options: ICollectLocalizableStringsOptions, property: JsonObjectProperty, translateObj: any): void {
+  const locStr = getLocalizableStr(obj, property);
+  if(!locStr) return;
+  const defaultValue = getDefaultValue(obj, property);
+  if(canShowProperty("add-locsr", options, obj, property, !locStr.isEmpty, defaultValue)) {
+    options.onAddLocalizedString(obj, property, locStr, translateObj);
   }
 }
-function addCustomPropertiesForItemValue(
-  itemValue: any,
-  textItem: TranslationItem
-) {
-  var locProperties = this.getLocalizedProperties(itemValue);
-  for (var i = 0; i < locProperties.length; i++) {
-    if (locProperties[i].name == "text") continue;
-    var item = addLocalizationItem(itemValue, locProperties[i]);
-    if (!!item) {
-      item.customText = textItem.text + " (" + item.localizableName + ")";
-      item.name = itemValue.value + "." + item.name;
-      this.itemValues.push(item);
+
+function createItemValuesLocale(obj: any, options: ICollectLocalizableStringsOptions, translateObj: any): void {
+  for (let i = 0; i < obj.length; i++) {
+    const val = obj[i];
+    addPropertiesForItemValue(val, options, translateObj);
+  }
+}
+function addPropertiesForItemValue(itemValue: any, options: ICollectLocalizableStringsOptions, translateObj: any) {
+  const locProperties = getLocalizedProperties(itemValue, options);
+  for(let i = 0; i < locProperties.length; i++) {
+    const prop = locProperties[i];
+    const isText = prop.name === "text";
+    const reason = isText ? "itemvalue-text" : "itemvalue-customprop";
+    const defValue = isText ? itemValue.value : undefined;
+    const canshow = canShowProperty(reason, options, itemValue, prop, true, defValue);
+    if(!canshow && isText) break;
+    if(canshow) {
+      options.onAddLocalizedString(itemValue, prop, getLocalizableStr(itemValue, prop), translateObj);
     }
   }
 }
@@ -120,24 +101,72 @@ function getLocalizedProperties(obj: any, options: ICollectLocalizableStringsOpt
     const property = properties[i];
     if (property.readOnly || !property.visible || !property.isSerializable || !property.isLocalizable) continue;
     const isShowing = ["url", "file"].indexOf(property.type) < 0;
-    if (this.canShowProperty(property, !!obj[property.name], isShowing)) {
+    if (canShowProperty("get-property", options, obj, property, isShowing)) {
       res.push(property);
     }
   }
   return res;
 }
-function createGroups(value: any, property: JsonObjectProperty) {
-  for (let i = 0; i < value.length; i++) {
-    const obj = value[i];
-    if (!!obj && obj.getType) {
-      let name = obj["name"];
-      let text = name;
-      if (!name) {
-        const index = "[" + i.toString() + "]";
-        name = property.name + index;
-        text = editorLocalization.getPropertyName(property.name) + index;
-      }
-      this.addNewGroup(new TranslationGroup(name, obj, this.translation, text));
+function getArrayProperties(obj: any): Array<JsonObjectProperty> {
+  var res = new Array<JsonObjectProperty>();
+  var properties = Serializer.getPropertiesByObj(obj);
+  for (var i = 0; i < properties.length; i++) {
+    var property = properties[i];
+    if (property.isSerializable === false) continue;
+    var value = obj[property.name];
+    if (!!value && Array.isArray(value) && value.length > 0) {
+      res.push(property);
     }
   }
+  return res;
+}
+
+function getLocalizableStr(obj: Base, prop: JsonObjectProperty): LocalizableString {
+  return obj[prop.serializationProperty];
+}
+
+function createGroup(options: ICollectLocalizableStringsOptions, obj: any, property: JsonObjectProperty, translateObj: any) {
+  if (!!obj && (obj.getType || Array.isArray(obj))) {
+    translateObj = options.onCreateNewTranslateObj(obj, property, translateObj);
+    collectStringsByObj(obj, options, translateObj);
+  }
+}
+function canShowProperty(reason: string, options: ICollectLocalizableStringsOptions, obj: Base, property: JsonObjectProperty, isShowing: boolean, defaultValue?: any): boolean {
+  if(options.canShowProperty) {
+    return options.canShowProperty(reason, obj, property, getLocalizableStr(obj, property), isShowing, defaultValue);
+  }
+  return isShowing;
+}
+function createMatrixCellsGroup(options: ICollectLocalizableStringsOptions, obj: any, translateObj: any): void {
+  Serializer.getPropertiesByObj(obj).forEach(prop => {
+    if (prop.type === "cells" && canShowProperty("matrix-cell", options, obj, prop, true)) {
+      createMatrixCellsGroupCore(options, obj, prop, translateObj);
+    }
+  });
+}
+function createMatrixCellsGroupCore(options: ICollectLocalizableStringsOptions, obj: any, prop: JsonObjectProperty, translateObj: any): void {
+  const cells = <MatrixCells>obj[prop.name];
+  if (cells.isEmpty) return;
+  const matrix = <QuestionMatrixModel>obj;
+  translateObj = options.onCreateNewTranslateObj(obj, prop, translateObj);
+  const defaultName = surveySettings.matrix.defaultRowName;
+  const rows = [defaultName];
+  matrix.rows.forEach(row => rows.push(row.value));
+  rows.forEach(val => {
+    matrix.columns.forEach(col => {
+      const locStr = cells.getCellDisplayLocText(val, col);
+      if (!!locStr) {
+        options.onAddLocalizedString(obj, prop, locStr, translateObj);
+      }
+    });
+  });
+}
+function getDefaultValue(obj: any, prop: JsonObjectProperty): string {
+  const locStr = getLocalizableStr(obj, prop);
+  if(!locStr) return "";
+  if (prop.name == "title") {
+    if (!!locStr && !obj.isPage && !obj.isPanel && !!locStr.onGetTextCallback)
+      return obj["name"];
+  }
+  return "";
 }
