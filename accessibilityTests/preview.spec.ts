725e36a95e5df88cf9766890c8752240281be0d3
Dmitry Kuzin <dk981234@gmail.com>
2025-03-25 17:15:05 +0400

    Add tests files on other tabs

--- accessibilityTests/preview.spec.ts
+++ accessibilityTests/preview.spec.ts
@@ -1,15 +1,15 @@
 import { test } from "@playwright/test";
 import { injectAxe, checkA11y } from "axe-playwright";
-import { axeOptions, updateCreatorModel, url } from "./helper";
+import { axeOptions, creatorTabPreviewName, getTabbedMenuItemByText, url } from "./helper";
 
-test.describe("designer a11y", () => {
+test.describe("preview a11y", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(`${url}`);
-    await updateCreatorModel(page, { showOneCategoryInPropertyGrid: true });
     await injectAxe(page);
     await page.setViewportSize({ width: 1920, height: 1080 });
   });
-  test("Check designer tab empty", async ({ page }) => {
+  test("Check preview tab empty", async ({ page }) => {
+    await getTabbedMenuItemByText(page, creatorTabPreviewName).click();
     await checkA11y(page, ".svc-creator", { axeOptions });
   });
 });
