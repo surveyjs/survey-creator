46f88ffb4eec5fa20c1f011f0c388887563a162f
Andrew <andrew.telnov@gmail.com>
2025-03-25 09:46:35 +0200

    Feature/ai translator support (#6769)
    * Creator locstring collector
    
    * Add creator.translateStrings function
    
    * Fix bugs for collecting strings
    
    * Rename function
    
    * Add a description
    
    * Add a cross-link
    
    * Add undo/redo transactions
    
    ---------
    
    Co-authored-by: RomanTsukanov <sergeich16@gmail.com>

---
+++ packages/survey-creator-core/tests/ai-translation.tests.ts
@@ -0,0 +1,36 @@
+import { CreatorTester } from "./creator-tester";
+
+test("Translate untranslated strings", () => {
+  const creator = new CreatorTester();
+  creator.JSON = {
+    pages: [
+      {
+        name: "page1",
+        elements: [
+          {
+            type: "checkbox",
+            name: "q1",
+            description: "desc",
+            choices: [
+              { value: 1, text: "text1" },
+              { value: "text2" },
+              { value: 3 }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+  creator.onMachineTranslate.add((sender, options) => {
+    const translatedStrings = new Array<string>();
+    options.strings.forEach(str => { translatedStrings.push(options.toLocale + ": " + str); });
+    options.callback(translatedStrings);
+  });
+  creator.startMachineTranslationTo(["de", "fr"]);
+  const q = creator.survey.getQuestionByName("q1");
+  expect(q.locTitle.isEmpty).toBeTruthy();
+  expect(q.locDescription.getJson()).toStrictEqual({ default: "desc", fr: "fr: desc", de: "de: desc" });
+  expect(q.choices[0].locText.getJson()).toStrictEqual({ default: "text1", fr: "fr: text1", de: "de: text1" });
+  expect(q.choices[1].locText.getJson()).toStrictEqual({ fr: "fr: text2", de: "de: text2" });
+  expect(q.choices[2].locText.isEmpty).toBeTruthy();
+});
