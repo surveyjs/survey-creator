afa464b6e9a0d5f4f6e4b0c59f83fe1b7b249e00
OlgaLarina <olga.larina.dev@gmail.com>
2025-04-14 11:34:48 +0300

    SamMousa chore: cleanup CreatorStylesManager

--- packages/survey-creator-core/src/creator-theme/styles-manager.ts
+++ packages/survey-creator-core/src/creator-theme/styles-manager.ts
@@ -1,43 +1,20 @@
-import { DomDocumentHelper } from "../utils/global_variables_utils";
-
 export class CreatorStylesManager {
-  private static SurveyJSCreatorStylesSheetId = "surveyjs-creator-styles";
   public static Enabled = true;
+  private static sheet: CSSStyleSheet = new CSSStyleSheet();
 
-  static findSheet(styleSheetId: string): any {
-    if (!DomDocumentHelper.isAvailable()) return null;
-    for (let i = 0; i < document.styleSheets.length; i++) {
-      if (!!document.styleSheets[i].ownerNode && (<any>document).styleSheets[i].ownerNode["id"] === styleSheetId) {
-        return <CSSStyleSheet>document.styleSheets[i];
+  public static insertStylesRulesIntoDocument(rules: Array<{ selector: string, styles: string }>): void {
+    if (CreatorStylesManager.Enabled && "undefined" !== typeof document) {
+      document.adoptedStyleSheets.push(CreatorStylesManager.sheet);
+      let cssContent = "";
+      if (Object.keys(rules).length) {
+        rules.forEach((rule) => {
+          try {
+            cssContent += `${rule.selector} { ${rule.styles} }`;
+          } catch(e) { }
+        });
       }
-    }
-    return null;
-  }
 
-  static createSheet(styleSheetId: string): any {
-    if (!DomDocumentHelper.isAvailable()) return null;
-    let style = DomDocumentHelper.createElement("style") as HTMLStyleElement;
-    style.id = styleSheetId;
-    style.appendChild(new Text(""));
-    document.head.appendChild(style);
-    return <CSSStyleSheet>style.sheet;
-  }
-
-  public static insertStylesRulesIntoDocument(rules: Array<{ selector: string, styles: string }>): any {
-    if (CreatorStylesManager.Enabled) {
-      let sheet = CreatorStylesManager.findSheet(CreatorStylesManager.SurveyJSCreatorStylesSheetId);
-      if (!sheet) {
-        sheet = CreatorStylesManager.createSheet(CreatorStylesManager.SurveyJSCreatorStylesSheetId);
-      }
-      if (sheet) {
-        if (Object.keys(rules).length) {
-          rules.forEach((rule) => {
-            try {
-              sheet.insertRule(`${rule.selector} { ${rule.styles} }`, 0);
-            } catch(e) { }
-          });
-        }
-      }
+      CreatorStylesManager.sheet.replaceSync(cssContent);
     }
   }
 }
