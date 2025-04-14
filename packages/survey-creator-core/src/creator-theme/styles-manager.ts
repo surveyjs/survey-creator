b0c043fc382d9fbbf93085308466bff3033d6246
OlgaLarina <olga.larina.dev@gmail.com>
2025-04-14 11:44:40 +0300

    SamMousa chore: cleanup CreatorStylesManager

--- packages/survey-creator-core/src/creator-theme/styles-manager.ts
+++ packages/survey-creator-core/src/creator-theme/styles-manager.ts
@@ -1,10 +1,12 @@
+import { DomDocumentHelper } from "../utils/global_variables_utils";
 export class CreatorStylesManager {
   public static Enabled = true;
   private static sheet: CSSStyleSheet = new CSSStyleSheet();
 
   public static insertStylesRulesIntoDocument(rules: Array<{ selector: string, styles: string }>): void {
-    if (CreatorStylesManager.Enabled && "undefined" !== typeof document) {
-      document.adoptedStyleSheets.push(CreatorStylesManager.sheet);
+    const _document = DomDocumentHelper.getDocument();
+    if (_document && CreatorStylesManager.Enabled) {
+      _document.adoptedStyleSheets.push(CreatorStylesManager.sheet);
       let cssContent = "";
       if (Object.keys(rules).length) {
         rules.forEach((rule) => {
