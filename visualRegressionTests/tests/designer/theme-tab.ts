8473cc635b3d392525b1d70f5a3c481c1ebedeee
Aleksey Novikov <novikov82@gmail.com>
2025-05-07 23:42:51 +0300

    Revert "#6876 - fix tests"
    This reverts commit 3aef6bbee900487c8a71674900dd84243e68813f.

--- visualRegressionTests/tests/designer/theme-tab.ts
+++ visualRegressionTests/tests/designer/theme-tab.ts
@@ -157,6 +157,7 @@ test("Scrollbar theming", async (t) => {
 
     const expandedGroup = Selector(".spg-theme-builder-root .spg-panel.sd-element--expanded");
     await t
+      .click(getPropertyGridCategory("General"))
       .click(expandedGroup.find(".spg-button-group__item-caption").withText("Dark"))
       .hover(Selector(".svd-simulator-main"));
 
