07bc087553e3fd5c5098b909f7df5b43e302d7a4
tsv2013 <tsv2013@noreply.github.com>
2025-05-14 14:50:39 +0300

    `creator.onPageGetFooterActions` - The three-dot button appears for a custom page footer action Fixes #6889

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,25 @@
 import { defineConfig } from "@playwright/test";
 import { resolve } from "path";
 export default defineConfig({
+  retries: process.env.CI ? 3 : 0,
   webServer: {
     command: "",
     url: "http://localhost:8080"
   },
+  snapshotPathTemplate: "{testDir}/{testFilePath}-snapshots/{arg}{ext}",
+  reporter: [["junit", { outputFile: "test-results/e2e-junit-results.xml" }], ["html", { open: "never" }]],
   projects: [
     {
       name: "a11y",
       testDir: resolve(__dirname, "./accessibilityTests")
     },
     {
-      name: "vrt",
-      testDir: resolve(__dirname, "./visualRegressionTests")
+      name: "scr",
+      testDir: resolve(__dirname, "./screenshotTests")
     },
     {
       name: "e2e",
-      testDir: resolve(__dirname, "./functionalTests")
+      testDir: resolve(__dirname, "./e2e")
     }
   ]
 });
