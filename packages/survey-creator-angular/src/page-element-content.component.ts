e0408e6828060a02fcfc931635a19afda9c3ed1e
Aleksey Novikov <novikov82@gmail.com>
2025-05-16 15:36:22 +0300

    #6894 - fix build

--- packages/survey-creator-angular/src/page-element-content.component.ts
+++ packages/survey-creator-angular/src/page-element-content.component.ts
@@ -4,11 +4,10 @@ import { SurveyCreatorModel } from "survey-creator-core";
 
 @Component({
   selector: "svc-page-element-content",
-  template: "<page [model]=\"model\" [survey]=\"survey\" [creator]=\"creator\"></page>",
+  template: "<page [model]=\"model\" [survey]=\"survey\"></page>",
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageElementContentComponent {
   @Input() model!: PageModel;
   @Input() survey!: SurveyModel;
-  @Input() creator!: SurveyCreatorModel;
 }
