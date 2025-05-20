c22aad7d344706aafc3074fa82305ec7311bfdfa
Aleksey Novikov <novikov82@gmail.com>
2025-05-20 14:53:08 +0300

    #6894 [Angular 18] Performance Issues when navigating through a large survey-2 Fixes #6894

--- packages/survey-creator-angular/src/page-element-content.component.ts
+++ packages/survey-creator-angular/src/page-element-content.component.ts
@@ -1,15 +1,19 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { EmbeddedViewContentComponent } from "survey-angular-ui";
 import { PageModel, SurveyModel } from "survey-core";
-import { SurveyCreatorModel } from "survey-creator-core";
 
 @Component({
   selector: "svc-page-element-content",
   template: "<ng-template #template><page [model]=\"model\" [survey]=\"survey\"></page></ng-template>",
-  styles: [":host { display: none }"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [":host { display: none }"]
 })
-export class PageElementContentComponent extends EmbeddedViewContentComponent {
+export class PageElementContentComponent extends EmbeddedViewContentComponent implements AfterViewInit, OnChanges {
   @Input() model!: PageModel;
   @Input() survey!: SurveyModel;
+  ngAfterViewInit() {
+    this.embeddedView?.detach();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.embeddedView?.detectChanges();
+  }
 }
--- packages/survey-creator-angular/src/question-element-content.component.ts
+++ packages/survey-creator-angular/src/question-element-content.component.ts
@@ -1,13 +1,18 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { EmbeddedViewContentComponent } from "survey-angular-ui";
 
 @Component({
   selector: "svc-question-element-content",
   template: "<ng-template #template><ng-template [component]=\"{ name: componentName, data: componentData }\"></ng-template></ng-template>",
-  styles: [":host { display: none }"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [":host { display: none }"]
 })
-export class QuestionElementContentComponent extends EmbeddedViewContentComponent {
+export class QuestionElementContentComponent extends EmbeddedViewContentComponent implements AfterViewInit, OnChanges {
   @Input() componentName!: string;
   @Input() componentData!: any;
+  ngAfterViewInit() {
+    this.embeddedView?.detach();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.embeddedView?.detectChanges();
+  }
 }
