78fa372d64ebe1ca392eadce9a676eb8b691eb07
tsv2013 <tsv2013@noreply.github.com>
2025-05-20 14:48:45 +0300

    Added playwright f-test for pages lazy rendering

---
+++ e2e/lazy-rendering.spec.ts
@@ -0,0 +1,22 @@
+import { url, compareScreenshot } from "./helper";
+import { test, expect } from "@playwright/test";
+import { json as hugeJson } from "./questions_4114";
+
+const title = "Design Surface Lazy Rendering";
+
+test.describe(title, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${url}`);
+  });
+  test("Pages adorner content lazy rendering", async ({ page }) => {
+    await page.setViewportSize({ width: 1000, height: 800 });
+    await page.evaluate((json) => {
+      window["creator"].JSON = json;
+    }, hugeJson);
+    const skeletonElement = page.locator(".sv-skeleton-element");
+    const nonRenderedPages = page.locator(".svc-page__loading-content");
+
+    await expect(skeletonElement).toHaveCount(85);
+    await expect(nonRenderedPages).toHaveCount(107);
+  });
+});
