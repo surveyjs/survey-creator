328ff991c149c891bc986090e9ac5be12de98a68
Aleksey Novikov <novikov82@gmail.com>
2025-05-20 09:44:12 +0300

    #6894 [Angular 18] Performance Issues when navigating through a large survey

--- packages/survey-creator-angular/src/page-element-content.component.ts
+++ packages/survey-creator-angular/src/page-element-content.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { EmbeddedViewContentComponent } from "survey-angular-ui";
 import { PageModel, SurveyModel } from "survey-core";
 import { SurveyCreatorModel } from "survey-creator-core";
 
 @Component({
   selector: "svc-page-element-content",
-  template: "<page [model]=\"model\" [survey]=\"survey\"></page>",
+  template: "<ng-template #template><page [model]=\"model\" [survey]=\"survey\"></page></ng-template>",
+  styles: [":host { display: none }"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PageElementContentComponent {
+export class PageElementContentComponent extends EmbeddedViewContentComponent {
   @Input() model!: PageModel;
   @Input() survey!: SurveyModel;
 }
--- packages/survey-creator-angular/src/question-element-content.component.ts
+++ packages/survey-creator-angular/src/question-element-content.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { EmbeddedViewContentComponent } from "survey-angular-ui";
 
 @Component({
   selector: "svc-question-element-content",
-  template: "<ng-template [component]=\"{ name: componentName, data: componentData }\"></ng-template>",
+  template: "<ng-template #template><ng-template [component]=\"{ name: componentName, data: componentData }\"></ng-template></ng-template>",
+  styles: [":host { display: none }"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class QuestionElementContentComponent {
+export class QuestionElementContentComponent extends EmbeddedViewContentComponent {
   @Input() componentName!: string;
   @Input() componentData!: any;
 }
