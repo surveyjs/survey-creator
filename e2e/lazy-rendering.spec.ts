a784efb13e7471ad92bd766f3b86c191c2aec533
tsv2013 <tsv2013@noreply.github.com>
2025-05-21 12:41:36 +0300

    Added JSON loading time performance test

--- e2e/lazy-rendering.spec.ts
+++ e2e/lazy-rendering.spec.ts
@@ -19,4 +19,27 @@ test.describe(title, () => {
     await expect(await skeletonElement.count()).toBeGreaterThanOrEqual(85);
     await expect(await nonRenderedPages.count()).toBeGreaterThanOrEqual(107);
   });
+
+  test("Huge JSON long task time", async ({ page }) => {
+    const timeLimit = 4000;
+    const totalBlockingTime: number = await page.evaluate(([json, timeLimit]) => {
+      return new Promise<number>((resolve) => {
+        let totalBlockingTime = 0;
+        new PerformanceObserver(function (list) {
+          const perfEntries = list.getEntries();
+          for (const perfEntry of perfEntries) {
+            totalBlockingTime += perfEntry.duration;
+          }
+          resolve(totalBlockingTime);
+        }).observe({ type: "longtask", buffered: true });
+
+        window["creator"].JSON = json;
+        setTimeout(() => resolve(totalBlockingTime), (timeLimit as number) + 500);
+      });
+    }, [hugeJson, timeLimit]);
+
+    // eslint-disable-next-line no-console
+    console.log("totalBlockingTime: " + totalBlockingTime.toString() + "ms");
+    await expect(totalBlockingTime).toBeLessThan(timeLimit);
+  });
 });
