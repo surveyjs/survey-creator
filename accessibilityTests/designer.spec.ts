23858befd06a3836fa2c24eeb6f7dbd067200248
OlgaLarina <olga.larina.dev@gmail.com>
2025-05-29 17:02:00 +0300

    work for #6931 Interactive controls must not be nested - add a11y tests

--- accessibilityTests/designer.spec.ts
+++ accessibilityTests/designer.spec.ts
@@ -9,32 +9,34 @@ test.describe("designer a11y", () => {
     await injectAxe(page);
     await page.setViewportSize({ width: 1920, height: 1080 });
   });
+
+  const jsonWithMultiplePages = {
+    "pages": [
+      {
+        "name": "page1",
+        "elements": [{ "type": "text", "name": "question1" }]
+      },
+      {
+        "name": "page2",
+        "elements": [{ "type": "text", "name": "question2" }]
+      }
+    ],
+  };
   test("Check designer tab empty", async ({ page }) => {
     await checkA11y(page, ".svc-creator", { axeOptions });
   });
   test("Check designer tab with multiple pages", async ({ page }) => {
-    await setJSON(page, {
-      "pages": [
-        {
-          "name": "page1",
-          "elements": [
-            {
-              "type": "text",
-              "name": "question1"
-            }
-          ]
-        },
-        {
-          "name": "page2",
-          "elements": [
-            {
-              "type": "text",
-              "name": "question2"
-            }
-          ]
-        }
-      ],
-    });
+    await setJSON(page, jsonWithMultiplePages);
     await checkA11y(page, ".svc-creator", { axeOptions });
+  });
+  test("Check page navigator", async ({ page }) => {
+    await setJSON(page, jsonWithMultiplePages);
+    await page.click(".svc-page-navigator__selector");
+    await checkA11y(page, ".svc-tab-designer__page-navigator", { axeOptions });
+  });
+  test("Check sidebar header", async ({ page }) => {
+    await setJSON(page, jsonWithMultiplePages);
+    await page.click(".svc-sidebar__header-content .svc-menu-action__button");
+    await checkA11y(page, ".svc-sidebar__header", { axeOptions });
   });
 });
