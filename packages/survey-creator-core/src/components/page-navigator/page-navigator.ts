69af16140fc1818a3f64cfa713cf564abc8c88e2
OlgaLarina <olga.larina.developer@gmail.com>
2025-06-02 17:45:01 +0300

    Interactive controls must not be nested (#6933)
    * resolve #6931 Interactive controls must not be nested
    
    * work for #6931 Interactive controls must not be nested - add a11y tests
    
    * resolve #6928 Ensure ARIA attributes are not prohibited for an element's role
    
    * work for #6931 Interactive controls must not be nested - fix visual tests
    
    * lint ignore playwright-report
    
    * work for #6931 Interactive controls must not be nested - fix visual test
    
    * work for #6931 - fix page navigation a11y test
    
    ---------
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- packages/survey-creator-core/src/components/page-navigator/page-navigator.ts
+++ packages/survey-creator-core/src/components/page-navigator/page-navigator.ts
@@ -40,7 +40,9 @@ export class PageNavigatorViewModel extends Base {
         this.popupModel.hide();
       },
       cssClasses: listComponentCss,
-      allowSelection: true
+      allowSelection: true,
+      listRole: "menu",
+      listItemRole: "menuitemradio"
     });
     this.popupModel = new PopupModel("sv-list", { model: this.pageListModel }, { cssClass: "svc-creator-popup" });
     this.popupModel.focusFirstInputSelector = ".svc-list__item--selected";
@@ -299,7 +301,7 @@ export class PageNavigatorViewModel extends Base {
     // this._updateVisibility();
     setTimeout(() => {
       this._updateVisibility();
-    }, 10);
+    }, 100);
   }
   public get visibleItems() {
     if (this.items.length <= this.visibleItemsCount) {
