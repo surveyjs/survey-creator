feb5c67709ddd94c324a1d5f5637abcc7ca6edc6
OlgaLarina <olga.larina.dev@gmail.com>
2025-10-02 16:13:52 +0300

    playwright test scrollbar.ts

---
+++ screenshotTests/scrollbar.spec.ts
@@ -0,0 +1,47 @@
+import { url, compareScreenshot, test, setJSON } from "./helper";
+
+const title = "Sidebar Screenshot";
+
+test.describe(title, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${url}`);
+  });
+
+  test("check scrollbars", async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 900 });
+
+    const json = {
+      showQuestionNumbers: "on",
+      "logoPosition": "right",
+      "pages": [
+        {
+          "name": "page1",
+          "elements": [
+            {
+              "type": "radiogroup",
+              "name": "question1"
+            },
+            {
+              "type": "radiogroup",
+              "name": "question2"
+            },
+            {
+              "type": "radiogroup",
+              "name": "question2"
+            },
+          ]
+        },
+      ]
+    };
+
+    await setJSON(page, json);
+
+    await compareScreenshot(page, ".svc-side-bar__container", "scrollbar-property-grid.png");
+    // await page.hover(".svc-side-bar__container", { position: { x: -6, y: 100 } });
+    // await compareScreenshot(page, ".svc-side-bar__container", "scrollbar-property-grid-hover.png");
+
+    await compareScreenshot(page, ".svc-creator__content-holder", "scrollbar-creator-content.png");
+    // await page.hover(".svc-creator__content-holder", { position: { x: -23, y: 100 } });
+    // await compareScreenshot(page, ".svc-creator__content-holder", "scrollbar-creator-content-hover.png");
+  });
+});
