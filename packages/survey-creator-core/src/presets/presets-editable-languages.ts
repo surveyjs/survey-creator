6c88540b4c871d3126afbc3cc499f41482c309ea
Aleksey Novikov <novikov82@gmail.com>
2025-10-15 11:47:09 +0300

    #7004 - fixed language to default

--- packages/survey-creator-core/src/presets/presets-editable-languages.ts
+++ packages/survey-creator-core/src/presets/presets-editable-languages.ts
@@ -106,7 +106,7 @@ export class CreatorPresetEditableLanguages extends CreatorPresetEditableBase {
   private get surveyUseEnglishNames(): string { return this.path + "_surveyUseEnglishNames"; }
 
   public get questionNames() {
-    return [this.surveyLocalesName, this.surveyUseEnglishNames];
+    return [this.creatorLocaleName, this.surveyLocalesName, this.surveyUseEnglishNames];
   }
   private getIsShowInEnglishSelected(model: SurveyModel): boolean { return model.getValue(this.surveyUseEnglishNames) === true; }
   private getCreatorLocales(): Array<ItemValue> {
