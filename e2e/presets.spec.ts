efd85231ed87abe3fb4964d62b55bda169b44095
Aleksey Novikov <novikov82@gmail.com>
2025-10-24 18:02:18 +0300

    #7004 - drag-drop tabs tests

--- e2e/presets.spec.ts
+++ e2e/presets.spec.ts
@@ -1,4 +1,4 @@
-import { urlPresets, test, expect, showPresets } from "./helper";
+import { urlPresets, test, expect, showPresets, doDragDrop } from "./helper";
 
 const title = "Presets";
 
@@ -14,7 +14,7 @@ test.describe(title, () => {
 
   test("Check presets tabs", async ({ page }) => {
     await page.locator(".sps-list__container").getByText("Tabs").click();
-    const actualTexts = (await page.locator(".svc-tabbed-menu-item").allTextContents()).filter(t => t != "Presets");
+    const actualTexts = (await page.locator(".svc-tabbed-menu-item").allTextContents()).map(t => t.trim()).filter(t => t != "Presets");
     expect(actualTexts).toEqual(["Designer", "Preview", "Logic", "Translations"]);
 
     const items = page.locator(".sps-question--matrixdynamic table").nth(0);
@@ -69,29 +69,34 @@ test.describe(title, () => {
     expect(hidden.locator("tr input").nth(0)).toHaveValue("Themes");
     expect(hidden.locator("tr input").nth(1)).toHaveValue("JSON Editor");
 
-    await items.locator("tr").nth(1).locator(".sd-table__cell--drag").dragTo(hidden);
+    //await items.locator("tr").nth(1).locator(".sd-table__cell--drag").dragTo(hidden);
+    await doDragDrop({ page, element: items.locator("tr").nth(1).locator(".sd-table__cell--drag"), target: hidden, options: { targetPosition: { x: 5, y: 5 } } });
 
     expect(items.locator("tr input").nth(0)).toHaveValue("Designer");
     expect(items.locator("tr input").nth(1)).toHaveValue("Logic");
     expect(items.locator("tr input").nth(2)).toHaveValue("Translations");
 
-    expect(hidden.locator("tr input").nth(0)).toHaveValue("Themes");
-    expect(hidden.locator("tr input").nth(1)).toHaveValue("JSON Editor");
-    expect(hidden.locator("tr input").nth(2)).toHaveValue("Preview");
+    expect(hidden.locator("tr input").nth(0)).toHaveValue("Preview");
+    expect(hidden.locator("tr input").nth(1)).toHaveValue("Themes");
+    expect(hidden.locator("tr input").nth(2)).toHaveValue("JSON Editor");
 
     const newTexts = (await page.locator(".svc-tabbed-menu-item").allTextContents()).filter(t => t != "Presets");
     expect(newTexts).toEqual(["Designer", "Logic", "Translations"]);
 
-    await hidden.locator("tr").nth(1).locator(".sd-table__cell--drag").dragTo(items);
-    expect(items.locator("tr input").nth(0)).toHaveValue("Designer");
-    expect(items.locator("tr input").nth(1)).toHaveValue("Logic");
-    expect(items.locator("tr input").nth(2)).toHaveValue("Translations");
-    expect(items.locator("tr input").nth(3)).toHaveValue("JSON Editor");
-
-    expect(hidden.locator("tr input").nth(0)).toHaveValue("Themes");
-    expect(hidden.locator("tr input").nth(1)).toHaveValue("Preview");
+    await doDragDrop({ page, element: hidden.locator("tr").nth(2).locator(".sd-table__cell--drag"), target: items, options: { targetPosition: { x: 5, y: 5 } } });
+    expect(items.locator("tr input").nth(0)).toHaveValue("JSON Editor");
+    expect(items.locator("tr input").nth(1)).toHaveValue("Designer");
+    expect(items.locator("tr input").nth(2)).toHaveValue("Logic");
+    expect(items.locator("tr input").nth(3)).toHaveValue("Translations");
 
     const newTexts2 = (await page.locator(".svc-tabbed-menu-item").allTextContents()).filter(t => t != "Presets");
-    expect(newTexts2).toEqual(["Designer", "Logic", "Translations", "JSON Editor"]);
+    expect(newTexts2).toEqual(["JSON Editor", "Designer", "Logic", "Translations"]);
+    await doDragDrop({ page, element: items.locator("tr").nth(1).locator(".sd-table__cell--drag"), target: items.locator("tr").nth(3).locator(".sd-table__cell--drag"), options: { targetPosition: { x: 5, y: 5 } } });
+    expect(items.locator("tr input").nth(0)).toHaveValue("JSON Editor");
+    expect(items.locator("tr input").nth(1)).toHaveValue("Logic");
+    expect(items.locator("tr input").nth(2)).toHaveValue("Designer");
+    expect(items.locator("tr input").nth(3)).toHaveValue("Translations");
+    const newTexts3 = (await page.locator(".svc-tabbed-menu-item").allTextContents()).filter(t => t != "Presets");
+    expect(newTexts3).toEqual(["JSON Editor", "Logic", "Designer", "Translations"]);
   });
 });
