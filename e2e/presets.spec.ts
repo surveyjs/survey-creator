d1b428c35dca4eedb2e493cab60d86448056dfd8
Aleksey Novikov <novikov82@gmail.com>
2025-10-28 18:03:35 +0300

    #7004 - e2e tests

--- e2e/presets.spec.ts
+++ e2e/presets.spec.ts
@@ -13,14 +13,14 @@ async function getTabsTexts(page: any) {
 }
 
 async function getToolboxTexts(page: any) {
-  return (await page.locator(".svc-toolbox__item").filter({ visible: true }).allTextContents()).map(t => t.trim());
+  return (await page.locator(".svc-toolbox__item-title").filter({ visible: true }).allTextContents()).map(t => t.trim());
 }
 
 test.describe(title, () => {
   test.beforeEach(async ({ page }) => {
     await page.waitForLoadState("networkidle");
     await page.goto(`${urlPresets}`);
-    await page.setViewportSize({ width: 1440, height: 900 });
+    await page.setViewportSize({ width: 1440, height: 1400 });
     await showPresets(page);
   });
 
@@ -98,7 +98,7 @@ test.describe(title, () => {
     expect(await getRowsInputValues(items)).toEqual(["Designer", "Preview", "Logic", "Translations"]);
   });
 
-  test("Check presets toolbox - edit", async ({ page }) => {
+  test("Check presets toolbox", async ({ page }) => {
     await page.locator(".sps-list__container").getByText("Toolbox").click();
     expect((await getToolboxTexts(page)).slice(0, 3)).toEqual(["Radio Button Group", "Rating Scale", "Slider"]);
 
@@ -121,6 +121,49 @@ test.describe(title, () => {
 
     await hidden.getByText("Move to new category").click();
     expect((await getToolboxTexts(page)).slice(20, 21)).toEqual(["Panel"]);
+  });
+
+  test("Check presets tabs - drag-drop categories", async ({ page }) => {
+    await page.locator(".sps-list__container").getByText("Toolbox").click();
+    expect((await getToolboxTexts(page)).slice(0, 3)).toEqual(["Radio Button Group", "Rating Scale", "Slider"]);
+
+    const items = page.locator(".sps-row--multiple > div").nth(0).locator(".sps-question--matrixdynamic table").nth(0);
+    const hidden = page.locator(".sps-row--multiple > div").nth(1).locator(".sps-question--matrixdynamic table").nth(0);
+
+    expect(await getRowsInputValues(items)).toEqual(["Choice Questions", "Text Input Questions", "Containers", "Matrix Questions", "Misc"]);
+    expect(await getRowsInputValues(hidden)).toEqual([]);
+    expect((await getToolboxTexts(page)).slice(0, 4)).toEqual(["Radio Button Group", "Rating Scale", "Slider", "Checkboxes"]);
+
+    await doDragDrop({ page, element: items.locator("tr").filter({ visible: true }).nth(2).locator(".sd-table__cell--drag"), target: items.locator("tr").filter({ visible: true }).nth(0).locator(".sd-table__cell--drag"), options: { targetPosition: { x: 5, y: 5 } } });
+    expect(await getRowsInputValues(items)).toEqual(["Containers", "Choice Questions", "Text Input Questions", "Matrix Questions", "Misc"]);
+    expect(await getRowsInputValues(hidden)).toEqual([]);
+    expect((await getToolboxTexts(page)).slice(0, 4)).toEqual(["Panel", "Dynamic Panel", "Radio Button Group", "Rating Scale"]);
+  });
+
+  test("Check presets toolbox - drag-drop items", async ({ page }) => {
+    await page.locator(".sps-list__container").getByText("Toolbox").click();
+    expect((await getToolboxTexts(page)).slice(0, 3)).toEqual(["Radio Button Group", "Rating Scale", "Slider"]);
+
+    const items = page.locator(".sps-row--multiple > div").nth(0).locator(".sps-question--matrixdynamic table").nth(0);
+    const hidden = page.locator(".sps-row--multiple > div").nth(1).locator(".sps-question--matrixdynamic table").nth(0);
+    await items.getByRole("row", { name: "Choice Questions" }).locator("#show-detail").getByRole("button").click();
+    await doDragDrop({ page, element: items.getByRole("row", { name: "Slider" }).locator(".sd-table__cell--drag"), target: hidden, options: { targetPosition: { x: 5, y: 5 } } });
+
+    expect((await getToolboxTexts(page)).slice(0, 3)).toEqual(["Radio Button Group", "Rating Scale", "Checkboxes"]);
+
+    await items.getByRole("row", { name: "Text Input Questions" }).locator("#show-detail").getByRole("button").click();
+    await doDragDrop({ page, element: hidden.getByRole("row", { name: "Slider" }).locator(".sd-table__cell--drag"), target: items.getByRole("row", { name: "Multiple Textboxes" }), options: { targetPosition: { x: 25, y: 25 } } });
+
+    expect((await getToolboxTexts(page)).slice(9, 13)).toEqual(["Single-Line Input", "Long Text", "Slider", "Multiple Textboxes"]);
+
+    await doDragDrop({ page, element: items.getByRole("row", { name: "Long Text" }).locator(".sd-table__cell--drag"), target: items.getByRole("row", { name: "Ranking" }), options: { targetPosition: { x: 5, y: 5 } } });
+    expect((await getToolboxTexts(page)).slice(7, 12)).toEqual(["Image Picker", "Long Text", "Ranking", "Single-Line Input", "Slider"]);
+
+    await items.getByRole("row", { name: "Rating" }).locator("#show-detail").getByRole("button").click();
+    await doDragDrop({ page, element: items.getByRole("row", { name: "Slider" }).locator(".sd-table__cell--drag"), target: items.getByRole("row", { name: "Stars" }) });
+    await page.locator(".svc-toolbox__item-submenu-button").nth(0).hover();
+    expect(await page.locator(".svc-toolbox-subtypes .sv-popup__container").filter({ visible: true })).toBeVisible();
+    expect((await page.locator(".svc-toolbox__item-subtype").filter({ visible: true }).allTextContents()).map(t => t.trim())).toEqual(["Labels", "Stars", "Slider", "Smileys"]);
   });
 
 });
