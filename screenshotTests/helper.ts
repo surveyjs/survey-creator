ead0f0a8647a21ef6cc1c032a96f198bcb479e0d
OlgaLarina <olga.larina.developer@gmail.com>
2025-11-06 16:00:37 +0300

    Preview - Impossible to select survey UI text (#7255)
    * resolve #7253 Preview - Impossible to select survey UI text
    
    * work for #7253
    
    ---------
    
    Co-authored-by: OlgaLarina <olga.larina.dev@gmail.com>

--- screenshotTests/helper.ts
+++ screenshotTests/helper.ts
@@ -168,10 +168,11 @@ export async function addQuestionByAddQuestionButton(page, text) {
   await page.locator(".svc-list__item span").getByText(text).click();
 }
 
-export async function changeToolboxLocation(page, newVal: string) {
+export async function changeToolboxLocation(page: Page, newVal: string) {
   await page.evaluate((newVal) => {
     window["creator"].toolboxLocation = newVal;
   }, newVal);
+  await page.waitForTimeout(500);
 }
 
 export async function changeToolboxScrolling(page, hasScroll: boolean) {
