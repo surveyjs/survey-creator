95ebfcee9d16d2b19082728779105ca838250939
Elena Gorbatkova <102306951+ElenaGorbatkova@users.noreply.github.com>
2025-11-11 19:44:48 +0400

    Update webpack.config.js

--- packages/survey-creator-js/webpack.config.js
+++ packages/survey-creator-js/webpack.config.js
@@ -14,25 +14,42 @@ const banner = [
   "SurveyJS Creator UI v" + packageJson.version,
   "(c) 2015-" + year + " Devsoft Baltic OÜ - http://surveyjs.io/",
   "Github: https://github.com/surveyjs/survey-creator",
-  "License: https://surveyjs.io/Licenses#SurveyCreator"
+  "License: https://surveyjs.io/licensing"
 ].join("\n");
 
 const buildPlatformJson = {
   name: packageJson.name,
   version: packageJson.version,
-  description:
-    "Use SurveyJS Creator to create or edit JSON for SurveyJS Form Library.",
+  description: "A white-label drag-and-drop form builder for HTML, CSS, and JavaScript apps. It generates JSON schemas for rendering dynamic forms with SurveyJS Form Library. Design complex, interactive forms and surveys without writing code.",
+  author: "DevSoft Baltic OU <info@devsoftbaltic.com>",
+  homepage: "https://surveyjs.io/",
+  license: "SEE LICENSE IN LICENSE",
+  licenseUrl: "https://surveyjs.io/licensing",
   keywords: [
-    "Survey",
-    "Survey Creator",
-    "Form Builder",
-    "Survey Maker",
-    "SurveyJS",
-    "JavaScript",
-    "TypeScript"
+    "vanilla",
+    "vanilla-js",
+    "survey",
+    "form",
+    "surveyjs",
+    "survey-library",
+    "form-builder",
+    "dynamic-form-builder",
+    "drag-and-drop-form-builder",
+    "form-library",
+    "form-management",
+    "questionnaire",
+    "data-collection",
+    "ui-component",
+    "json",
+    "json-schema",
+    "schema-form",
+    "survey-creator",
+    "survey-builder",
+    "client-side",
+    "frontend",
+    "javascript",
+    "typescript"
   ],
-  homepage: "https://surveyjs.io/Overview/Survey-Creator",
-  license: "https://surveyjs.io/Licenses#SurveyCreator",
   files: [
     "**/*"
   ],
