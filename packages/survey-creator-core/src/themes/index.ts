73e206896c6e9e796dfb9bdaac7f4e380d5a9b4c
Aleksey Novikov <novikov82@gmail.com>
2025-11-17 15:03:12 +0300

    #7070 fix export

--- packages/survey-creator-core/src/themes/index.ts
+++ packages/survey-creator-core/src/themes/index.ts
@@ -3,5 +3,5 @@ import DefaultLight from "./default-light";
 import DefaultDark from "./default-dark";
 import DefaultContrast from "./default-contrast";
 const __surveyjs_internal_themes_hash = true;
-export { SC2020, DefaultDark, DefaultContrast, __surveyjs_internal_themes_hash };
+export { SC2020, DefaultLight, DefaultDark, DefaultContrast, __surveyjs_internal_themes_hash };
 export default { SC2020, DefaultLight, DefaultDark, DefaultContrast, __surveyjs_internal_themes_hash };
