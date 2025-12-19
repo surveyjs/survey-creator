44ca9683350068bd654a2fe976a80eadfe8e2880
Aleksey Novikov <novikov82@gmail.com>
2025-12-19 09:10:35 +0300

    #7300 - rename module flag

--- packages/survey-creator-core/src/ui-presets/index.ts
+++ packages/survey-creator-core/src/ui-presets/index.ts
@@ -2,6 +2,6 @@ import Basic from "./basic";
 import Advanced from "./advanced";
 import Expert from "./expert";
 
-const __surveyjs_internal_themes_hash = true;
-export { Basic, Advanced, Expert, __surveyjs_internal_themes_hash };
-export default { Basic, Advanced, Expert, __surveyjs_internal_themes_hash };
+const __surveyjs_internal_configs_hash = true;
+export { Basic, Advanced, Expert, __surveyjs_internal_configs_hash };
+export default { Basic, Advanced, Expert, __surveyjs_internal_configs_hash };
