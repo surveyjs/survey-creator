d01f4a2e00ccca63a43522c31246c45576512ec0
Andrew Telnov <andrew.telnov@gmail.com>
2026-02-11 11:39:15 +0300

    Add missed properties into property grid definition

--- packages/survey-creator-core/src/question-editor/definition.ts
+++ packages/survey-creator-core/src/question-editor/definition.ts
@@ -227,7 +227,8 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       { name: "colCount", tab: "layout" },
       { name: "inputSize", tab: "layout" },
       { name: "itemErrorLocation", tab: "layout" },
-      { name: "items", tab: "items" }
+      { name: "items", tab: "items" },
+      { name: "itemTitleWidth", tab: "layout" },
     ],
     tabs: [{ name: "items", index: 10 }]
   },
@@ -311,14 +312,17 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       { name: "minSelectedChoices", tab: "choices" },
       { name: "maxSelectedChoices", tab: "choices" },
       { name: "separateSpecialChoices", tab: "choices" },
+      { name: "valuePropertyName", tab: "data" }
     ]
   },
   ranking: {
     properties: [
+      "selectToRankEmptyRankedAreaText",
+      "selectToRankEmptyUnrankedAreaText",
       { name: "selectToRankEnabled" },
       { name: "selectToRankAreasLayout" },
       { name: "minSelectedChoices", tab: "choices" },
-      { name: "maxSelectedChoices", tab: "choices" }
+      { name: "maxSelectedChoices", tab: "choices" },
     ]
   },
   radiogroup: {
@@ -369,7 +373,7 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       "imageFit",
       "imageHeight",
       "imageWidth",
-      "text"
+      "altText"
     ]
   },
   "itemvalue[]@choices": {
@@ -420,6 +424,7 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       "label",
       "labelTrue",
       "labelFalse",
+      "swapOrder",
       { name: "valueTrue", tab: "data" },
       { name: "valueFalse", tab: "data" }
     ]
@@ -432,7 +437,8 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       "format",
       "maximumFractionDigits",
       "minimumFractionDigits",
-      "useGrouping"
+      "useGrouping",
+      { name: "precision", tab: "data" }
     ]
   },
   "panellayoutcolumn": {
@@ -641,6 +647,7 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       { name: "rateColorMode", tab: "rateValues" },
       { name: "scaleColorMode", tab: "rateValues" },
       { name: "autoGenerate", tab: "rateValues" },
+      { name: "rateValues", tab: "rateValues" },
       { name: "rateMin", tab: "rateValues" },
       { name: "rateMax", tab: "rateValues" },
       { name: "rateStep", tab: "rateValues" },
@@ -736,7 +743,9 @@ const defaultProperties: ISurveyPropertiesDefinition = {
       { name: "templateQuestionTitleLocation", tab: "questionSettings" },
       { name: "templateQuestionTitleWidth", tab: "questionSettings" },
       { name: "templateErrorLocation", tab: "questionSettings" },
+      { name: "templateVisibleIf", tab: "logic" },
       { name: "removePanelButtonLocation", tab: "layout" },
+      { name: "newPanelPosition", tab: "layout" },
       { name: "showNumber", tab: "numbering" },
       { name: "showQuestionNumbers", tab: "numbering" },
       { name: "questionStartIndex", tab: "numbering" },
