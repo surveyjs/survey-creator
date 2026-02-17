8568d11d15005425b109a2bafd7d33ad90046e95
dmitrykurmanov <kurmanov.work@gmail.com>
2026-02-17 12:29:05 +0400

    SSR and Shadow-Dom compatibility https://github.com/surveyjs/private-tasks/issues/512

--- packages/survey-creator-vue/src/questions/EmbeddedSurvey.vue
+++ packages/survey-creator-vue/src/questions/EmbeddedSurvey.vue
@@ -1,18 +1,23 @@
 <template>
-  <SvComponent
-    :is="'sv-page'"
-    v-if="!!question && !!survey && survey.currentPage"
-    :page="survey.currentPage"
-    :survey="survey"
-  ></SvComponent>
+  <div style="display: 'contents'" ref="root">
+    <SvComponent
+      :is="'sv-page'"
+      v-if="!!question && !!survey && survey.currentPage"
+      :page="survey.currentPage"
+      :survey="survey"
+    ></SvComponent>
+  </div>
 </template>
 <script lang="ts" setup>
 import { SvComponent } from "survey-vue3-ui";
 import type { QuestionEmbeddedSurveyModel } from "survey-creator-core";
 import { useQuestion } from "survey-vue3-ui";
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 const props = defineProps<{ question: QuestionEmbeddedSurveyModel }>();
 const root = ref<HTMLElement>();
 useQuestion(props, root);
 const survey = computed(() => props.question.embeddedSurvey);
+onMounted(() => {
+  survey.value.rootElement = root.value as HTMLElement;
+});
 </script>
