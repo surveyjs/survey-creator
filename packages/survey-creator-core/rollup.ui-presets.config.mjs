2c5fba4fed0f0b7eb8ade116405d9dea510ab8a9
Dmitrii Gerasimov <dima@rimmon.dev>
2026-02-19 12:15:52 +0200

    ui-presets

--- packages/survey-creator-core/rollup.ui-presets.config.mjs
+++ packages/survey-creator-core/rollup.ui-presets.config.mjs
@@ -16,20 +16,20 @@ export default () => {
       tsconfig: resolve(__dirname, "tsconfig.ui-presets.json"),
       version: pkg.version
     }),
-    // ...[
-    //   { filename: "basic", path: resolve(__dirname, "./src/ui-presets/basic.ts"), globalName: "SurveyCreatorUIPreset.Basic" },
-    //   { filename: "advanced", path: resolve(__dirname, "./src/ui-presets/advanced.ts"), globalName: "SurveyCreatorUIPreset.Advanced" },
-    //   { filename: "expert", path: resolve(__dirname, "./src/ui-presets/expert.ts"), globalName: "SurveyCreatorUIPreset.Expert" },
-    //   { filename: "index", path: resolve(__dirname, "./src/ui-presets/index.ts"), globalName: "SurveyCreatorUIPreset" },
-    // ].map(({ filename, path, globalName }) => createUmdConfig({
-    //   input: { [filename]: path },
-    //   tsconfig: resolve(__dirname, "tsconfig.ui-presets.json"),
-    //   external: ["survey-creator-core"],
-    //   dir: resolve(buildPath, "ui-presets"),
-    //   globalName: globalName,
-    //   globals: { "survey-creator-core": "SurveyCreatorCore" },
-    //   emitMinified: process.env.emitMinified === "true",
-    //   version: pkg.version,
-    // }))
+    ...[
+      { filename: "basic", path: resolve(__dirname, "./src/ui-presets/basic.ts"), globalName: "SurveyCreatorUIPreset.Basic" },
+      { filename: "advanced", path: resolve(__dirname, "./src/ui-presets/advanced.ts"), globalName: "SurveyCreatorUIPreset.Advanced" },
+      { filename: "expert", path: resolve(__dirname, "./src/ui-presets/expert.ts"), globalName: "SurveyCreatorUIPreset.Expert" },
+      { filename: "index", path: resolve(__dirname, "./src/ui-presets/index.ts"), globalName: "SurveyCreatorUIPreset" },
+    ].map(({ filename, path, globalName }) => createUmdConfig({
+      input: { [filename]: path },
+      tsconfig: resolve(__dirname, "tsconfig.ui-presets.json"),
+      external: ["survey-creator-core"],
+      dir: resolve(buildPath, "ui-presets"),
+      globalName: globalName,
+      globals: { "survey-creator-core": "SurveyCreatorCore" },
+      emitMinified: process.env.emitMinified === "true",
+      version: pkg.version,
+    }))
   ];
 };
