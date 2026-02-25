469297989cfa8cbe1c09907f23ea70f65018e706
Aleksey Novikov <novikov82@gmail.com>
2026-02-25 08:40:42 +0300

    Issue/7385 presets  custom presets edit (#7401)
    * #7381 Presets: top bar navigation
    Fixes #7381
    
    * Presets: top bar navigation
    Fixes #7381
    
    * #7381 - update tests
    
    * #7381 update tests
    
    * #7381 - fixed dialog
    
    * #7381 - update screenshots
    
    * Presets: custom presets edit
    Fixes #7385
    
    * Presets: custom presets edit
    Fixes #7385
    
    * #7385
    
    * #7381 - update screenshot
    
    * #7385 - save as dialog
    
    * #7381 - keep menu title
    
    * Presets: custom presets edit
    Fixes #7385
    
    * #7381 - increase timeout
    
    * #7385
    
    * #7385
    
    * #7385
    
    * #7385 - extract PresetsManager
    
    * fix reference
    
    * #7385 - fixed imports
    
    * Presets: custom presets edit
    Fixes #7385
    
    * Presets: custom presets edit
    Fixes #7385
    
    * #7385 - fix enable/disable save item
    
    * #7385 - presets management inside creator
    
    * Presets: custom presets edit
    Fixes #7385
    
    * popup width #7385
    
    * #7385 - popup css
    
    * #7385 - save status
    
    * #7385 - saving and confirmations
    
    * #7385 - scrolling
    
    * #7385 - add scroll
    
    * #7385 - hide dropdown popup on blur
    
    * #7385 - change methods signature
    
    * #7385 - fix initial property grid state
    
    * #7385 - fixed unit tests
    
    * #7385 - unit tests for presets manager
    
    * #7385 - screenshot tests
    
    * #7385 - screenshot tests
    
    * #7385 - add f-tests
    
    * #7385 - fix preset list dialog styles
    
    * #7385 - fixed tests
    
    * #7385 - remove labels from presets menu
    
    * #7385 - fix icon color
    
    * #7385 - update screenshots
    
    * #7385 - update legacy screenshots
    
    * #7385 - update f-tests
    
    * #7385 - fix property grid category reset
    
    * #7385 - update screenshot tests
    
    * #7402 fix angular and vue examples
    
    * #7385 - fix tests texts
    
    * #7385 - update legacy screenshot
    
    * #7385 - api
    
    * #7385 - fixed unit tests
    
    * #7385 - fix options navigation panel
    
    * #7385 - selected icon color fix
    
    * #7385 - fix api
    
    * #7385 - fixed page selection
    
    * #7385 - rewrite preset with the same name
    
    * #7385 - switch preset if selected is deleted
    
    * #7385 - deletig custom preset
    
    * Presets: custom presets edit Fixes #7385
    
    * #7385 - dialogs
    
    * work for #7385
    
    * #7385 - confirmation styles
    
    * #7385 - fix tests
    
    * #7385 - fix tests
    
    * #7385 - add screenshot tests for dialogs
    
    * #7385 - add close button to exit confirmation
    
    * #7385 - add exit confirm test
    
    * work for #7385
    
    * #7385 - fix quit action
    
    * #7385 - fix build
    
    * #7385 - fix tests
    
    * #7385 - saving status
    
    * #7385 - support preset rename
    
    * #7385 - remove unused method
    
    * #7385 - fix status on property grid
    
    * #7385 - save as
    
    * #7385 - fix f-test
    
    * #7385 - remove duplicated test
    
    * #7385 - update screenshots
    
    * #7385 - fix vue
    
    * #7385 - fix styles for angular
    
    * #7385 - try to fix v-test
    
    * #7385 - update legacu screenshots

--- packages/survey-creator-core/src/ui-preset-editor/presets-editable-properties.ts
+++ packages/survey-creator-core/src/ui-preset-editor/presets-editable-properties.ts
@@ -467,6 +467,7 @@ export class CreatorPresetEditablePropertyGrid extends CreatorPresetEditableCare
   }
 
   private firstTimeLoading = false;
+  public get isSettingUp() { return this.firstTimeLoading; }
   protected updateOnValueChangedCore(model: SurveyModel, name: string): void {
     super.updateOnValueChangedCore(model, name);
     if (name == this.nameCategories) {
@@ -512,7 +513,7 @@ export class CreatorPresetEditablePropertyGrid extends CreatorPresetEditableCare
       model.setValue(this.nameCategories, categories);
       const items = this.getCurrentlyHiddenItems(categories);
       model.setValue(this.nameMatrix, items);
-      this.propertyGridSetObj(this.currentProperties.getObj());
+      if (this.pageName == model.currentPage.name)this.propertyGridSetObj(this.currentProperties.getObj());
     }
   }
   private getCurrentlyHiddenItems(categories: any) {
