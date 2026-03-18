529fcd8e5f5196cd2c322bf561c146b597c7c981
LARINA-NB-W10\OlgaLarina <olga.larina.dev@gmail.com>
2026-03-18 17:25:32 +0300

    resolve #7494 Dropdown question editor in a survey trigger editor for questions with large names renders incorrectly

--- screenshotTests/pg-editors.spec.ts
+++ screenshotTests/pg-editors.spec.ts
@@ -371,6 +371,37 @@ test.describe(title, () => {
     await compareScreenshot(page, page.locator("div[data-name='triggers']"), "triggers-editor-focused.png");
   });
 
+  test("Check triggers long setToName wrap", async ({ page }) => {
+    await page.setViewportSize({ width: 1280, height: 900 });
+    await setJSON(page, {
+      "pages": [
+        {
+          "name": "page1",
+          "elements": [
+            {
+              "type": "radiogroup",
+              "name": "question1adas dad asd sdfdsfdsa fsfdsafds fdsf sdaf sdafsdf ads f",
+              "choices": ["Item 1", "Item 2", "Item 3"]
+            }
+          ]
+        }
+      ],
+      "triggers": [
+        {
+          "type": "setvalue",
+          "setToName": "question1adas dad asd sdfdsfdsa fsfdsafds fdsf sdaf sdafsdf ads f"
+        }
+      ]
+    });
+
+    await getPropertyGridCategory(page, "Conditions").click();
+
+    await page.locator("div[data-name='triggers'] button[title='Show Details']").first().click();
+    await page.waitForTimeout(500);
+
+    await compareScreenshot(page, page.locator("div[data-name='triggers'] .spg-table__cell--detail-panel").first(), "triggers-editor-dropdown-long-text.png");
+  });
+
   test("Check question with error", async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1920 });
     await setJSON(page, { pages: [{ elements: [{ type: "text", name: "question1" }] }] });
