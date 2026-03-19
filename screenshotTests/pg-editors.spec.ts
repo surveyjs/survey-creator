3994ce2efc6076a289b65b6b4ec5daa869c74c42
Andrew Telnov <andrew.telnov@gmail.com>
2026-03-19 18:19:04 +0200

    Add delay before screenshot comparison in logic popup mobile test

--- screenshotTests/pg-editors.spec.ts
+++ screenshotTests/pg-editors.spec.ts
@@ -236,6 +236,7 @@ test.describe(title, () => {
     await getPropertyGridCategory(page, generalGroupName).click();
     await getPropertyGridCategory(page, "Conditions").click();
     await page.locator(".spg-panel__content div[data-name='visibleIf'] button").filter({ hasText: "Edit" }).click();
+    await page.waitForTimeout(500);
     await compareScreenshot(page, page.locator(".sv-popup.svc-property-editor.sv-popup--modal-overlay"), "pg-logic-popup-mobile.png");
   });
 
