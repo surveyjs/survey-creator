f89f902100ffd9d36d6e7261abf4ef59a6ed37a1
Aleksey Novikov <novikov82@gmail.com>
2026-03-27 13:30:04 +0300

    #7535 - set property directly

--- packages/survey-creator-core/src/ui-presets-creator/creator-presets-model.ts
+++ packages/survey-creator-core/src/ui-presets-creator/creator-presets-model.ts
@@ -9,23 +9,19 @@ export class CreatorPresetsModel extends Base implements IPreset {
   }
 
   @property() presetName: string = "";
-  private _suppressEvent = false;
 
   public get name(): string { return this.presetName; }
   public set name(val: string) { this.presetName = val; }
 
   public setPresetNameSilent(val: string): void {
-    if (this.presetName === val) return;
-    this._suppressEvent = true;
-    this.presetName = val;
-    this._suppressEvent = false;
+    this.setPropertyValueDirectly("presetName", val);
   }
 
   public onPresetSelected = new EventBase<CreatorPresetsModel, { preset: IPreset }>();
   constructor() {
     super();
     this.onPropertyValueChangedCallback = (propName: string) => {
-      if (propName === "presetName" && this.presetName && !this._suppressEvent) {
+      if (propName === "presetName" && this.presetName) {
         this.onPresetSelected.fire(this, { preset: CreatorPresets[this.presetName] });
       }
     };
