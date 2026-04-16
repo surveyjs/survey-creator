1d892efb2abdb2df5cb88be0a6b73ec79498c24a
Andrew Telnov <andrew.telnov@gmail.com>
2026-04-16 06:47:58 +0300

    Fix Logic popup mobile vrt test

--- screenshotTests/pg-editors.spec.ts
+++ screenshotTests/pg-editors.spec.ts
@@ -238,6 +238,7 @@ test.describe(title, () => {
     await getPropertyGridCategory(page, "Conditions").click();
     await page.locator(".spg-panel__content div[data-name='visibleIf'] button").filter({ hasText: "Edit" }).click();
     await page.waitForTimeout(500);
+    await resetFocusToBody(page);
     await compareScreenshot(page, page.locator(".sv-popup.svc-property-editor.sv-popup--modal-overlay"), "pg-logic-popup-mobile.png");
   });
 
