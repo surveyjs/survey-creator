6f8845161bb63557d3abcc088365aa56fcc88c9d
Dmitry Kuzin <dk981234@gmail.com>
2026-04-18 02:43:06 +0400

    Fix angular templates

--- packages/survey-creator-angular/src/side-bar/tabs.component.ts
+++ packages/survey-creator-angular/src/side-bar/tabs.component.ts
@@ -1,16 +1,14 @@
 import { Component, Input } from "@angular/core";
 import { BaseAngular } from "survey-angular-ui";
-import { ActionContainer } from "survey-core";
-import { MenuButton } from "survey-creator-core";
-
+import { Action, ActionContainer } from "survey-core";
 @Component({
   selector: "svc-tabs",
   templateUrl: "./tabs.component.html",
   styles: [":host { display: none; }"]
 })
-export class TabsComponent extends BaseAngular<ActionContainer<MenuButton>> {
-  @Input() model!: ActionContainer<MenuButton>;
-  protected getModel(): ActionContainer<MenuButton> {
+export class TabsComponent extends BaseAngular<ActionContainer<Action>> {
+  @Input() model!: ActionContainer<Action>;
+  protected getModel(): ActionContainer<Action> {
     return this.model;
   }
 }
