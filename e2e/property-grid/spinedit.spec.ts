f4fa7a518f53d622f4250a753d1aade19df249bc
Dmitry Kuzin <dk981234@gmail.com>
2026-05-05 13:17:35 +0400

    Fix spin edit action error

--- e2e/property-grid/spinedit.spec.ts
+++ e2e/property-grid/spinedit.spec.ts
@@ -8,6 +8,35 @@ test.describe(title, () => {
     await page.setViewportSize({ width: 1920, height: 1080 });
   });
 
+  test("Check spinedit arrows click", async ({ page }) => {
+    await page.evaluate(() => {
+      (<any>window).Survey.Serializer.addProperty("survey", {
+        name: "fontSize",
+        default: 16,
+        category: "general",
+        visibleIndex: 0
+      });
+      (<any>window).SurveyCreatorCore.PropertyGridEditorCollection.register({
+        fit: function (prop) {
+          return prop.name === "fontSize";
+        },
+        getJSON: function (obj, prop, options) {
+          return { type: "spinedit", unit: "px" };
+        }
+      });
+    });
+    await setJSON(page, {});
+    await getPropertyGridCategory(page, generalGroupName).click();
+    const questionLocator = page.locator("div[data-name='fontSize']");
+    const arrowButtonLocator = questionLocator.locator(".sd-formbox__group .sd-action");
+    await arrowButtonLocator.first().click();
+    const fontSize1 = await page.evaluate(() => (<any>window).creator.survey.fontSize);
+    expect(fontSize1).toBe(15);
+    await arrowButtonLocator.last().click();
+    const fontSize2 = await page.evaluate(() => (<any>window).creator.survey.fontSize);
+    expect(fontSize2).toBe(16);
+  });
+
   test("Check spinedit editor applies value on enter", async ({ page }) => {
     await page.evaluate(() => {
       (<any>window).Survey.Serializer.addProperty("survey", {
