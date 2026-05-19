2346ad01ae3523ed4950bb1607db46e776d49b64
Aleksey Novikov <novikov82@gmail.com>
2026-05-19 16:08:18 +0300

    fix vueexample build #7702

--- packages/survey-creator-vue/example/src/components/test/Common.vue
+++ packages/survey-creator-vue/example/src/components/test/Common.vue
@@ -8,7 +8,7 @@ import { slk } from "survey-core";
 import { registerSurveyTheme, SurveyCreatorModel, type ICreatorOptions } from "survey-creator-core";
 import SurveyThemes from "survey-core/themes";
 registerSurveyTheme(SurveyThemes);
-registerCreatorTheme(SurveyTheme);
+registerCreatorTheme(SurveyThemes);
 import "survey-core/survey.i18n";
 import "survey-creator-core/survey-creator-core.i18n";
 import { shallowRef } from "vue";
