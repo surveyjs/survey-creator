4489f903f7d942c8ce74e6ff5859f7bce1102012
Dmitry <dk981234@gmail.com>
2026-06-09 17:09:23 +0400

    Update etalons after remove fallbacks in library [library:11374] (#7786)
    * Update etalons after remove fallbacks in library
    
    * Fix unit test

--- packages/survey-creator-core/tests/tabs/theme-tab-plugin.tests.ts
+++ packages/survey-creator-core/tests/tabs/theme-tab-plugin.tests.ts
@@ -426,7 +426,7 @@ test("Get theme changes only", (): any => {
 
   const fullModifiedTheme = themePlugin.getCurrentTheme() || {};
   expect(Object.keys(fullModifiedTheme).length).toBe(10);
-  expect(Object.keys(fullModifiedTheme.cssVariables).length).toBe(44);
+  expect(Object.keys(fullModifiedTheme.cssVariables).length).toBe(42);
 
   const modifiedThemeChanges = themePlugin.getCurrentTheme(true) || {};
   expect(Object.keys(modifiedThemeChanges).length).toBe(6);
@@ -450,7 +450,7 @@ test("Get theme changes only", (): any => {
     "header",
     "headerView",
   ]);
-  expect(Object.keys(fullThemeReset.cssVariables).length).toBe(40);
+  expect(Object.keys(fullThemeReset.cssVariables).length).toBe(38);
 
   const themeChangesReset = themePlugin.getCurrentTheme(true);
   expect(Object.keys(themeChangesReset).length).toBe(6);
