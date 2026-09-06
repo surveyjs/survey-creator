95c9d8def5220ecdb73ba1f5ff4d280e91aec861
Dmitry Kuzin <dk981234@gmail.com>
2026-09-06 22:54:17 +0400

    Fix matrixs on design surface and presets

--- packages/survey-creator-core/src/ui-preset-editor/presets-manager.ts
+++ packages/survey-creator-core/src/ui-preset-editor/presets-manager.ts
@@ -379,7 +379,7 @@ export class PresetsManager {
         ...propertyGridCss.matrixdynamic,
         actionsCell: propertyGridCss.matrixdynamic.actionsCell + " sps-table__cell--actions",
         buttonAdd: presetsCss.matrixdynamic.buttonAdd,
-        footer: presetsCss.matrixdynamic.footer + " sps-matrixdynamic__footer--in-dialog",
+        footer: presetsCss.matrixdynamic.toolbar + " sps-matrixdynamic__footer--in-dialog",
       }
     };
     const popupModel = settings.showDialog?.(<IDialogOptions>{
