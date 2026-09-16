a7056feedec8f7ed435e823fa6de5b534d99fba6
RomanTsukanov <sergeich16@gmail.com>
2026-09-16 19:00:28 +0400

    Fix tests

--- packages/survey-creator-core/src/variable-presets.ts
+++ packages/survey-creator-core/src/variable-presets.ts
@@ -88,7 +88,7 @@ export class VariablePresetsManager extends Base {
     this.setPropertyValue("active", name);
     this.raiseChanged("edit");
   }
-  private raiseChanged(reason: string): void {
+  private raiseChanged(reason: "select" | "edit"): void {
     if (!this.creator) return;
     this.creator.onVariablePresetsChanged.fire(this.creator, {
       reason: reason,
